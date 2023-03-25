3e62f1731250ab2c0252cc3ba43e1fbd48d8ac6d
rasnikolay <ras.nikolay@mail.ru>
2023-03-25 15:40:10 +0300

    #13.2 Code Review

--- rasnikolay/calc.js
+++ rasnikolay/calc.js
@@ -1,23 +1,30 @@
 const ADD = 'add';
 const MULTI = 'multi';
 const SUBTRACT = 'subtract';
-const ERROR = 'This is operation undefined'
+const ERROR = 'This is operation undefined';
 
-function getOperator(operation, a, b) {
-    switch (operation) {
-        case ADD:
-            return a + b;
-        case MULTI:
-            return a * b;
-        case SUBTRACT:
-            return a - b;
-        default:
-            return ERROR;
+function getOperator(operation, firstNumber, secondNumber) {
+
+    let checkNumber = (typeof firstNumber === 'number') && (typeof secondNumber === 'number');
+
+    if (checkNumber) {
+        switch (operation) {
+            case ADD:
+                return firstNumber + secondNumber;
+            case MULTI:
+                return firstNumber * secondNumber;
+            case SUBTRACT:
+                return firstNumber - secondNumber;
+            default:
+                return ERROR;
+        }
+    } else {
+        return 'This is not number';
     }
 }
 
-function calc(operator, a, b) {
-    let result = getOperator(operator, a, b);
+function calc(operator, firstNumber, secondNumber) {
+    let result = getOperator(operator, firstNumber, secondNumber);
     console.log(result);
 }
 
@@ -25,3 +32,4 @@ calc('add', 3, 3);
 calc('multi', 3, 3);
 calc('subtract', 3, 3);
 calc('error', 3, 3);
+calc('add', '3', 3);
