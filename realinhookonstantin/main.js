82f2be8f7625d574aec6a34840d97e406664d701
Константин Сукин <konkomen174@gmail.com>
2023-03-25 19:20:31 +0300

    add files

---
+++ realinhookonstantin/main.js
@@ -0,0 +1,32 @@
+"use strict"
+
+function сalc(a, b, operation) {
+  // if (operation === 'add') {
+  //   return console.log(a + b)
+  // }
+
+  // if (operation == 'multi') {
+  //   return console.log(a * b)
+  // }
+
+  // if (operation == 'subtract') {
+  //   return console.log(a - b)
+  // }
+
+  switch(operation) {
+
+    case 'add':
+    return a + b;
+
+    case 'multi':  
+    return a * b;
+
+    case 'subtract':  
+    return a - b;
+    
+    default: 
+    console.log('Strada one love!');
+  }
+}
+
+сalc(5 , 2, 'subtract');
