193a159576f216e61b6f8ceb3852fdcff8ffad5c
cloudeqv <vladvoronin428@gmail.com>
2023-03-25 14:08:29 +0500

    practice calc on switch

---
+++ weitend/calc.js
@@ -0,0 +1,19 @@
+function calc(operation, a, b) {
+    switch (operation) {
+        case 'add':
+            console.log(a + b);
+        break;
+        case 'multi':
+            console.log(a * b);
+        break;
+        case 'subtract':
+            console.log(a % b);
+        break;
+    };
+};
+
+calc('add', 1, 2)
+
+calc('multi', 1, 2)
+
+calc('subtract', 3, 2)
