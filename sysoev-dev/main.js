671dc2dc32c2796f8cb0657cfc8b8b38851d9b0d
Vadim Sysoev <sysoev.dev@gmail.com>
2023-03-26 11:55:55 +0300

    feat: Добавил проверку на число и пространство имен

--- sysoev-dev/main.js
+++ sysoev-dev/main.js
@@ -1,17 +1,34 @@
-function calc(a, b, operation) {
+const OPERATIONS = {
+  ADD: 'add',
+  MULTI: 'multi',
+  SUBSTRACT: 'substract',
+};
+
+const errorMessage = 'Error';
+
+function checkNumber(num) {
+  return typeof num === 'number' ? true : false;
+}
+
+function calc(num1, num2, operation) {
+  if (!(checkNumber(num1) && checkNumber(num2))) {
+    console.log(errorMessage);
+    return null;
+  }
   switch (operation) {
-    case 'add':
-      return a + b;
-    case 'multi':
-      return a * b;
-    case 'substract':
-      return a - b;
+    case OPERATIONS.ADD:
+      return num1 + num2;
+    case OPERATIONS.MULTI:
+      return num1 * num2;
+    case OPERATIONS.SUBSTRACT:
+      return num1 - num2;
     default:
-      return 'Error';
+      console.log(errorMessage);
+      return null;
   }
 }
 
-console.log(calc(5, 6, 'add'));
-console.log(calc(5, 5, 'multi'));
-console.log(calc(25, 10, 'substract'));
+console.log(calc(5, 6, OPERATIONS.ADD));
+console.log(calc(5, 5, OPERATIONS.MULTI));
+console.log(calc(25, 10, OPERATIONS.SUBSTRACT));
 console.log(calc(5, 5, 'plus'));
