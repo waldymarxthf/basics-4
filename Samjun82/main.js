1bf4637723f92f007b1732240f4b78da7c944bb6
Samj <samjun82gmail.com>
2023-03-26 23:12:48 +0300

    upgrade calc

--- Samjun82/main.js
+++ Samjun82/main.js
@@ -1,15 +1,14 @@
 function Calc(a, b, operation) {
-switch (operation) {
-    case 'add':
-        return a + b;
-    case 'multi':
-        return a * b;
-    case 'subtract':
-        return a - b;
+    if (operation === 'add' && typeof a !== 'number') {
+        return a + b 
+    } else if (operation === 'multi') {
+        return a * b 
+    } else if (operation === 'subtract') {
+        return a - b
     }
-
-}
-
-console.log(Calc(24, 12, 'add'))
-console.log(Calc(24, 12, 'multi'))
-console.log(Calc(24, 12, 'subtract'))
+    
+    }
+    
+    console.log(Calc(f, 12, 'add'))
+    console.log(Calc(24, 12, 'multi'))
+    console.log(Calc(24, 12, 'subtract'))
