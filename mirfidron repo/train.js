df18d55370b55a51c3249eb2983bf4825f967fd6
mirfidron <mirfidron@mail.ru>
2023-03-26 18:49:55 +0300

    v1.3

---
+++ mirfidron repo/train.js
@@ -0,0 +1,16 @@
+function calc (operation, a, b) {
+    switch (operation) {
+        case "add":
+            return a+b;
+        case "multi":
+            return a*b;
+        case "subtract":
+            return a-b;
+        default:
+            return "error"
+    }
+}
+
+console.log(calc("add",1,2));
+console.log(calc("multi",1,2));
+console.log(calc("subtract",3,2));
