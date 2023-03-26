173d0dcc0a8e670b361fa4cd4642ed4c25cad3ea
Ch1lim <Ch1lim@yandex.ru>
2023-03-26 23:47:55 +1000

    2

--- Ch1lim/main.js
+++ Ch1lim/main.js
@@ -1,4 +1,10 @@
+const error = "Ошибка";
+
 function calc(operator, valueFirst, valueSecond) {
+    if (typeof(valueFirst) !== "number" || typeof(valueSecond) !== "number"  ) {
+        return error;
+    }
+    
     switch (operator) {
         case 'add':
             return +valueFirst + +valueSecond;
@@ -8,6 +14,8 @@ function calc(operator, valueFirst, valueSecond) {
             
         case 'subtract':
             return +valueFirst - +valueSecond;
+        default:
+            return error;
     }
 }
 console.log(calc("multi", 1, 2));
