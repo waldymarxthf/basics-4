70b94b291d57385be2857681de4df526ff65ec2c
Stikast <coldstikast@gmail.com>
2023-03-26 22:15:26 +0300

    Add a number check

--- calc/main.js
+++ calc/main.js
@@ -1,7 +1,12 @@
 function calc(operation, a, b) {
-  switch (operation) {
-    case "add" :
-    	return a + b;
+	a = Number(a);
+	b = Number(b);
+
+	if (isNaN(a) || isNaN(b)) {
+		return "Enter a number"
+	} else switch (operation) {
+		case "add" :
+			return a + b;
 		case "multi" :
 			return a *  b;
 		case "subtract" :
@@ -10,3 +15,10 @@ function calc(operation, a, b) {
 			return "Error"
 	}
 }
+
+console.log(calc("add", 6, 0.25))
+console.log(calc("multi", 8, "7"))
+console.log(calc("subtract", 1000, 7))
+console.log(calc("add", null, 7))
+console.log(calc("add", undefined, 7))
+console.log(calc("multi", false, 7))
