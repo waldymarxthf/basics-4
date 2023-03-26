94f6001f455e8714c1391f217729615e50abcbcd
Stikast <coldstikast@gmail.com>
2023-03-26 21:54:11 +0300

    add file

---
+++ calc/main.js
@@ -0,0 +1,12 @@
+function calc(operation, a, b) {
+  switch (operation) {
+    case "add" :
+    	return a + b;
+		case "multi" :
+			return a *  b;
+		case "subtract" :
+			return a - b;
+		default:
+			return "Error"
+	}
+}
