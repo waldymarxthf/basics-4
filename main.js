523dc41a3747f625db747bf7ea6df8dc1e1544f6
Vadim Sysoev <sysoev.dev@gmail.com>
2023-03-27 18:39:47 +0300

    Complete 15 task

---
+++ main.js
@@ -0,0 +1,15 @@
+let i = 1;
+while (i <= 19) {
+  console.log(`i: ${i}`);
+  i++;
+}
+
+let j = 1;
+do {
+  console.log(`j: ${j}`);
+  j++;
+} while (j <= 19);
+
+for (let k = 1; k <= 19; k++) {
+  console.log(`k: ${k}`);
+}
