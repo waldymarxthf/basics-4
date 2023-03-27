719e98757b811d69a7b81f9f1a56840b0959c0a8
Ilusya <18sir08@mail.ru>
2023-03-27 21:56:54 +0300

    practice-13

---
+++ ilusya/main.js
@@ -0,0 +1,18 @@
+let operation='add';
+let a=3;
+let b=2;
+
+    switch (operation) 
+    {
+        case 'add':
+         console.log(a+b);
+         return;
+        case 'multi':
+         console.log(a*b);
+         return;
+        case 'substract':
+        console.log(a-b);
+        return;
+    }
+
+
