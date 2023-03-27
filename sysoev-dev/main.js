2e1faed2ef11245a26d295aa4cad7d0b6009cf25
Vadim Sysoev <sysoev.dev@gmail.com>
2023-03-27 21:29:49 +0300

    Добавил валидацию

--- sysoev-dev/main.js
+++ sysoev-dev/main.js
@@ -4,32 +4,34 @@ const OPERATIONS = {
   SUBSTRACT: 'substract',
 };
 
-const ERROR_MESSAGE = 'Error';
+const ERROR_NUMBER_MESSAGE = 'Number Error';
+const ERROR_OPERATION_MESSAGE = 'Unknow operation';
 
-function checkNumber(num) {
-  return typeof num === 'number' ? true : false;
+function isNumber(num) {
+  return !isNaN(num);
 }
 
 function calc(num1, num2, operation) {
-  if (!(checkNumber(num1) && checkNumber(num2))) {
-    console.log(ERROR_MESSAGE);
-    return null;
+  const isValid = isNumber(num1) && isNumber(num2);
+
+  if (!isValid) {
+    return ERROR_NUMBER_MESSAGE;
   }
 
   switch (operation) {
     case OPERATIONS.ADD:
-      return num1 + num2;
+      return Number(num1) + Number(num2);
     case OPERATIONS.MULTI:
-      return num1 * num2;
+      return Number(num1) * Number(num2);
     case OPERATIONS.SUBSTRACT:
-      return num1 - num2;
+      return Number(num1) - Number(num2);
     default:
-      console.log(ERROR_MESSAGE);
-      return null;
+      return ERROR_OPERATION_MESSAGE;
   }
 }
 
-console.log(calc(5, 6, OPERATIONS.ADD));
+console.log(calc(3, '4', OPERATIONS.ADD));
 console.log(calc(5, 5, OPERATIONS.MULTI));
 console.log(calc(25, 10, OPERATIONS.SUBSTRACT));
 console.log(calc(5, 5, 'plus'));
+console.log(calc('5five', 5, 'plus'));
