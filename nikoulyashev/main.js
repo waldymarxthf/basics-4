6d8b2b17cd68c960f4851c1b8ae51a598880bc18
Nikolay Ulyashev <ulyashev.n@yandex.ru>
2023-03-27 23:42:26 +0300

    add calc function

---
+++ nikoulyashev/main.js
@@ -0,0 +1,27 @@
+const ERROR_MESSAGE = 'Ошибка'
+function calc(operation, a, b) {
+  if (typeof(a) !== 'number') {
+    return 'введите числа';
+  } else if (typeof(b) !== 'number') {
+    return 'введите числа';
+  }
+ 
+  
+  if (operation === 'add') {
+    return a + b;
+  } else if (operation === 'subtract') {
+    return a - b;
+  } else if (operation === 'multi') {
+    return a * b;
+  } else {
+    return ERROR_MESSAGE;
+  }
+}
+
+console.log(calc('add', 2, 4));
+console.log(calc('add', '2s', 4));
+console.log(calc('multi', 2, '4s'));
+console.log(calc('mdulti', 2, '4s'));
+
+
+
