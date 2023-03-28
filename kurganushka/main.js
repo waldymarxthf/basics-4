7461e155ef6416ae954a46e886fa5cc9479ba5a1
kurganushka <nataly1991@list.ru>
2023-03-28 22:39:43 +0300

    Добавлены изменения в функцию calc

--- kurganushka/main.js
+++ kurganushka/main.js
@@ -9,9 +9,13 @@ function calc(operation, a, b) {
         case "substract":
             return b - a;
             break;
+        default:
+            console.log('oops!');
+            return null;
 
     }
 }
 console.log(calc("add", 1, 2));
 console.log(calc("multi", 1, 2));
 console.log(calc("substract", 1, 2));
+console.log(calc("something", 3, 4));
