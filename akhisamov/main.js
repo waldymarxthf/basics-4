8ba4d75bdc3a409093188c143d2a82a28de16e78
Askar Khisamov <askarcheg244@gmail.com>
2023-03-28 13:35:51 +0300

    cycles

---
+++ akhisamov/main.js
@@ -0,0 +1,20 @@
+//while
+
+let i = 1;
+while (i < 20) {
+    console.log(i);
+    i++;
+}
+
+let j = 1;
+do {
+    console.log(j);
+    j++;
+} while (j < 20);
+
+
+//for
+
+for (let l = 1; l < 20; l++) {
+    console.log(l);
+}
