95455312a713c71ef3f421e89c247d98fd4edd2b
Saylar2210 <55531937+Saylar2210@users.noreply.github.com>
2023-03-28 16:08:49 +0300

    #15 Циклы

---
+++ practica-15/main.js
@@ -0,0 +1,15 @@
+// let i = 1;
+// while (i <= 19) {
+//   console.log(i);
+//   i += 1;
+// }
+
+// let i = 1;
+// do {
+//   console.log(i);
+//   i += 1;
+// } while (i <= 19);
+
+// for (i = 1; i <= 19; i += 1) {
+//   console.log(i);
+// }
