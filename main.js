9459a0ad966748af4a3b332432844858a649859d
Grigoriy116 <128392901+Grigoriy116@users.noreply.github.com>
2023-03-28 23:20:34 +0400

    Calculator

---
+++ main.js
@@ -0,0 +1,16 @@
+function cals(a, b, operation) {
+  switch (operation) {
+    case "add":
+      return a + b;
+    case "multi":
+      return a * b;
+    case "subtract":
+      return a - b;
+  }
+}
+let x = cals(1, 2, "add");
+console.log(x);
+let y = cals(1, 2, "multi");
+console.log(y);
+let z = cals(1, 2, "subtract");
+console.log(z);
