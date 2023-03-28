73ea432a5eb3fe05fdcec765d7ed02a07bea7f60
waldymarxthf <iliya.nov223@gmail.com>
2023-03-28 16:50:07 +0300

    [feat] feat: add obj phoneBook

--- waldymarxthf/script.js
+++ waldymarxthf/script.js
@@ -1,12 +1,12 @@
-for (let i = 0; i < 10; i++) {
-  // если true, пропустить оставшуюся часть тела цикла
-  if (i % 2 == 0) continue;
-  console.log(i); // 1, затем 3, 5, 7, 9
+const phoneBook = {
+	list: {
+		Iliya: +649738218,
+		Falikul: +364825197,
+		Egorik: +845136498,
+	},
+	// log() {
+	// 	console.log(this.list)
+	// }
 }
 
-for(let i = 0; i < 10; i++) {
-	if (i % 2 === 0) {
-		console.log(i)
-		i++
-	}
-}
+console.log(phoneBook.list)
