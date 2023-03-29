eb5b094a7eb0393028d73039336f501346d79782
Kirill <streetmtb@mail.ru>
2023-03-29 23:26:39 +0300

    a

--- kirillmalofeev/cycles.js
+++ kirillmalofeev/cycles.js
@@ -1,4 +1,4 @@
-for (let i = 2; i < 11; i++) {
+/*for (let i = 2; i < 11; i++) {
   if (i % 2 !== 0) continue;
   {
     console.log(i);
@@ -9,4 +9,9 @@ let i = 0;
 while (i < 3) {
   console.log(`number ${i}!`);
   i++;
-}
+}*/
+
+let num;
+do {
+  alert("Введите число, которое больше 100!");
+} while (i <= 100 && num);
--- kirillmalofeev/objects.js
+++ kirillmalofeev/objects.js
@@ -1,13 +1,12 @@
 const telegramBook = {
-  Лерочка: 89811977481,
-  Мама: 89218894595,
-  "Slava Rostovtcev": 89213221185,
-  "Ромарио ипполит": 89137664368,
+  list: {
+    Лерочка: 89811977481,
+    Мама: 89218894595,
+    "Slava Rostovtcev": 89213221185,
+    "Ромарио ипполит": 89137664368,
+  },
+  log() {
+    console.log(this.list);
+  },
 };
-console.log(telegramBook["Ромарио ипполит"]);
-console.log(telegramBook.Лерочка);
-telegramBook["Aspirine Ильич"] = 89213584466;
-telegramBook["Мама"] = 8211111111;
-delete telegramBook["Slava Rostovtcev"];
-console.log(telegramBook["Aspirine Ильич"]);
-console.log(telegramBook.Мама);
+telegramBook.log();
