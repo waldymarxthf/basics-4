a0026510006af3152f1e0c79574df416a24c7427
waldymarxthf <iliya.nov223@gmail.com>
2023-03-29 16:48:03 +0300

    [feat] feat: add some metods

--- waldymarxthf/script.js
+++ waldymarxthf/script.js
@@ -4,9 +4,25 @@ const phoneBook = {
 		Falikul: +364825197,
 		Egorik: +845136498,
 	},
-	// log() {
-	// 	console.log(this.list)
-	// }
+	log() {
+		console.log(this.list)
+	},
+	add(name, number) {
+		this.list[name] = number
+	},
+	delete(name) {
+		delete this.list[name]
+	}
+	
 }
 
-console.log(phoneBook.list)
+
+phoneBook.add('galya', 7392756481)
+phoneBook.add('galyasdaa', 73935456481)
+phoneBook.delete('Egorik')
+phoneBook.log()
+console.log('Iliya' in phoneBook.list)
+
+for(const name in phoneBook.list) {
+	console.log(`${name} - ${phoneBook.list[name]}`)
+}
