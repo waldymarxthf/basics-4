94aaf8fc33b76e2e0ba06a7a9056a562f4922791
Maria Ckopkova <mariakons@mail.ru>
2023-03-29 22:03:25 +0300

    #16 Объекты: введение

---
+++ mariackopkova/main.js
@@ -0,0 +1,13 @@
+//Создание объекта
+//let userInlo = new Object() //синтаксис "конструктор объекта"
+//let userInlo = {} //литерал объекта
+const phoneBook = {
+  "Aleks": 123456789,
+  "Mama": 7894561323,
+  "Papa": 4561237989,
+}
+let Papa = {
+  name:"Vitalii",
+  age:56,
+}
+console.log("Papa"+" " +Papa.name+" "+phoneBook["Papa"]+" " +Papa.age+" "+"age");
