cb3409d02ea5eca501080b3698f9d5bd75269677
Samozvan Stanislav <ssfrontendd@gmail.com>
2023-03-29 18:23:52 +0200

    practice-15

---
+++ Samstas/main.js
@@ -0,0 +1,43 @@
+let i = 1;
+while (i < 7) {
+    console.log(i);
+    i = i + 1;
+}
+
+console.log(i);
+
+
+for (let i = 1; i <= 19; i += 1) {
+    console.log(i)
+}
+
+for (let i = 1; i <= 5; i += 1) {
+    console.log(i)
+}
+
+do {
+    console.log(i);
+    i = i + 1;
+} while (i < 20);
+
+for (let i = 1; i <= 19; i += 1) {
+    console.log(i)
+}
+
+for (let i = 3; i <= 11; i += 1) {
+
+    console.log(i);
+}
+
+for (let i = 0; i <= 10; i += 5) {
+    console.log(i);
+}
+
+
+for (let i = 0; i <= 20; i += 5) {
+    console.log(i);
+}
+
+for (let i = -2; i <= 0; i += 1) {
+    console.log(i);
+}
