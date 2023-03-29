294d3b049d718067a3dd2d1697f54361180cee65
Olga <olgarabota222@gmail.com>
2023-03-29 23:29:30 +0300

    Create main.js
    calcsss

---
+++ origa/main.js
@@ -0,0 +1,20 @@
+function calc(operation, a, b) {
+    switch (operation) {
+    case "add":
+        return a + b
+    
+    case "multi":
+        return a * b
+    
+    case "subtract":
+        return a - b
+    }
+}
+
+
+
+alert(calc("add", 1, 2))
+
+alert(calc("multi", 1, 2))
+
+alert(calc("subtract", 3, 2))
