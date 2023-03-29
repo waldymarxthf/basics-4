c5c62c751ea4807bd10e647d3b11caf915e2c137
waldymarxthf <iliya.nov223@gmail.com>
2023-03-30 00:05:32 +0300

    [refactor] refactor: refact 2 metods

--- waldymarxthf/script.js
+++ waldymarxthf/script.js
@@ -8,21 +8,31 @@ const phoneBook = {
 		console.log(this.list)
 	},
 	add(name, number) {
-		this.list[name] = number
+		if (name === this.list[name]) {
+			console.log('Этот человек есть уже в записной книге \n')
+		} else {
+			this.list[name] = number
+			console.log(`Человека с именем ${name} добавил \n`)
+		}
 	},
 	delete(name) {
-		delete this.list[name]
+		if (!(name === this.list[name])) {
+			delete this.list[name]
+			console.log(`Человек с именем ${name} удален \n` ) 
+		} else {
+			console.log(`Человек с именем ${name} не существует \n`)
+		}
+	},
+	showContact() {
+		console.log('Contacts:')
+		for(const name in phoneBook.list) {
+			console.log(`\t ${name} - ${phoneBook.list[name]}`)
+		}
 	}
-	
 }
 
 
 phoneBook.add('galya', 7392756481)
 phoneBook.add('galyasdaa', 73935456481)
 phoneBook.delete('Egorik')
-phoneBook.log()
-console.log('Iliya' in phoneBook.list)
-
-for(const name in phoneBook.list) {
-	console.log(`${name} - ${phoneBook.list[name]}`)
-}
+phoneBook.showContact()
