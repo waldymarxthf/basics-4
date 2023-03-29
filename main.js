3f26c2dc342371fb0120e24ebf8ced7c67e35946
DariaYukseeva <ducha2005@mail.ru>
2023-03-29 13:05:47 +0500

    add main.js

---
+++ main.js
@@ -0,0 +1,45 @@
+const contacts = {
+    'Alyona': '@aaaaa',
+    'Olga': '@oooooo',
+    'beloved son': '@mmmmm',
+}
+
+console.log(contacts);
+console.log(contacts.Olga);
+console.log(contacts['beloved son']);
+
+contacts['dad'] = '@ddddddddd';
+console.log(contacts);
+
+contacts.Olga = '@ooooo1';
+console.log(contacts.Olga);
+
+contacts['beloved son'] = '@max';
+console.log(contacts['beloved son']);
+
+contacts.mom = '@mother';
+console.log(contacts);
+
+delete contacts.Alyona;
+console.log(contacts);
+
+contacts.log = function () {
+    console.log(this.dad);
+}
+console.log(contacts.log());
+
+contacts.add = function(name, nickname) {
+    this[name] = nickname;
+}
+contacts.remove = function(name) {
+    delete this[name];
+}
+
+delete contacts.log;
+console.log(contacts);
+
+contacts.add('brother', '@ssssssssss');
+console.log(contacts);
+
+contacts.remove('Olga');
+console.log(contacts);
