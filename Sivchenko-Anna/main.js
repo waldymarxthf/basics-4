58957b381fdaf7b0f08398e62d75634fd9ce062d
Sivchenko-Anna <anya.malakhova.99@mail.ru>
2023-03-29 11:58:03 +1000

    add the ability to delete and add a contact

---
+++ Sivchenko-Anna/main.js
@@ -0,0 +1,21 @@
+const phoneBook = {
+  list: {
+    'Ilya': 787,
+    'Liza': 5533322,
+    'Sasha': 778899,
+  },
+  showList() {
+    console.log(this.list);
+  },
+  addContact(name, number) {
+    this.list[name] = number;
+  },
+  deleteContact(name) {
+    delete this.list[name];
+  }
+};
+
+phoneBook.addContact("Misha", 88991122);
+phoneBook.deleteContact("Sasha");
+
+console.log(phoneBook);
