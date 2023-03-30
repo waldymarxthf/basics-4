33bab7ab541c3d4802129fc2157a420e3e7effe7
Anastasia <tgmonresu@gmail.com>
2023-03-30 16:53:31 +0300

    validation

--- monresu/main.js
+++ monresu/main.js
@@ -1,21 +1,45 @@
+const pattern = /^\+\d+$/;
 const phoneBook = {
-    list: {
-        "Strada": 88005553535,
-        "Илья": 88003335555,
-        "Ирина": 89002225656,
-        "Мать": 9000192938,
-        "Лукашенко Александр": 3456789,
-        "Путин Владимир": 1234567890,
-    },
-    add(name, number) {
-        this.list[name] = number; 
+	list: {
+		Iliya: '+649738218',
+		Falikul: '+364825197',
+		Egorik: '+845136498',
+	},
+	log() {
+		console.log(this.list)
+	},
+	add(name, number) {
+    if (pattern.test(number)) {
+      if (name in this.list || Object.values(phoneBook.list).includes(number)) {
+        console.log('Этот человек есть уже в записной книге \n')
+      } else {
+        this.list[name] = number
+        console.log(`Человека с именем ${name} добавлен \n`)
+      }
     }
-};
-
-let whoIAm = 'Jesus';
-phoneBook.add(whoIAm, 666666666);
-
-for (const name in phoneBook.list) {
-    console.log(name);
-    console.log(phoneBook.list[name]);
+    else {
+      console.log('Неверный формат номера', name, number)
+    }
+	},
+	delete(name) {
+		if (!(name === this.list[name])) {
+			delete this.list[name]
+			console.log(`Человек с именем ${name} удален \n` ) 
+		} else {
+			console.log(`Человек с именем ${name} не существует \n`)
+		}
+	},
+	showContact() {
+		console.log('Contacts:')
+		for(const name in phoneBook.list) {
+			console.log(`\t ${name} - ${phoneBook.list[name]}`)
+		}
+	}
 }
+
+
+phoneBook.add('Iliyusha', '+649738218') //тут ошибка что уже есть такой
+phoneBook.add('galyasdaa', '+73935456481')
+phoneBook.add('фв', '73935456481') // тут ошибка что неверный формат телефона
+phoneBook.delete('Egorik')
+phoneBook.showContact()
