60ba78b244127571d4da0f7a199608ba6f08f785
waldymarxthf <iliya.nov223@gmail.com>
2023-03-30 17:23:40 +0300

    [feat] feat: add regular expressions for phone

--- waldymarxthf/script.js
+++ waldymarxthf/script.js
@@ -1,19 +1,26 @@
+const mobilePhoneRegexp = /^\+375 \((17|25|29|33|44)\) \d{3}-\d{2}-\d{2}$/;
+
 const phoneBook = {
 	list: {
-		Iliya: '+649738218',
-		Falikul: '+364825197',
-		Egorik: '+845136498',
+		Iliya: '+375 (29) 316-09-34',
+		Falikul: '+375 (25) 289-20-12',
+		Galya: '+375 (33) 344-34-37',
+		Egorik: '+375 (17) 129-78-02',
 	},
 	log() {
 		console.log(this.list)
 	},
 	add(name, number) {
-		if (name in this.list || Object.values(this.list).includes(number)) {
-			console.log('Этот человек есть уже в записной книге\n')
+		if(mobilePhoneRegexp.test(number)) {
+			if (name in this.list || Object.values(this.list).includes(number)) {
+				console.log('Этот человек есть уже в записной книге\n')
+			} else {
+				this.list[name] = number
+				console.log(`Человек с именем ${name} добавлен\n`)
+			}
 		} else {
-			this.list[name] = number
-			console.log(`Человека с именем ${name} добавлен\n`)
-		}
+      console.log('Неверный формат номера', name, number, '\n')
+    }
 	},
 	delete(name) {
 		if (!(name === this.list[name])) {
@@ -31,8 +38,7 @@ const phoneBook = {
 	}
 }
 
-
-phoneBook.add('Iliyusha', '+649738218')
+phoneBook.add('Iliyusha', '+375 (29) 137-92-18')
 phoneBook.add('galyasdaa', '+73935456481')
 phoneBook.delete('Egorik')
 phoneBook.showContact()
