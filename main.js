077cb654986050d5d6461a7e5587ddd92b09f9fe
Vitalii Miagkov <maposia@gmail.com>
2023-03-30 18:49:51 +0700

    Adress book object with methods

---
+++ main.js
@@ -0,0 +1,39 @@
+const replacingNum = false //Управляет возможность замены номера, даже если такой уже существует
+
+const phoneBook = {
+  list: {
+    John: 79118851351,
+    Maria: 79245542334,
+    Kate: 79110552233,
+    Mike: 79250066111,
+    Sam: 79220112245,
+  },
+
+  addContact(newContact, newNumber) {
+    if (newContact in this.list) {
+      console.log('Такой контакт уже есть')
+      if (replacingNum) {
+        console.log('Авозамена включена. Меняем номер на новый')
+        phoneBook.list[newContact] = newNumber
+      }
+      return
+    } else {
+      this.list[newContact] = newNumber
+    }
+  },
+  editContact(name, newNumber) {
+    for (key in phoneBook.list) {
+      if (key === name) {
+        this.list[name] = newNumber
+      }
+    }
+  },
+}
+
+phoneBook.addContact('John', 333)
+
+phoneBook.editContact('Sam', 79995551111)
+
+for (const name in phoneBook.list) {
+  console.log(name + ' - ' + phoneBook.list[name])
+}
