64c962527e9f9234c6a9934fdcfc356f7c0b0e7d
Maria Ckopkova <mariakons@mail.ru>
2023-03-30 20:33:01 +0300

    #17 Объекты: подробнее

--- mariackopkova/main.js
+++ mariackopkova/main.js
@@ -1,13 +1,28 @@
-//Создание объекта
-//let userInlo = new Object() //синтаксис "конструктор объекта"
-//let userInlo = {} //литерал объекта
-const phoneBook = {
-  "Aleks": 123456789,
-  "Mama": 7894561323,
-  "Papa": 4561237989,
-}
-let Papa = {
-  name:"Vitalii",
-  age:56,
-}
-console.log("Papa"+" " +Papa.name+" "+phoneBook["Papa"]+" " +Papa.age+" "+"age");
+/*const phoneBook = {
+	list: {
+	  "John": 123456789,
+	  "Jane Doe": 987654321,
+	  "Jim Smith": 111111111
+	},
+	add(name, number) { // добавили метод add с параметрами name и number
+	  this.list[name] = number; // добавили number в свойство [name] свойства list
+	}
+  };*/
+  const phoneBook = {
+	list:{
+	"Aleks": 123456789,
+	"Mama": 7894561323,
+	"Papa": 4561237989,
+	"Brother":159753456,
+	"Sister":357159456
+    },
+	add(name, number){
+		this.list[name] = number;
+	},
+	delete(name){
+		delete this.list[name]
+	},
+  }
+  phoneBook.delete("Aleks");
+  phoneBook.add("Babyshka",951357456)
+  console.log(phoneBook.list);
