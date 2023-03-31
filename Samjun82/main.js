afe72bb939dce146e9a6b3189448e55f502381d7
Samj <samjun82gmail.com>
2023-03-31 14:03:33 +0300

    new practice

--- Samjun82/main.js
+++ Samjun82/main.js
@@ -1,11 +1,27 @@
-console.log(5 > 4) // true
-console.log("жигули" > "порше") //false
-console.log("2" > "12") //true
-console.log(undefined == null) //true
-console.log(undefined === null) //false
-console.log(null == "\n0\n") //false
-console.log(null === +"\n0\n") //false
-console.log(null > null) //false
-console.log(null > 0);  // (1) false
-console.log(null == 0); // (2) false
-console.log(null >= 0); // (3) true
+let a = 4;
+let b = 7;
+console.log(a<b);
+let c = 4;
+let d = 7;
+console.log(c>d);
+console.log(c==a);
+console.log(c===a);
+console.log(c==d);
+console.log(c===d);
+console.log(c!=d);
+let e = undefined;
+let f = null;
+console.log(e===f);
+console.log(e==f);
+console.log(Boolean(e) == f)
+let g = NaN;
+console.log(g==f)
+console.log(c==g)
+console.log(e<f)
+console.log(e>f)
+let h = 'a';
+let i = 'b';
+console.log(h < i);
+console.log(a > h);
+console.log(451 === 1105)
+console.log(3>2>1)
