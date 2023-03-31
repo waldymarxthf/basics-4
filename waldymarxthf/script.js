7448c4d034109776b93947f9cd947363fa87306d
waldymarxthf <iliya.nov223@gmail.com>
2023-03-31 17:01:49 +0300

    [chore]chore: add validation for deleteTask, changeStatus

--- waldymarxthf/script.js
+++ waldymarxthf/script.js
@@ -1,3 +1,7 @@
+const TODO = 'To Do'
+const IN_PROGRESS = "In Progress"
+const DONE = 'Done'
+
 const list = {
 	"create a new practice task": "In Progress", 
 	"make a bed": "Done",
@@ -5,17 +9,29 @@ const list = {
 }
 
 function changeStatus(task, newCondition) {
+	if (!(task in list)) {
+		console.log(`Задача '${task}' не найдена 🚫\n`)
+		return
+	}
+	for (const key in list) {
+    if (list[key] === newCondition && key !== task) {
+      console.log(`Свойство '${newCondition}' уже используется для задачи '${key}' ⚠\n`);
+      break;
+    }
+  }
 	list[task] = newCondition
 }
 
+
 function addTask(newTask) {
-	const condition = 'To Do'
-	list[newTask] = condition
+	list[newTask] = TODO
 }
 
 function deleteTask(task) {
 	if(task in list) {
 		delete list[task]
+	} else {
+		console.log(`Задачи '${task}' не существует 🚫\n`)
 	}
 }
 
@@ -23,7 +39,7 @@ function showList() {
 	console.log('Todo:')
 	let hasToDo = false
 	for (const task in list) {
-		if (list[task] === 'To Do') {
+		if (list[task] === TODO) {
 			console.log(`\t${task}`)
 			hasToDo = true
 		}
@@ -35,7 +51,7 @@ function showList() {
 	console.log('In Progress:')
 	let hasInProgress = false
 	for (const task in list) {
-		if (list[task] === 'In Progress') {
+		if (list[task] === IN_PROGRESS) {
 			console.log(`\t${task}`)
 			hasInProgress = true
 		}
@@ -47,7 +63,7 @@ function showList() {
 	console.log('Done:')
 	let hasDone = false
 	for (const task in list) {
-		if (list[task] === 'Done') {
+		if (list[task] === DONE) {
 			console.log(`\t${task}`)
 			hasDone = true
 		}
@@ -55,11 +71,13 @@ function showList() {
 	if (!hasDone) {
 		console.log('\t-')
 	}
+	return null
 }
 
 
-changeStatus("create a new practice task", "To Do")
+changeStatus("create a new practice task", "In Progress")
 changeStatus("make a bed", "In Progress")
 addTask('hello my name')
-// deleteTask("make a bed")
+deleteTask("make a bedв")
 showList()
+// console.log(list)
