99121324a6ffdece1449ad3acbadf699613455ea
Samozvan Stanislav <ssfrontendd@gmail.com>
2023-03-31 02:36:39 +0200

    practice-18

--- Samstas/main.js
+++ Samstas/main.js
@@ -1,1 +1,19 @@
-console.log('Practice #18');
+let user = {
+  name: "John Doe",
+};
+
+let object1 = {
+  age: 20,
+};
+
+let object2 = {
+  height: 180,
+};
+
+Object.assign(user, object1, object2);
+
+console.log(user);
+
+let newUser = user;
+
+console.log(newUser);
