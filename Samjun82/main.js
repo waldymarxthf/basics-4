4c5710a0e139923cc8c906b4832b5cce081eb867
Samj <samjun82gmail.com>
2023-03-31 14:42:50 +0300

    new practice

--- Samjun82/main.js
+++ Samjun82/main.js
@@ -1,27 +1,8 @@
-let a = 4;
-let b = 7;
-console.log(a<b);
-let c = 4;
-let d = 7;
-console.log(c>d);
-console.log(c==a);
-console.log(c===a);
-console.log(c==d);
-console.log(c===d);
-console.log(c!=d);
-let e = undefined;
-let f = null;
-console.log(e===f);
-console.log(e==f);
-console.log(Boolean(e) == f)
-let g = NaN;
-console.log(g==f)
-console.log(c==g)
-console.log(e<f)
-console.log(e>f)
-let h = 'a';
-let i = 'b';
-console.log(h < i);
-console.log(a > h);
-console.log(451 === 1105)
-console.log(3>2>1)
+let message = (login == 'Сотрудник') ? 'Привет':
+    (login == 'Директор') ? 'Здравствуйте':
+    (login == '') ? 'Кланяйся в ноги':
+    ""; 
+    
+
+let result = (a + b < 4) ? 'Мало' : 'Много';
+
