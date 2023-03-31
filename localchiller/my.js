36875193357453087838e4960b4d58aac6a1f230
Panamanapape <localchiller@gmail.com>
2023-03-31 20:08:37 +0300

    new commit

---
+++ localchiller/my.js
@@ -0,0 +1,17 @@
+function calc(a, b) {
+    switch (a + b) {
+        case 2 : console.log('Тепло');
+            break;
+        case 3 : console.log('Теплее');
+            break;
+        case 4 : console.log('Попал');
+            break;
+        default :
+            console.log('goddamn')
+
+        
+    }
+
+};
+
+calc(1, 10);
