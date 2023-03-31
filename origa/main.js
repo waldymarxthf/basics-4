da5abc6a84b93477cfb33c7c45a560997c678363
Olga <olgarabota222@gmail.com>
2023-03-31 07:42:55 +0300

    for/while
    for/while

--- origa/main.js
+++ origa/main.js
@@ -1,1 +1,14 @@
-console.log("I hate Git")
+let i=0
+while (i<19) {
+    i = i + 1
+    console.log(i)
+    
+}
+
+console.log(i)
+
+// do  {
+//     i = i + 1;
+//     console.log(i)
+    
+// } while (i<19)
