7f72f6028cf2e782f192ac78071cdd5865ca45a7
Samj <samjun82gmail.com>
2023-03-31 15:02:08 +0300

    new practice

--- Samjun82/main.js
+++ Samjun82/main.js
@@ -1,24 +1,15 @@
-const error = "Please enter number"
-function сalc(num1, num2, operation) {
-if (operation === 'add' && typeof num1 === 'number' && typeof num2 === 'number') {
-    return num1 + num2 
-} else if (operation === 'multi' && typeof num1 === 'number' && typeof num2 === 'number') {
-    return num1 * num2 
-} else if (operation === 'subtract' && typeof num1 === 'number' && typeof num2 === 'number') {
-    return num1 - num2
-} else {
-    return error
-}
+function Calc(a, b, operation) {
+switch (operation) {
+    case 'add':
+        return a + b;
+    case 'multi':
+        return a * b;
+    case 'subtract':
+        return a - b;
+    }
 
 }
 
-console.log(сalc(12, 12, 'add'))
-console.log(сalc('24', 12, 'multi'))
-console.log(сalc(24, false, 'subtract'))
-
-// let num1 = 'numb';
-// if(typeof num1 === 'number') {
-//     console.log('ok')
-// } else {
-//     console.log('please enter number')
-// }
+console.log(Calc(24, 12, 'add'))
+console.log(Calc(24, 12, 'multi'))
+console.log(Calc(24, 12, 'subtract'))
