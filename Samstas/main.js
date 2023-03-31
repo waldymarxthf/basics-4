a8d3e5e024d4b5da3bfb322e775cd67d84033a15
Samozvan Stanislav <ssfrontendd@gmail.com>
2023-03-31 17:39:32 +0200

    practice-18

--- Samstas/main.js
+++ Samstas/main.js
@@ -1,3 +1,4 @@
+// 3 objects in one
 let user = {
   name: "John Doe",
 };
@@ -14,6 +15,62 @@ Object.assign(user, object1, object2);
 
 console.log(user);
 
+//copy link in a new variable
 let newUser = user;
 
 console.log(newUser);
+
+//copy link of object with three dots
+//first way
+const book = {
+  pages: 100,
+  tiitle: "American boy",
+};
+
+const book2 = { ...book };
+book2.pages = 300;
+
+console.log(book);
+console.log(book2);
+
+//second way
+const phone = {
+  number: 77777,
+  location: "Chicago, USA",
+};
+
+const phone2 = {
+  ...phone,
+  model: "Apple",
+};
+
+console.log(phone2);
+
+//deep copy of object
+const person = {
+  name: "Bob",
+  age: 22,
+  location: {
+    country: "USA",
+    city: "Miami",
+  },
+};
+
+function deepCopy(obj) {
+  if (typeof obj !== "object" || obj === null) {
+    return obj;
+  }
+
+  const copy = {};
+  for (let key in obj) {
+    copy[key] = deepCopy(obj[key]);
+  }
+  return copy;
+}
+
+const personCopy = deepCopy(person);
+
+personCopy.location.city = 'Boston'
+
+console.log(person)
+console.log(personCopy);
