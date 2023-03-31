3ea16f578494cdf6385cd4f3bcfe32c2a666e374
Samj <samjun82gmail.com>
2023-03-31 14:58:07 +0300

    new practice

--- Samjun82/main.js
+++ Samjun82/main.js
@@ -1,15 +1,13 @@
-let question = "ECMAScript";
-if(question === "ECMAScript") {
-    console.log("You right")
-} else {
-    console.log("no. correct answer - EMCAScript")
-}
-
-let message = (login == 'Сотрудник') ? 'Привет':
-    (login == 'Директор') ? 'Здравствуйте':
-    (login == '') ? 'Кланяйся в ноги':
-    ""; 
+function chekAge(age) {
+    if(age < 18) {
+     console.log("you are not allowed")
+    } else if(age >= 18 && age <=99) {
+     console.log("you are welcome!")
+    } else if(age >= 100 && age <=120) {
+     console.log("are you still breathing?")
+    } else {
+     console.log("are you human?")
+    }
     
-
-let result = (a + b < 4) ? 'Мало' : 'Много';
-
+    }
+    chekAge(200)
