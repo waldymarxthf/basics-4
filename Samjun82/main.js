d29da39b49da124cca88e9f45d9f0f67a49b2b67
Samj <83956238+Samjun82@users.noreply.github.com>
2023-03-31 09:17:59 +0300

    Update main.js

--- Samjun82/main.js
+++ Samjun82/main.js
@@ -1,3 +1,31 @@
-console.log('Hi everybody')
-console.log(1+5-4)
-console.log(125+643 + ' сумма петиций '+ 65+35)
+let userName = 'Serh';
+let userAge = 40;
+console.log(userName + ' is ' + userAge + ' years old');
+
+let userHeight; // не присвоенное знаяение 
+
+console.log(userHeight); // undefined
+userHeight = 170; // Number
+console.log(userHeight); // 170
+
+
+console.log(`18 + 82 = ${18+82}`) 
+
+let user = null; // null - определенное нулевое значение, undefined - неопределенное. 
+
+let nameFieldChecked = true; // boolean
+let ageFieldChecked = false; 
+
+let a = 65;
+let b = a;
+let c = b;
+console.log(c);
+
+const myBirthday = '18.04.1982';
+// myBirthday = '01.01.2001'; // ошибка, константу нельзя перезаписать!
+
+const COLOR_ORANGE = "#FF7F00";
+let color = COLOR_ORANGE; 
+console.log(color); 
+
+console.log(c + myBirthday + b + color + 45 + '6' + nameFieldChecked)
