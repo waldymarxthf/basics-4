dd765ceb7a86598593aa4be7391a6051d6728d91
Samozvan Stanislav <ssfrontendd@gmail.com>
2023-04-01 00:32:53 +0200

    practice-19

--- Samstas/main.js
+++ Samstas/main.js
@@ -1,1 +1,33 @@
-const practice = 19
+const practice = 19;
+
+const toDoList = {
+  list: {
+    "Buy food": "To Do",
+    "Read a book": "In Progress",
+    "Meditation 10min": "Done",
+    "Workout 30min": "Done",
+  },
+
+  addTask(task, status) {
+    this.list[task] = status;
+  },
+
+  deleteTask(task) {
+    delete this.list[task];
+  },
+
+  changeStatus(task, status) {
+    this.list[task] = status;
+  },
+
+    showList() {
+      for (let task in toDoList.list) {
+        console.log(`${task}: ${toDoList.list[task]}`);
+      }
+    },
+};
+
+// toDoList.deleteTask("buy food");
+// toDoList.addTask("make a bed");
+// toDoList.changeStatus('Buy food', 'Done');
+toDoList.showList();
