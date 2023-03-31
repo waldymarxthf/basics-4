eff573df562016812c35e380ad445ef476eee289
Samj <samjun82gmail.com>
2023-03-31 14:49:41 +0300

    changed

--- Samjun82/main.js
+++ Samjun82/main.js
@@ -1,3 +1,10 @@
+let question = "ECMAScript";
+if(question === "ECMAScript") {
+    console.log("You right")
+} else {
+    console.log("no. correct answer - EMCAScript")
+}
+
 let message = (login == 'Сотрудник') ? 'Привет':
     (login == 'Директор') ? 'Здравствуйте':
     (login == '') ? 'Кланяйся в ноги':
