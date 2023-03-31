6921bab288cb7e5c161fcd3289c13300060815be
Samj <samjun82gmail.com>
2023-03-31 13:59:13 +0300

    new practice

--- Samjun82/main.js
+++ Samjun82/main.js
@@ -1,27 +1,11 @@
-let name = 'Pavel';
-console.log (Number(name), Boolean(name), String(name));
-
-let age = 20;
-console.log (Number(age), Boolean(age), String(age));
-
-let drivingLicense = false;
-console.log (Number(drivingLicense), Boolean(drivingLicense), String(drivingLicense));
-
-let car = null;
-console.log (Number(car), Boolean(car), String(car));
-
-let wife = undefined;
-console.log (Number(wife), Boolean(wife), String(wife));
-
-let money = 10n;
-console.log (Number(money), Boolean(money), String(money));
-
-let homeAdress = {
-    street: 'moskovskaya',
-    numberHouse: 56,
-    flat: 32
-};
-console.log (Number(homeAdress), Boolean(homeAdress), String(homeAdress)); 
-
-let id = symbol('id');
-console.log (Number(id), Boolean(id), String(id));
+console.log(5 > 4) // true
+console.log("жигули" > "порше") //false
+console.log("2" > "12") //true
+console.log(undefined == null) //true
+console.log(undefined === null) //false
+console.log(null == "\n0\n") //false
+console.log(null === +"\n0\n") //false
+console.log(null > null) //false
+console.log(null > 0);  // (1) false
+console.log(null == 0); // (2) false
+console.log(null >= 0); // (3) true
