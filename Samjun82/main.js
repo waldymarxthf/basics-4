dc70c62154f5c7125830a1ea795b0b27d614a22d
Samj <samjun82gmail.com>
2023-04-01 09:07:49 +0300

    new practice

--- Samjun82/main.js
+++ Samjun82/main.js
@@ -1,34 +1,37 @@
-const phoneBook = {
-    list: {
+const sim1 = {
       "Анастасия Резникова": 74155434543,
       "Петр Голованин": 74876543213,
       "Сергей Усс": 7487894363,
-      "Илья Прокофьев": 74876576217,
-    },
-    add(name, number) {
-          this.list[name] = number;          
-    },
-    delete(name) {
-      delete this.list[name]
-    },
-    log() {
-      console.log(this.list)
-    },
-    getContact() {
-      for(const name in this.list) {
-        console.log(name + ' - ' + this.list[name])
-       
-      }
-    }
-  };
-  
-  phoneBook.getContact()
- 
-  phoneBook.add("Joana Frost", 89994456633)
-  console.log(phoneBook.list['Joana Frost'])
-  phoneBook.delete("Сергей Усс")
-  console.log("Сергей Усс" in phoneBook.list);
+      "Илья Прокофьев": 74876576217
+}
 
+const phoneBook = {
+    "Анастасия Резникова": 7465534543,
+    "Петр Голованин": 74876543213,
+    "Сергей Усс": 7487894363,
+    "Илья Прокофьев": 74876576217,
+    "Василий Пупкин": 74876576453,
+    "Андрей Петров": 74876576865,
+    "Денис Сафонов": 74876576143,
+    "Илья Резников": 74876576778,
+}
 
-phoneBook.add("Анастасия Резникова", 84435556436)
-phoneBook.getContact()
+const cloudContacts = {
+    "Сергей Антонов": 7487894231,
+    "Илья Понамарев": 74856776217,
+    "Василий Шишкин": 74856646453,
+    "Андрей Афанасьев": 74873546865,
+    "Денис Верещягин": 74876546143,
+    "Илья Кива": 74875677778,
+}
+
+const all = {}
+for(const key in phoneBook) {
+  all[key] = phoneBook[key]
+}
+console.log(all)
+
+Object.assign(sim1, phoneBook, cloudContacts);
+
+console.log(sim1)
+
