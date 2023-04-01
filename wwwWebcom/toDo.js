f96f078547ad2eee2d606569f7eda824da0753af
wwwWebcom <dim.golowcko2014@yandex.ru>
2023-04-02 00:47:51 +0300

    add task - #19 ToDoList

---
+++ wwwWebcom/toDo.js
@@ -0,0 +1,68 @@
+const toDoList = {
+  tasks: {
+    "wake up": "ToDo",
+    "brush teeth": "ToDo",
+  },
+  changeStatus(task, status) {
+    let lowStatus = status.toLowerCase();
+    if (
+      task in this.tasks &&
+      (lowStatus === "todo" ||
+        lowStatus === "done" ||
+        lowStatus === "in progress")
+    ) {
+      this.tasks[task] = status;
+      console.log('Статус изменен')
+    } else {
+      console.log("Такой задачи/такого статуса не существует");
+    }
+  },
+  addTask(task) {
+    if (!(task in this.tasks)) {
+      this.tasks[task] = "ToDo";
+      console.log('Задача добавлена')
+    } else {
+      console.log("Такая задача уже есть!");
+    }
+  },
+  deleteTask(task) {
+    if (task in this.tasks) {
+      delete this.tasks[task];
+      console.log('Задача удалена')
+    } else {
+      console.log("Такой задачи нет в списке!");
+    }
+  },
+  checkStatus(status) {
+    let checkTask = "";
+    console.log(`${status}:`);
+    for (let task in this.tasks) {
+      if (this.tasks[task].toLowerCase() === status.toLowerCase()) {
+        console.log("\t", task);
+        checkTask = "!";
+      }
+    }
+    if (checkTask === "") console.log("\t -");
+  },
+  showTasks() {
+    this.checkStatus("ToDo");
+    this.checkStatus("In Progress");
+    this.checkStatus("Done");
+  },
+}
+
+// Добавление задачи
+toDoList.addTask("learn English");
+toDoList.addTask("learn Programming");
+toDoList.addTask("go to sleep")
+
+// Изменение статуса
+toDoList.changeStatus("wake up", "Done");
+toDoList.changeStatus("learn English", "Done");
+toDoList.changeStatus("go to sleep", 'Done')
+
+// Удаление задачи
+toDoList.deleteTask("brush teeth");
+
+//Показать текущие задачи
+toDoList.showTasks();
