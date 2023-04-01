4717945490eb694f006a7d06d36b2ec960dee5de
Folze <thefolze@gmail.com>
2023-04-01 10:36:27 +0500

    add new folder

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
-console.log('zzz')
+function сalc(a, b, operation) {
+    if (operation === "add") {
+        return a + b
+    } else if (operation === "multi") {
+        return a * b
+    } else if (operation === "subtract") {
+        return a - b
+    } else return "Что-то не так"
+}
+
+// switch  
+
+function calc(a, b, operation) {
+    switch (operation) {
+        case "add":
+            return a + b;
+        case "multi":
+            return a * b;
+        case "subtract":
+            return a - b;
+        default:
+            return "hzhz"
+    }
+}
