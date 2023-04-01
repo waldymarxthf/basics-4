2c50780b60c1059bb9d07a12ab7da952c88722f7
mirfidron <mirfidron@mail.ru>
2023-04-01 13:39:35 +0300

    ToDo ~ 19

--- mirfidron repo/train.js
+++ mirfidron repo/train.js
@@ -1,16 +1,47 @@
-function calc (operation, a, b) {
-    switch (operation) {
-        case "add":
-            return a+b;
-        case "multi":
-            return a*b;
-        case "subtract":
-            return a-b;
-        default:
-            return "error"
+const Status = {
+    Done: "Done",
+    InProgress: "In Progress",
+	Todo: "To Do",
+
+};
+
+const ToDo ={
+    list : {
+        "create a new practice task": "In Progress", 
+        "make a bed": "Done", // задача "заправить кровать" в статусе "Готово"
+        "write a post": "To Do",
+    },
+    addTask(task, status) {
+        this.list[task] = status;
+    },
+	changeStatus(task, status){
+        this.list[task] = status;
+    },
+    deleteTask(task) {
+        delete this.list[task];
+    },
+    showList() {
+        console.log('To Do:')
+        for (task in ToDo.list ) {
+            if(ToDo.list[task] === Status.Todo){console.log(' '+ task)};
+        };
+        console.log('Done:')
+        for (task in ToDo.list) {
+            if(ToDo.list[task] === Status.Done){console.log(' '+ task)}
+        };
+        console.log('In Progress:')
+        for(task in ToDo.list) {
+            if(ToDo.list[task] === Status.InProgress){console.log(' ' + task)}
+        }
+
     }
 }
 
-console.log(calc("add",1,2)); // 3
-console.log(calc("multi",1,2)); // 2
-console.log(calc("subtract",3,2)); // 1
+ToDo.addTask("awake", "To Do");
+ToDo.addTask("wash up", "To Do");
+ToDo.addTask("read the book", "Done");
+ToDo.addTask("sleep", "Done");
+ToDo.changeStatus("wash up", "In Progress");
+ToDo.changeStatus("make a bed", "To Do")
+ToDo.deleteTask("sleep");
+ToDo.showList();
