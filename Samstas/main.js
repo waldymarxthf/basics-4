90b78d584acaef409dac964aca6a72d9f7702999
Stas <121170610+Samstas@users.noreply.github.com>
2023-04-02 18:50:08 +0200

    Update main.js

--- Samstas/main.js
+++ Samstas/main.js
@@ -1,1 +1,14 @@
-//practice #20
+
+// arrow functions
+common way
+function sum(num, num2) {
+  return num + num2;
+}
+
+console.log(sum(70, 8));
+
+//arrow function way
+const sum2 = (num, num2) => num + num2;
+
+console.log(sum2(11, 2));
+
