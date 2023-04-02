56f4c0e86bba595db8602025303e24c737788ed8
Tropic <deputat1120@gmail.com>
2023-04-02 15:57:30 +0300

    file

---
+++ tropic_practice_19/Untitled-1.js
@@ -0,0 +1,66 @@
+const list = {
+	"create a new practice task": "Progress", 
+	"make a bed": "Done", 
+	"write a post": "To Do"
+};
+
+function changeStatus(task, status) {
+    list[task] = status
+
+}
+
+function addTask(task, status) {
+    list[task] = status
+}
+
+function deleteTask(task) {
+    delete list[task]
+}
+
+function showList() {
+    let hasProgress = false;
+    console.log('Progress: ')
+    for(let listStatus in list) {
+        if(list[listStatus] === 'Progress'){
+            console.log(`\t${listStatus}`);
+            hasProgress = true;
+        } 
+        
+    }
+
+    if (!hasProgress){
+        console.log('-')
+    }
+
+    let hasDone = false;
+    console.log('Done: ')
+    for(let listStatus in list) {
+        if(list[listStatus] === 'Done'){
+            console.log(`\t${listStatus}`);
+            hasDone = true;
+        }
+    }
+
+    if (!hasDone){
+        console.log('-')
+    }
+        
+    let hasTodo = false;
+    console.log('To Do: ')
+    for(let listStatus in list) {
+        if (list[listStatus] === 'To Do'){
+            console.log(`\t${listStatus}`);
+            hasTodo = true; 
+        }
+
+    }
+    if (!hasTodo){
+        console.log(`\t-`)
+    }
+  // console.log(list);
+}
+
+changeStatus('write a post', 'Done');
+addTask('Door ', 'Done');
+deleteTask('make a bed');
+showList();
