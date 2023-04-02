68ee6862dc406316409d9f4669ec9d888f0846c4
elfraat <elfraat96@gmail.com>
2023-04-02 23:52:50 +0600

    v123

--- elfraat/todo.js
+++ elfraat/todo.js
@@ -14,8 +14,12 @@ const TO_DO_LIST = {
     addTask(task){
         this.list[task] = STATUS.TO_DO;
     },
-    deleteTask(task){
-        if (task in this.list) delete this.list[task];
+    deleteTask(task) {
+        if (!(task in this.list)) {
+            console.log('NO TASK FOR DELETE') ;
+        } else {
+            delete this.list[task];
+        }
     },
     showList(){
         for (const task in this.list){
@@ -23,16 +27,20 @@ const TO_DO_LIST = {
         }
     },
     changeStatus(task, status){
-        if (task in this.list) this.list[task] = status;
+        if (!(task in this.list)){
+            console.log('NO TASK FOR CHANGE')
+        } else {
+            this.list[task] = status;
+        }
     },
 
 
 };
 
 TO_DO_LIST.addTask('do todo list');
-TO_DO_LIST.deleteTask('make a bed and go');
+TO_DO_LIST.deleteTask('make a bed');
 TO_DO_LIST.addTask('write change status function');
-TO_DO_LIST.changeStatus('do todo list', STATUS.DONE);
+TO_DO_LIST.changeStatus('do todo lister', STATUS.DONE);
 TO_DO_LIST.addTask('commit and push pull request');
 TO_DO_LIST.deleteTask('do todo listt');
 TO_DO_LIST.changeStatus('do todo list',STATUS.IN_PROGRESS);
