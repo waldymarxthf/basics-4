a61f93ae39716f4cdcab28f73185f64938aa5789
Ruslan Polyanski <ruslapolyanski@yandex.by>
2023-04-03 21:56:33 +0300

    feat: add number function

---
+++ 22-number/number.js
@@ -0,0 +1,36 @@
+
+console.log(Math.random())
+console.log(Math.min(3,5,9))
+console.log(Math.max(3,9,7,11))
+console.log(Math.pow(3,3))
+
+console.log(Math.floor(2.7))
+console.log(Math.ceil(2.3))
+console.log(Math.round(2.4))
+console.log(Math.round(3.5))
+
+console.log(Math.trunc(3.9))
+
+console.log(0.1 + 0.2)
+
+let num = 12.46;
+console.log(num.toFixed(1))
+console.log(+num.toFixed(1))
+console.log(Number(num.toFixed(1)))
+
+console.log(isNaN(NaN))
+console.log(isNaN('strrgfdff'))
+
+console.log(NaN === NaN)
+
+console.log(isFinite('12'))
+console.log(isFinite('sdfsdf'))
+
+console.log(Number.isNaN('dfds'))
+console.log(Number.isNaN(NaN))
+
+console.log(parseInt('12.5px'))
+console.log(parseFloat("12.5px"))
+
+console.log(parseFloat('e3333'))
+console.log(parseInt('sd4353'))
