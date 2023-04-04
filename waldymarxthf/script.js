971ab3f8e5331b2c5e2ff4cd98be8614626c58f9
waldymarxthf <iliya.nov223@gmail.com>
2023-04-04 16:06:07 +0300

    [refactor] refactor: func showVerticalMessage

--- waldymarxthf/script.js
+++ waldymarxthf/script.js
@@ -1,13 +1,13 @@
 function showVerticalMessage(string) {
-	if (string.slice(0, 1) === 's') {
-		string = string[0].toUpperCase() + string.slice(1)
+	const firstLetter = 's'
+	if (string.charAt(0) === firstLetter) {
+		string = string.charAt(0).toUpperCase() + string.slice(1);
 	}
-	if(string.length > 7) {
-		string = string.slice(0, 6)
-	}
-	for(let num of string) {
-		console.log(num)
+	const maxLength = Math.min(string.length, 7)
+	string = string.slice(0, maxLength);
+	for (let num of string) {
+		console.log(num);
 	}
 }
 
-showVerticalMessage('stradastr')
+showVerticalMessage('stradastr');
