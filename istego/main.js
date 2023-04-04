751a6a69446499212d0d92a69de61141d1e8e044
Ivan Marahovskii <marahovskii81@gmail.com>
2023-04-04 13:13:37 +0500

    showVerticalMessage

--- istego/main.js
+++ istego/main.js
@@ -1,1 +1,23 @@
-// здесь пока пусто
+const maxLength = 7;
+const firstSymbolUpperCase = 's';
+const errorEmptyStr = 'Была передана пустая строка';
+
+const str = 'stradaxxxx';
+
+let result = null;
+
+showVerticalMessage(str);
+
+function showVerticalMessage(str) {
+    //Проверяем строку на пустоту
+    if (!str) return console.log(errorEmptyStr);
+
+    result = str.slice(0, maxLength);
+    //Убираем чувствительность к регистру и проверяем первый символ на 's'
+    if(str.at(0).toLowerCase() === firstSymbolUpperCase) {
+        result = str.at(0).toUpperCase() + str.slice(1, maxLength);
+    }
+    for (const symbol of result) {
+        console.log(symbol);
+    }
+}
