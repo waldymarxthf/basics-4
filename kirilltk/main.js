46fd8c841f24f43ed14317e33c9d02a0b42480cc
Kirill-Tkachenko242 <110831941+Kirill-Tkachenko242@users.noreply.github.com>
2023-04-04 17:24:59 +0300

    new commit

---
+++ kirilltk/main.js
@@ -0,0 +1,23 @@
+
+const letterS = 's';
+function showVerticalMessage(str) {
+	if (str.length  > 7 && str[0] === letterS) {
+		for (let i = 0; i < 7; i++) {
+			(i === 0) ? console.log(str[i].toUpperCase()) : console.log(str[i]);
+		};
+	} else if (str.length  > 7) {
+		for (let i = 0; i < 7; i++) {
+			console.log(str[i]);
+		};
+	} else if (str[0] === letterS) {
+		for (let i = 0; i < str.length; i++) {
+			(i === 0) ? console.log(str[i].toUpperCase()) : console.log(str[i]);
+		};
+	} else {
+		for (let i = 0; i < str.length; i++) {
+			console.log(str[i]);
+		};
+	};
+};
+
+showVerticalMessage('strada');
