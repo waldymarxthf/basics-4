ae34dbbd4bda10d0b375211206d85acb0366f2aa
waldymarxthf <iliya.nov223@gmail.com>
2023-04-04 16:19:50 +0300

    [chore] chore: add validation on the empty string

--- waldymarxthf/script.js
+++ waldymarxthf/script.js
@@ -1,13 +1,22 @@
 function showVerticalMessage(string) {
+	if(!string) {
+		console.log('String is empty')
+		return null;
+	}
+	
 	const firstLetter = 's'
+
 	if (string.charAt(0) === firstLetter) {
 		string = string.charAt(0).toUpperCase() + string.slice(1);
 	}
+
 	const maxLength = Math.min(string.length, 7)
 	string = string.slice(0, maxLength);
+
 	for (let num of string) {
 		console.log(num);
 	}
+	return null;
 }
 
-showVerticalMessage('stradastr');
+showVerticalMessage('strada');
