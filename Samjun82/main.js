dec727ab96f1196add1b7a4a04131859d9531009
Samj <samjun82gmail.com>
2023-04-04 23:10:06 +0300

    new practice

--- Samjun82/main.js
+++ Samjun82/main.js
@@ -1,46 +1,12 @@
-const list = {
-	"create a new practice task": "In Progress", 
-	"make a bed": "Done",
-	"write a post": "To Do",
+const showVerticalMassage = word => {
+  let str = word.toLowerCase()
+  str = str[0].toUpperCase() + str.slice(1)
+  str = str.substring(0, 7)
+//   for(let i = 0; i < str.length; i++) {     <----   первоначальный способ
+// console.log(str[i])
+for(const go of str) {
+  console.log(go)
+  }
 }
 
-function changeStatus(name, status) {  // меняет статус задачи
-  return list[name] = status
-}
-
-function addTask(name) {              // добавляет новую задачу
-  return list[name] = "To Do"
-}
-
-function deleteTask(name) {           // удаляет задачу
-  return delete list[name]
-}; 
-
-function showList() {
-  let toDo = ''
-  let inProg = ''
-  let done = ''
-
-    for(const name in list) {
-      switch(list[name]) {
-        case 'To Do':
-                    toDo += name + '\n\t'
-                    break
-        case 'In Progress':
-                    inProg += name + '\n\t'
-                    break
-        case 'Done':
-                    done += name + '\n\t'
-                    break
-                    
-      }
-    }
-console.log(`To Do:\n\t${toDo}\nIn Progress:\n\t${inProg}\nDone:\n\t${done}`)
-}
-
-changeStatus('create a new practice task', 'Done')
-addTask('learning english')
-addTask('swiming')
-changeStatus('swiming', 'In Progress')
-deleteTask('somthin')
-showList()
+showVerticalMassage('NASTRADAMUS')
