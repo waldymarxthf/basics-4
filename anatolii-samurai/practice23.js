200339b2736f40cac7989c0d279d0c231ec8040a
Anatolii <goddevil22@yandex.ru>
2023-04-04 19:00:46 +0400

    practice23 new changes

--- anatolii-samurai/practice23.js
+++ anatolii-samurai/practice23.js
@@ -1,11 +1,21 @@
 // функция которая преобразует полученную строку в "вертикальный вид" и выводит ее в консоль
 function showVerticalMessage(str){
+   //проверка на букву "s"
     if(str[0] == "s"){
         str = str[0].toUpperCase() + str.slice(1);
-    }
+        showChar(str);
+    }else if(str.length > 7){   //проверка на длину строки
+        str = str.slice(0, 5);
+        showChar(str);
+    } else{
+        console.log("error");
+    }  // 
+}
+// функция для вывода цикла for...of
+function showChar(str){
     for(let char of str){ 
         console.log(char);
     }
-    // 
 }
-showVerticalMessage("sea");
+
+showVerticalMessage("sum");
