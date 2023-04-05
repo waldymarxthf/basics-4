9df0ac169d4044151d57c14bfab6229d62ee8b47
Samozvan Stanislav <ssfrontendd@gmail.com>
2023-04-05 18:49:53 +0200

    practice-24

---
+++ Samstas/main.js
@@ -0,0 +1,16 @@
+// create array
+let toDoList = ["coding", "workout", "read"];
+// create a new task
+toDoList.unshift("morning stretch");
+toDoList.push("meditation");
+toDoList.splice(1, 0, "watch movie", "play games");
+
+// delete task
+toDoList.shift();
+toDoList.pop();
+toDoList.splice(1, 1);
+
+//show tasks
+for (const task of toDoList) {
+  console.log(task);
+}
