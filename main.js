1df90ac2abfe14b5a4990a0900fafaae1c6bb7bc
Nikolay R <ras.nikolay@mail.ru>
2023-04-05 14:13:37 +0300

    update else string

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 function showVerticalMessage(str, end) {
-
-    let newStr = str.slice(0, 1).toUpperCase() + str.slice(1, end);
+    let newStr = '';
+    if (str[0] === 's') {
+        newStr = str.slice(0, 1).toUpperCase() + str.slice(1, end);
+    } else {
+        newStr = str.slice(0, end);
+    }
 
     for (let i = 0; i < newStr.length; i++) {
         console.log(newStr[i]);
@@ -8,4 +12,4 @@ function showVerticalMessage(str, end) {
 
 };
 
-showVerticalMessage('stradafffffffffffffff', 10);
+showVerticalMessage('dtradafffffffffffffff', 7);
