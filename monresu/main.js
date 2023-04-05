757f3ed2d1642b39e0dfbcbcc6146e86427a182f
Anastasia <tgmonresu@gmail.com>
2023-04-05 06:25:11 +0300

    todo v1 array

--- monresu/main.js
+++ monresu/main.js
@@ -1,76 +1,46 @@
-const message = {
-  errorNotTask: 'Sorry! Такой задачи нет!',
-  errorIsTask: 'Sorry! Такая задача уже в наличии',
-  deleteTaskConst: 'Complete! Task was удалена',
-  addTaskConst: 'Task was добавлена',
-  changeTaskConst: 'Task was изменена',
-  errorTaskIsEmpty: 'Task пустая',
+const todoList = [];
+
+const error = 'Something went wrong!';
+
+function isValid(task) {
+  const validation = !(task == '\0') || (task == '\n') || (task == '\r');
+  return validation;
 }
 
-const STATUS = {
-  TODO: 'todo',
-  IN_PROGRESS: 'in progress',
-  DONE: 'done',
-  TEST: 'test',
+function inTodo(task) {
+  return todoList.includes(task);
 }
 
-function isTaskInList(task) {
-  return task in todoList.todos;
+function add(task) {
+  if (!isValid(task) || inTodo(task)) {
+    console.log(error);
+    return;
+  }
+  todoList.push(task);
 }
 
-function isTaskTextValid(task) {
-  return !task;
+function del(task) {
+  if (!isValid(task) || !inTodo(task)) {
+    console.log(error)
+    return;
+  }
+  for (let i = 0; i < todoList.length; i++) {
+    if (todoList[i] == task) {
+      todoList.splice(i, 1);
+      break;
+    }
+  };
 }
 
-const todoList = {
-	todos: {},
-  changeStatus(task, text) {
-    if (!isTaskInList(task)) {
-      console.log(message.errorNotTask)
-      return;
-    }
-    this.todos[task] = text;
-    console.log(message.changeTaskConst)
-  },
-  addTask(task, status = STATUS.TODO) {
-    if (isTaskInList(task)) {
-      console.log(message.errorIsTask, task)
-      return;
-    }
-    if (isTaskTextValid(task)) {
-      console.log(message.errorTaskIsEmpty);
-      return;
-    }
-    this.todos[task] = status;
-    console.log(message.addTaskConst)
-  },
-  deleteTask(task) {
-    if (!isTaskInList(task)) {
-      console.log(message.errorNotTask)
-      return;
-    }
-    delete this.todos[task];
-    console.log(message.deleteTaskConst)
-  },
-  showList() {
-    for (const stat in STATUS) {
-      let tasks = '';
-      for (const key in this.todos) {
-        if (this.todos[key].toLowerCase() === STATUS[stat].toLowerCase()) {
-          tasks += `\n\t${key}`
-        }
-      }
-      console.log(`--${STATUS[stat].toUpperCase()}--`, tasks ? tasks : '\n\tNothing tasks!');
-    }
+function showList() {
+  for (const task of todoList) {
+    console.log(task);
   }
 }
 
-todoList.addTask('have a walk');
-todoList.addTask('SLEEPY');
-todoList.addTask('delete JSON from web');
-todoList.addTask('TESTIKIIGIIGIGIG', STATUS.TEST);
-todoList.addTask('');
-todoList.deleteTask('drink tea');
-todoList.changeStatus('delete JSON from web', STATUS.DONE);
-todoList.changeStatus('SLEEPY', STATUS.IN_PROGRESS);
-todoList.showList()
+add('Поспать!');
+del('Пока кать!');
+add('Попить чаю!');
+del('Попить чаю!');
+
+showList();
