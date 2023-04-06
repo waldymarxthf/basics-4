c7b108710a1e20de1b8bfde86c7d01915f0c59f4
Kirill <streetmtb@mail.ru>
2023-04-06 18:45:32 +0300

    поправил_upperCase

--- kirillmalofeev/main.js
+++ kirillmalofeev/main.js
@@ -1,12 +1,10 @@
 function showVerticalMessage(str) {
-  
-  if (str[0] == toLowerCase()) {
+  if (str[0] === str[0].toLowerCase()) {
     console.log(str[0].toUpperCase() + str.slice(1, 7));
-  };
+  }
   let strVertical = '';
   for (let i = 0, i < 7, i++) {
     strVertical += i + '\n';
   }console.log(strVertical);
-
-} console.log(showVerticalMessage(`strada`));
-
+}
+console.log(showVerticalMessage(`strada`));
