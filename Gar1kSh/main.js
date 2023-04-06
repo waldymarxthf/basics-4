63b8a2558e70626d72206cbad8fb8e1303c2b4c0
Gar1kSh <skydust63@gmail.com>
2023-04-06 22:05:56 +0300

    array negative number

--- Gar1kSh/main.js
+++ Gar1kSh/main.js
@@ -1,9 +1,13 @@
 const users = [1, 3, 4, 5, 8];
+const numbers1 = [1, 11, -2, 3, -10, 4];
+
 users.forEach((user) => {
   console.log(`Number is ${user}`);
 });
 
 const animals = ["cat", "dog", "elephant", "tiger", "lion"];
 const firstAnimalFind = animals.find((name) => name.length > 7);
+const isNegativeNumberArray = numbers1.filter((number) => number < 0);
 
 console.log(firstAnimalFind);
+console.log(isNegativeNumberArray);
