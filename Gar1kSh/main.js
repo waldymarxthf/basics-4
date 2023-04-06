657f68bd49c0ef1a753937bf27f7b50b5b0efe57
Gar1kSh <skydust63@gmail.com>
2023-04-06 22:03:09 +0300

    word search for the right length

--- Gar1kSh/main.js
+++ Gar1kSh/main.js
@@ -2,3 +2,8 @@ const users = [1, 3, 4, 5, 8];
 users.forEach((user) => {
   console.log(`Number is ${user}`);
 });
+
+const animals = ["cat", "dog", "elephant", "tiger", "lion"];
+const firstAnimalFind = animals.find((name) => name.length > 7);
+
+console.log(firstAnimalFind);
