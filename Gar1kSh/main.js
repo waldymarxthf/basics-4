9d442a1f3c511117c9facafa8a6514f41e6f8b96
Gar1kSh <skydust63@gmail.com>
2023-04-06 22:10:39 +0300

    array abs number

--- Gar1kSh/main.js
+++ Gar1kSh/main.js
@@ -8,6 +8,8 @@ users.forEach((user) => {
 const animals = ["cat", "dog", "elephant", "tiger", "lion"];
 const firstAnimalFind = animals.find((name) => name.length > 7);
 const isNegativeNumberArray = numbers1.filter((number) => number < 0);
+const absNumber = numbers1.map((number) => (number = Math.abs(number)));
 
 console.log(firstAnimalFind);
 console.log(isNegativeNumberArray);
+console.log(absNumber);
