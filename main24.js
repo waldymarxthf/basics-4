6baf7b630b8120673dbf776e4a45a4b1a9e9c847
Arsen2222 <arsen21081111@mail.ru>
2023-04-06 18:32:33 +0500

    Strada test 24

---
+++ main24.js
@@ -0,0 +1,6 @@
+// let toDoList = ['Почитать','',''];
+// let toDoList = Array.of('Почитать','','')
+// let toDoList = new Array('Почитать','','')
+let toDoList = ['почитать', 'помыть посуду', 'помыть машину', 'посадить цветы'];
+let tasksCount = toDoList.length; 
+console.log(toDoList.length);
