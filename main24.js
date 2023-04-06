a2c2978ea703e24d67be7093cdfb6cab260dcf1b
Arsen2222 <arsen21081111@mail.ru>
2023-04-06 18:35:36 +0500

    Strada test 24

--- main24.js
+++ main24.js
@@ -1,6 +1,11 @@
-// let toDoList = ['Почитать','',''];
-// let toDoList = Array.of('Почитать','','')
-// let toDoList = new Array('Почитать','','')
-let toDoList = ['почитать', 'помыть посуду', 'помыть машину', 'посадить цветы'];
-let tasksCount = toDoList.length; 
-console.log(toDoList.length);
+let muMassive = ['проснуться', 'учить', 'работать', 'играть', 'спать'];
+let Twix = muMassive.push('Дёрнуть Гуся');
+let Snickers = muMassive.slice(0,2);
+let Mars = muMassive.splice(2, 1, 'Что - то');
+console.log(muMassive);
+console.log(Snickers);
+console.log(muMassive);
+for(const task of muMassive) {
+    console.log(task);
+};
+
