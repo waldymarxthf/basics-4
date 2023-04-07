22a8d78b26cbb0ff65ee08ec85471b4915839075
Kirill <streetmtb@mail.ru>
2023-04-07 19:33:18 +0300

    все_работает

--- kirillmalofeev/main.js
+++ kirillmalofeev/main.js
@@ -1,12 +1,14 @@
 function showVerticalMessage(str) {
   if (str[0] === "s") {
     str = str[0].toUpperCase() + str.slice(1, 7);
+  } else if (str.length > 7) {
+    str = str.slice(0, 7);
   }
-  let strVertical = str;
-  for (let char of strVertical) {
-    strVertical = char;
-    console.log(strVertical);
+  let strVertical = "";
+  for (let char of str) {
+    strVertical += char + "\n";
   }
+  console.log(strVertical);
 }
 
 showVerticalMessage(`stradaa`);
