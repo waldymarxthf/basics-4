d3e5adf7977e1d9bd75e4e5e3cade84abd5de45a
Dmitry Komarov <dmitrykomarov@MacBook-Pro-Dmitry.local>
2023-04-08 00:51:04 +0300

    add file

---
+++ dkomarov/main.js
@@ -0,0 +1,75 @@
+const STATUS = {
+    DONE: "Done",
+    INPROGRESS: "In Progress"
+};
+
+
+const todoList = [
+    {name: 'create a post', status: STATUS.INPROGRESS}, 
+    {name: 'test', status: STATUS.INPROGRESS},
+    {name: 'abrakadabra', status: STATUS.INPROGRESS},
+    {name: 'walking', status: STATUS.INPROGRESS}
+  
+]; 
+
+function addTask(task) {
+    todoList.push({name: task, status: STATUS.INPROGRESS});
+    
+    
+};
+
+function deleteTask(task) {
+    const taskName = todoList.findIndex(tasks => tasks.name === task);
+    todoList.splice(taskName, 1);
+
+    
+    
+
+};
+
+function changeStatus(task, newStatus) {
+    for (key in todoList) {
+        if (todoList[key].name === task) {
+            todoList[key].status = newStatus;
+        };
+    };
+};
+
+function showList() {
+    let tasksInProg = '';
+    let tasksDone = '';
+    for (const task of todoList) {
+        if (task.status === STATUS.INPROGRESS) {
+            tasksInProg += `\n  ${task.name}`;
+            
+        } else if (task.status === STATUS.DONE) {
+            tasksDone += `\n  ${task.name}`;
+            
+        };
+        
+    };
+
+    if (tasksInProg === '') {
+        console.log(`In Progress: \n \t -`);
+    } else {
+        console.log(`In Progress:  ${tasksInProg}`);
+    };
+
+    if (tasksDone === '') {
+        console.log(`Done: \n \t -`);
+    } else {
+        console.log(`Done: ${tasksDone}`);
+    };  
+};
+
+addTask('qwerty');
+addTask('reading');
+addTask('testTEST');
+deleteTask('abrakadabra');
+deleteTask('testTEST')
+changeStatus('test', STATUS.DONE);
+changeStatus('qwerty', STATUS.DONE);
+changeStatus('reading', STATUS.DONE);
+
+
+showList();
