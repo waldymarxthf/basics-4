df2c22b12bccffecd42e084d0b471453300f8fba
Anastasia <tgmonresu@gmail.com>
2023-04-07 06:34:13 +0300

    ok

--- monresu/main.js
+++ monresu/main.js
@@ -1,46 +1,20 @@
-const todoList = [];
-
-const error = 'Something went wrong!';
-
-function isValid(task) {
-  const validation = !(task == '\0') || (task == '\n') || (task == '\r');
-  return validation;
+const status = {
+  TODO: 'ToDo',
+  DONE: 'Done',
+  IN_PROGRESS: 'In Progress',
 }
 
-function inTodo(task) {
-  return todoList.includes(task);
+const priority = {
+  HIGH: 'high',
+  LOW: 'low',
 }
 
-function add(task) {
-  if (!isValid(task) || inTodo(task)) {
-    console.log(error);
-    return;
-  }
-  todoList.push(task);
+const list = [ 
+	{name: 'create a post', status: 'In progress', priority: 'low'}, 
+  {name: 'test', status: 'Done', priority: 'high'} 
+];
+
+function isEmpty(task) {
+  return (task == '') || (task == '\n') || (task == '\r') || (task == '\0');
 }
 
-function del(task) {
-  if (!isValid(task) || !inTodo(task)) {
-    console.log(error)
-    return;
-  }
-  for (let i = 0; i < todoList.length; i++) {
-    if (todoList[i] == task) {
-      todoList.splice(i, 1);
-      break;
-    }
-  };
-}
-
-function showList() {
-  for (const task of todoList) {
-    console.log(task);
-  }
-}
-
-add('Поспать!');
-del('Пока кать!');
-add('Попить чаю!');
-del('Попить чаю!');
-
-showList();
