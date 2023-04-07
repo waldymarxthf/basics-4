266b4fef197f70bb69e438f385b5971f34546801
drunque <lufi.2023@gmail.com>
2023-04-07 20:01:12 +0300

    added showTasks function

--- drunque/practice.js
+++ drunque/practice.js
@@ -3,9 +3,13 @@ const tasks = [];
 tasks.push("Listen to Techno");
 tasks.push("Listen to lo-fi");
 
-let index = 0
-for (const task of tasks) {
-  console.log(`Task ${++index}. ${task}`)
+function showTasks() {
+  let index = 0;
+  for (const task of tasks) {
+    console.log(`Task ${++index}. ${task}`);
+  }
 }
+
+showTasks()
 
 let finishedTask = tasks.pop();
