c49b86dca4f7b68a9c45f600846fa2244ecd0fb4
Stas <121170610+Samstas@users.noreply.github.com>
2023-04-07 07:23:18 +0200

    Update main.js

--- Samstas/main.js
+++ Samstas/main.js
@@ -1,31 +1,63 @@
-const toDoList = {
-  list: {
-    "Buy food": "To Do",
-    "Read a book": "In Progress",
-    "Meditation 10min": "Done",
-    "Workout 30min": "Done",
-  },
-
-  addTask(task, status) {
-    this.list[task] = status;
-  },
-
-  deleteTask(task) {
-    delete this.list[task];
-  },
-
-  changeStatus(task, status) {
-    this.list[task] = status;
-  },
-
-    showList() {
-      for (let task in toDoList.list) {
-        console.log(`${task}: ${toDoList.list[task]}`);
-      }
-    },
+const taskStatus = {
+  TO_DO: "To Do",
+  IN_PROGRESS: "In Progress",
+  DONE: "Done",
 };
 
-// toDoList.deleteTask("buy food");
-// toDoList.addTask("make a bed");
-// toDoList.changeStatus('Buy food', 'Done');
-toDoList.showList();
+const list = {
+  "workout 10 min": taskStatus.IN_PROGRESS,
+  "read book 15 min": taskStatus.DONE,
+  "meditation 5 min": taskStatus.TO_DO,
+};
+
+function changeStatus(task, status) {
+  task in list
+    ? (list[task] = status)
+    : console.log(`В списке такой задачи нет: '${task}'`);
+}
+
+function addTask(task) {
+  if (!task || typeof task === "number") {
+    console.log(
+      'Пожалуйста, введите название задачи таким образом - "убрать дома"'
+    );
+    return;
+  }
+
+  task in list
+    ? console.log(`Задача '${task}' уже есть в списке дел`)
+    : (list[task] = taskStatus.TO_DO);
+}
+
+function deleteTask(task) {
+  task in list
+    ? delete list[task]
+    : console.log(`Задачи с таким именем  ${task} нет в списке`);
+}
+
+function showList() {
+  const tasksByStatus = {
+    [taskStatus.TO_DO]: "",
+    [taskStatus.IN_PROGRESS]: "",
+    [taskStatus.DONE]: "",
+  };
+
+  for (const task in list) {
+    const status = list[task];
+    tasksByStatus[status] += `\t"${task}"\n`;
+  }
+
+  for (const status in tasksByStatus) {
+    if (tasksByStatus[status] === "") {
+      tasksByStatus[status] = `\t-`;
+    }
+    console.log(`${status}: \n${tasksByStatus[status]}`);
+  }
+}
+
+addTask("wash the dishes");
+addTask("homework");
+changeStatus("workout 10 min", taskStatus.DONE);
+deleteTask("wash the dishes");
+addTask(979);
+showList();
