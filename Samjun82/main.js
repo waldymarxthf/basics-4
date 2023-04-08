e082eea2a94fcef7a9d80ad4768d077a0cbd5757
Samj <samjun82gmail.com>
2023-04-08 22:26:08 +0300

    ToDo on arrays

--- Samjun82/main.js
+++ Samjun82/main.js
@@ -1,20 +1,87 @@
-const number = [0,1,2,3,4,5,6,7,8,9]
-number.forEach(element => {
-  console.log(`Number is ${element}`)
-});
+const STATUS = {
+  IN_PROGRESS: 'In Progress',
+  DONE: 'Done',
+  TO_DO: 'To Do'
+}
 
+const PRIORITY = {
+  LOW: 'low',
+  MID: 'midium',
+  HIGH: 'high'
+}
 
-const animals = ['cat', 'dog', 'elephant', 'tiger', 'lion']
-const findAnimal = animals.find(str => str.length > 7)
-console.log(findAnimal)
+const MESSAGE = {
+  TASK_IS_ADDED: 'This task is already exists',
+  TASK_IS_WRONG: 'Please enter a valid value',
+  TASK_IS_DELETED: 'Task is already deleted'
+}
 
+const list = [
+	{name: 'create a post', status: STATUS.IN_PROGRESS, priority: PRIORITY.LOW}, 
+  {name: 'make a bed', status: STATUS.DONE, priority: PRIORITY.HIGH},
+  {name: 'create a new practice task', status: STATUS.IN_PROGRESS, priority: PRIORITY.LOW}, 
+  {name: 'write a post', status: STATUS.TO_DO, priority: PRIORITY.HIGH}
+]
 
-const num = [1, 11, -2, 3, -10, 4]
-const negativeNum = num.filter(number => number < 0)
-console.log(negativeNum)
+const changeTaskStatus = (name, status) => {
+  const task = list.find(task => task.name === name)
+  if(task){
+      task.status = status
+  }else{
+      console.log(MESSAGE.TASK_IS_WRONG)
+  }
+}
+const changeTaskPriority = (name, priority) => {
+  const task = list.find(task => task.name === name)
+  if(task){
+      task.priority = priority
+  }else{
+      console.log(MESSAGE.TASK_IS_WRONG)
+  }
+}
 
-const absoluteNum = num.map(number => Math.abs(number))
-console.log(absoluteNum)
+const addTask = (name, status = STATUS.TO_DO, priority = PRIORITY.LOW) => {
+  const task = list.find(task => task.name === name)
+  if(task){
+      console.log(MESSAGE.TASK_IS_ADDED)
+  } else{
+      list.push({name, status, priority})
+  }
+}
 
-const sortNum = num.sort(number => number)
-console.log(sortNum)
+const deleteTask = name => {
+  const task = list.findIndex(task => task.name === name) 
+    if(task){
+      list.splice(task,1)
+      console.log(MESSAGE.TASK_IS_DELETED)
+    } else {
+      console.log(MESSAGE.TASK_IS_WRONG)
+    }
+}
+
+const showList = () => {
+  let toDo = ''
+  let inProg = ''
+  let done = ''
+  
+  for (const task of list) {
+    if(task.status === STATUS.TO_DO){
+      toDo += `${task.name} \n\t`
+    } else if (task.status === STATUS.IN_PROGRESS){
+      inProg += `${task.name} \n\t`
+    } else if (task.status === STATUS.DONE){
+      done += `${task.name} \n\t`
+    }
+  }
+  console.log(`To Do:\n\t${toDo}\nIn Progress:\n\t${inProg}\nDone:\n\t${done}`)
+}
+
+changeTaskStatus('dc', 'To Do')
+changeTaskStatus('create a post', 'Done')
+addTask('jumpin')
+addTask('swimin')
+changeTaskStatus('jumpin', 'Done')
+deleteTask('make a bed')
+console.log(list)
+
+showList()
