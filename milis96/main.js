bd2e9fc20839a1e721bd8befd6a91dac5a351ea9
Stefan Milivojevic <milis96@me.com>
2023-04-08 18:57:40 +0200

    done project-26

--- milis96/main.js
+++ milis96/main.js
@@ -1,12 +1,66 @@
-let toDoList = ["Пойти на работу", "Закрыть задачи по проектам", "Покушать"];
+let toDoLits = [
+    {name: "Make an app", progress: "In progress", priority: "High"},
+    {name: "Drink coffee", progress: "Done", priority: "Low"},
+    {name: "Find new client", progress: "In progress", priority: "High"},
+    {name: "Have a breakfast", progress: "In progress", priority: "High"}
+]
 
-toDoList.push("Встретить жену после работы");
 
-console.log(toDoList);
-
-toDoList.shift();
-console.log(toDoList);
-
-for (let task of toDoList){
-    console.log(task);
+function addTask(name, progress, priority) {
+    toDoLits.push({name, priority, progress});
 }
+
+
+
+
+function changeStatus (name, progress) {
+    let taskIndex = toDoLits.findIndex(array => array.name == name);
+    toDoLits[taskIndex].progress = progress
+}
+
+
+
+function deleteTask(name) {
+    let taskIndex = toDoLits.findIndex(array => array.name == name);
+    toDoLits.splice(taskIndex, taskIndex);
+}
+
+function showList() {
+    let tasksInProgress = [];
+    let tasksDone = [];
+    for (const task of toDoLits){
+        switch(task.progress) {
+            case "In progress":
+                tasksInProgress.push(task);
+                break;
+            case "Done":
+                tasksDone.push(task);
+                break;
+        }
+    }
+    console.log("Tasks in progress:")
+    tasksInProgress.forEach(task => {
+        console.log(`\t` + task.name + ": " + task.priority);
+    })
+    console.log("Tasks have done:")
+    tasksDone.forEach(task => {
+        console.log(`\t` + task.name + ": " + task.priority);
+    })
+}
+
+addTask("new task", "In progress", "Low");
+console.log(toDoLits);
+
+changeStatus("Find new client", "Done");
+console.log(toDoLits);
+
+deleteTask("new task");
+console.log(toDoLits);
+
+showList();
+
+
+// changeStatus("write a post", "Done") // меняет статус задачи
+// addTask('have a walk'); // добавляет новую задачу
+// deleteTask('have a walk'); // удаляет задачу
+// showList(); // показывает список всех задач
