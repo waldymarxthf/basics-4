8606d4f7f251412367e4f29e79045c80676a4e91
Vitalii Miagkov <maposia@gmail.com>
2023-04-09 11:07:35 +0700

    Added: new function to change priority, messages, statuses and priorities are moved to objects. Fixed: code refactoring, some functions improved.

--- maposia/toDo.js
+++ maposia/toDo.js
@@ -1,8 +1,38 @@
 const tasks = []
-const STATUSES = ['To Do', 'In Progress', 'Done']
-const PRIORITIES = ['low', 'medium', 'high']
+const taskAtr = {
+  status: {
+    TODO: 'To Do',
+    INPROGRESS: 'In Progress',
+    DONE: 'Done',
+  },
+  priority: {
+    LOW: 'low',
+    MEDIUM: 'medium',
+    HIGH: 'high',
+  },
 
-function taskExist(name) {
+  checkStatus(status) {
+    return Object.values(this.status).includes(status)
+  },
+  checkPriority(priority) {
+    return Object.values(this.priority).includes(priority)
+  },
+}
+
+const MESSAGES = {
+  ERROR: {
+    TASKEXIST: 'Такая задача уже существует',
+    STATUSINCORRECT: 'Введен некорректный статус',
+    PRIORITYINCORRECT: 'Введен некорректный приоритет',
+    TASKNOTFOUND: 'Такой задачи не существует',
+  },
+  SUCCESS: {
+    ADDEDTASK: 'Задача успешно добавлена',
+    DELETEDTASK: 'Задача успешно удалена',
+  },
+}
+
+function isTaskExist(name) {
   if (tasks.length > 0) {
     if (tasks.find((task) => task.name === name)) {
       return true
@@ -11,66 +41,19 @@ function taskExist(name) {
     }
   }
 }
-// тут можно улучшить, соединить проверку приорити и статус в отдельную функцию
-function correctStatus(status) {
-  if (status && STATUSES.includes(status)) {
-    return true
-  } else {
-    console.log(`Статус ${status} не существует`)
-    return false
-  }
-}
 
-function correctPriority(priority) {
-  if (priority && PRIORITIES.includes(priority)) {
+function isCorrectTaskAtr(attribute) {
+  if (
+    (attribute && taskAtr.checkStatus(attribute)) ||
+    taskAtr.checkPriority(attribute)
+  ) {
     return true
   } else {
     return false
   }
 }
 
-function addTask(name, status = 'To Do', priority = 'low') {
-  if (taskExist(name)) {
-    console.log('Такая задача уже существует')
-    return
-  }
-  if (!correctStatus(status)) {
-    return
-  }
-  if (!correctPriority(priority)) {
-    return
-  }
-
-  tasks.push({ name, status, priority })
-}
-
-function delTask(name) {
-  if (!taskExist(name)) {
-    return
-  }
-  tasks.forEach((el, index) => {
-    if (el.name === name) {
-      console.log(`Задача ${name} удалена`)
-      tasks.splice(index, 1)
-    }
-  })
-}
-
-function chageStatus(name, status) {
-  if (!taskExist(name)) {
-    return
-  }
-  if (!correctStatus(status)) {
-    return
-  }
-  tasks.forEach((el) => {
-    if (el.name === name) {
-      el.status = status
-    }
-  })
-}
-
-function showList(status) {
+function listCreation(status) {
   const oneTypeStatus = tasks.filter((task) => {
     return task.status === status
   })
@@ -84,13 +67,76 @@ function showList(status) {
   })
 }
 
-function taskList() {
-  showList('To Do')
-  showList('In Progress')
-  showList('Done')
+function addTask(name, status = 'To Do', priority = 'low') {
+  if (isTaskExist(name)) {
+    console.log(MESSAGES.ERROR.TASKEXIST)
+    return
+  }
+  if (!isCorrectTaskAtr(status)) {
+    console.log(MESSAGES.ERROR.STATUSINCORRECT)
+    return
+  }
+  if (!isCorrectTaskAtr(priority)) {
+    console.log(MESSAGES.ERROR.STATUSINCORRECT)
+    return
+  }
+  console.log(MESSAGES.SUCCESS.ADDEDTASK)
+  tasks.push({ name, status, priority })
+}
+
+function delTask(name) {
+  if (!isTaskExist(name)) {
+    console.log(MESSAGES.ERROR.TASKNOTFOUND)
+    return
+  }
+  tasks.forEach((el, index) => {
+    if (el.name === name) {
+      console.log(MESSAGES.SUCCESS.DELETEDTASK)
+      tasks.splice(index, 1)
+    }
+  })
+}
+
+function chageStatus(name, status) {
+  if (!isTaskExist(name)) {
+    console.log(MESSAGES.ERROR.TASKNOTFOUND)
+    return
+  }
+  if (!isCorrectTaskAtr(status)) {
+    console.log(MESSAGES.ERROR.STATUSINCORRECT)
+    return
+  }
+  tasks.forEach((el) => {
+    if (el.name === name) {
+      el.status = status
+    }
+  })
+}
+
+function changePriority(name, priority) {
+  if (!isTaskExist(name)) {
+    console.log(MESSAGES.ERROR.TASKNOTFOUND)
+    return
+  }
+  if (!isCorrectTaskAtr(priority)) {
+    console.log(MESSAGES.ERROR.PRIORITYINCORRECT)
+    return
+  }
+  tasks.forEach((el) => {
+    if (el.name === name) {
+      el.priority = priority
+    }
+  })
+}
+
+function showList() {
+  listCreation('To Do')
+  listCreation('In Progress')
+  listCreation('Done')
 }
 
 addTask('сон')
+
 addTask('дартс')
 addTask('сноуборд')
 // console.log(tasks)
@@ -104,8 +150,6 @@ addTask('Херпойми', 'Done', 'high')
 delTask('доктор')
 delTask('купить воду')
 
-taskList()
-
 chageStatus('сон', 'In Progress')
-
-taskList()
+changePriority('сайт для компании', 'low')
+showList()
