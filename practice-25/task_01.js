f7460fcb5a073bb45692086c7f8b7897fbd5d333
Oleg Zavodtsov <podolski133@gmail.com>
2023-04-10 22:16:49 +0300

    add task_02

--- practice-25/task_01.js
+++ practice-25/task_01.js
@@ -1,3 +1,8 @@
+/* 
+Создать массив из целых чисел, используя метод forEach, 
+вывести в консоль с сообщением Number is ${number}
+*/
+
 const integers = [4, 8, 7, 43, 78, 99];
 
 integers.forEach(integers => {
---
+++ practice-25/task_02.js
@@ -0,0 +1,4 @@
+const animals = ['cat', 'dog', 'elephant', 'tiger', 'lion'];
+
+const longWord = animals.find(animal => animal.length > 7);
+console.log(longWord);
