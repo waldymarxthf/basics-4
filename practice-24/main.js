3f84388a66ee0ac428394caf9291603b8bd1ab88
Oleg Zavodtsov <podolski133@gmail.com>
2023-04-10 21:23:25 +0300

    add practice-24

---
+++ practice-24/main.js
@@ -0,0 +1,6 @@
+const tasks = ['План на день', 'Занятие спортом', 'Завтрак', 'Обед', 'Ужин', 'Чтение', 'Учение'];
+
+const delTask = tasks.splice(0, 4);
+
+console.log(delTask);
+console.log(tasks);
