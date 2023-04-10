d56be3fb47a76ed6a4852eb7adec78453f3074e4
John <98343581+johnbeelow@users.noreply.github.com>
2023-04-10 05:55:24 +0300

    style code upd

--- johnbeelow/array-todo.js
+++ johnbeelow/array-todo.js
@@ -1,102 +1,102 @@
 const STATUS = {
-  IN_PROGRESS: "In Progress",
-  DONE: "Done",
-  TO_DO: "To Do",
-};
+  IN_PROGRESS: 'In Progress',
+  DONE: 'Done',
+  TO_DO: 'To Do',
+}
 
 const ERORR = {
-  NO_TASK: "There is no such task in the list",
-  IS_TASK: "Such a task is already on the list",
-};
+  NO_TASK: 'There is no such task in the list',
+  IS_TASK: 'Such a task is already on the list',
+}
 
 const PRIORITY = {
-  LOW: "low",
-  HIGHT: "high",
-};
+  LOW: 'low',
+  HIGHT: 'high',
+}
 
 const list = [
   {
-    name: "create a post",
+    name: 'create a post',
     status: STATUS.IN_PROGRESS,
     priority: PRIORITY.LOW,
   },
   {
-    name: "test",
+    name: 'test',
     status: STATUS.DONE,
     priority: PRIORITY.HIGHT,
   },
-];
+]
 
 function checkTask(taskName) {
-  const check = list.filter((task) => task.name === taskName);
+  const check = list.filter((task) => task.name === taskName)
   if (check.length > 0) {
-    return true;
+    return true
   }
 }
 
 function indexTask(taskName) {
-  const indexValue = list.findIndex((task) => task.name === taskName);
-  return indexValue;
+  const indexValue = list.findIndex((task) => task.name === taskName)
+  return indexValue
 }
 
 function addTask(taskName) {
   if (checkTask(taskName)) {
-    console.log(ERORR.IS_TASK);
+    console.log(ERORR.IS_TASK)
   } else {
     list.push({
       name: taskName,
       status: STATUS.IN_PROGRESS,
       priority: PRIORITY.LOW,
-    });
+    })
   }
 }
 
 function changeStatus(taskName, statusName) {
   if (checkTask(taskName)) {
-    list[indexTask(taskName)].status = statusName;
+    list[indexTask(taskName)].status = statusName
   } else {
-    console.log(ERORR.NO_TASK);
+    console.log(ERORR.NO_TASK)
   }
 }
 
 function deleteTask(taskName) {
   if (checkTask(taskName)) {
-    list.splice(indexTask(taskName), 1);
+    list.splice(indexTask(taskName), 1)
   } else {
-    console.log(ERORR.NO_TASK);
+    console.log(ERORR.NO_TASK)
   }
 }
 
 function showList() {
-  let todo = "";
-  let in_progress = "";
-  let done = "";
+  let todo = ''
+  let in_progress = ''
+  let done = ''
 
   for (let taskName of list) {
     if (taskName.status === STATUS.TO_DO) {
-      todo += "\n" + taskName.name;
+      todo += '\n' + taskName.name
     } else if (taskName.status === STATUS.IN_PROGRESS) {
-      in_progress += "\n" + taskName.name;
+      in_progress += '\n' + taskName.name
     } else if (taskName.status === STATUS.DONE) {
-      done += "\n" + taskName.name;
+      done += '\n' + taskName.name
     }
   }
 
   if (!todo) {
-    todo = `\n -`;
+    todo = `\n -`
   } else if (!in_progress) {
-    in_progress = `\n -`;
+    in_progress = `\n -`
   } else if (!done) {
-    done = `\n -`;
+    done = `\n -`
   }
 
-  console.log(`${STATUS.TO_DO}:${todo}`);
-  console.log(`${STATUS.IN_PROGRESS}:${in_progress}`);
-  console.log(`${STATUS.DONE}:${done}`);
+  console.log(`${STATUS.TO_DO}:${todo}`)
+  console.log(`${STATUS.IN_PROGRESS}:${in_progress}`)
+  console.log(`${STATUS.DONE}:${done}`)
 }
 
-addTask("run run run");
-changeStatus("test", STATUS.TO_DO);
-deleteTask("run run run");
+addTask('run run run')
+changeStatus('test', STATUS.TO_DO)
+deleteTask('run run run')
 
-showList();
+showList()
