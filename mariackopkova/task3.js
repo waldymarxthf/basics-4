084af3e5b7e8bd713cf00626789274f7797262c1
mariackopkova <mariakons@mail.ru>
2023-04-10 12:15:09 +0300

    25 Практика. Методы массивов

---
+++ mariackopkova/task3.js
@@ -0,0 +1,4 @@
+const numbers = [1, 11, -2, 3, -10, 4];
+
+const negativNumbers = numbers.filter(number => number < 0);
+console.log(negativNumbers);
---
+++ mariackopkova/task5.js
@@ -0,0 +1,4 @@
+const numbers = [1, 11, -2, 3, -10, 4];
+
+const sortNumbers = numbers.sort((a, b) => a - b);
+console.log(sortNumbers);
