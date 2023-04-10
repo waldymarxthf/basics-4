ccb8da483089c64e0ba120a00095e29fd902a4b8
Vladislao <vlmik@outlook.com>
2023-04-10 18:19:08 +0300

    the first version of to do

--- ladislao/main.js
+++ ladislao/main.js
@@ -1,31 +1,34 @@
 const toDoList = {// хранилище
     list: {
-        "to create to do list": "In progress", // имч задачи: 
+        "to create to do list": "In progress", // имч задачи - ключ объекта значения - статусы
         "to open VScode": "Done",
         "to relax": "To DO",
     },
 
     changeStatus (name, status) {// изменение статуса
         this.list[name] = status;
+        console.log('Статус ' +`${status}` + ' для статуса ' + `${name}` + ' успешно изменен');
     },
 
-    addTask (name, status) {// добавление
+    addTask (name, status) {// добавление элементов
         this.list[name] = status;
+        console.log('Задача ' + `${name}` + ' со статусом ' + `${status}` + ' успешно добавлена')
     },
 
-    deleteTask(name) {// удаление
+    deleteTask(name) {// удаление элементов
         delete this.list[name];
+        console.log('Задача ' + `${name}` + ' была удалена')
     },
 
-    showList() {// Чтение
-        console.log(this.list)// выводит то, что хранится в list
+    showList() {// чтение элементов
+        for (key in this.list){
+            console.log(`${key}: ${this.list[key]}`);
+            
+        }
     }
-
-
 }
 
-toDoList.changeStatus("to create to do list", "To Do");
+toDoList.addTask("to open VScode", "Done");
 toDoList.deleteTask("to relax");
 toDoList.addTask("to work hard", "In progress");
 toDoList.showList();
-//console.log(toDoList);
