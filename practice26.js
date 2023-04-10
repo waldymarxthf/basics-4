f622de1d82b025ab48d401f2bef128a8bd3b3f87
Anatolii <goddevil22@yandex.ru>
2023-04-10 19:17:27 +0400

    add const status, priority

--- practice26.js
+++ practice26.js
@@ -1,15 +1,23 @@
-let morningToDoList = [
-    {name:'проснуться в 4:00',status:'In progress', priority:'high'},
-    {name:'заниматься в Strada',status:'Done', priority:'high'},
-    {name:'позавтракать',status:'In progress', priority:'middle'},
-    {name:'разбудить детей',status:'Done', priority:'high'},
-    {name:'собраться на работу',status:'To do', priority:'low'},
-    {name:'отвезти детей в сад',status:'To do', priority:'high'},
-    {name:'ехать на работу',status:'In progress', priority:'middle'}
+const STATUS = {
+    INPROGRESS:'In progress',
+    DONE:'Done',
+    TODO:'To do'
+};
+
+const PRIORITY = {
+    HIGH:'high',
+    MIDDLE:'middle',
+    LOW:'low'
+};
+
+const morningToDoList = [
+      
 ];
 
     function addTask(name,status,priority){
+       
         morningToDoList.push({name:name, status:status,priority:priority});
+        
     }
     
     function changeStatus(name,status){
@@ -20,6 +28,14 @@ let morningToDoList = [
        }
 
     }
+    function changePriority(name,priority){
+        for (let char of morningToDoList){
+             if (char.name === name){
+                 char.priority = priority;
+             }
+        }
+ 
+     }
 
     function deleteTask(name){
         const indexName = morningToDoList.findIndex(item => item.name === name);
@@ -42,14 +58,16 @@ let morningToDoList = [
     showListTasks('To do');
     showListTasks('In progress');
 }
-    addTask('найти телефон',"Done",'high');
-    deleteTask('проснуться в 4:00');
-    console.log(morningToDoList); 
-    changeStatus('отвезти детей в сад','In progress');
+    addTask('сделать ToDo',STATUS.DONE,PRIORITY.LOW);
+    addTask('использовать методы массивов',STATUS.TODO,PRIORITY.LOW);
+    addTask('понять как использовать методы',STATUS.TODO,PRIORITY.LOW);
+    addTask('сделать проверку',STATUS.TODO,PRIORITY.LOW);
+    deleteTask('сделать проверку'); 
+    changeStatus('понять как использовать методы',STATUS.INPROGRESS);
+    changePriority('понять как использовать методы',PRIORITY.LOW);
     console.log(morningToDoList); 
     showList(); 
-   
-       
+
        
         
 
