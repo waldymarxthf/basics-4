412e81fd72f3570a29e2646a7d9411b695174504
Oleg Zavodtsov <podolski133@gmail.com>
2023-04-10 22:18:24 +0300

    Update task_02.js

--- practice-25/task_02.js
+++ practice-25/task_02.js
@@ -1,3 +1,8 @@
+/*
+Дан массив строк ['cat', 'dog', 'elephant', 'tiger', 'lion']
+найти первое слово длинной больше 7 символов и вывести в консоль
+*/
+
 const animals = ['cat', 'dog', 'elephant', 'tiger', 'lion'];
 
 const longWord = animals.find(animal => animal.length > 7);
