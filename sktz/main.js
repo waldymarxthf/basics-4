64a556f4330ee6330d11cf636e55c82bc527129b
Vladimir <skittez@mail.ru>
2023-04-11 15:45:33 +0300

    Initial commit

--- sktz/main.js
+++ sktz/main.js
@@ -1,53 +1,15 @@
-const toDo = {
-    
-    list: {
-        "Create a new practice task": "In Progress", 
-	    "Make a bed": "Done",
-	    "Write a post": "To Do",
-        "Education": "To Do",
-        "BROKE TASK": "To Do",
-    },
+function showVerticalMessage (text) {
+    let newText = text.slice (0, 7);
 
-    addTask (nameTask) {
-        this.list[nameTask] = "To Do";
-    },
 
-    deleteTask (nameTask) {
-        delete this.list[nameTask];
-    },
-
-    showList () {
-        console.log (">>>>>>>>>>>>>");
-
-        console.log ('To Do');
-        for (const name in toDo.list) {
-            if (this.list[name] == 'To Do') {
-                console.log('  ', name);
-            }
+    for (let symbol of newText) {
+        if (symbol.toLowerCase() == 's') {
+            console.log (symbol.toUpperCase());
+        } else {
+            console.log (symbol);
         }
-
-        console.log ('Done');
-        for (const name in toDo.list) {
-            if (this.list[name] == 'Done') {
-                console.log('  ', name);
-            }
-        }
-
-        console.log ('In Progress');
-        for (const name in toDo.list) {
-            if (this.list[name] == 'In Progress') {
-                console.log('  ', name);
-            }
-        }
-    },
-
-    changeStatus (taskName, newStatus) {
-        this.list[taskName] = newStatus;
     }
 
-};
+}
 
-toDo.addTask ('Write code');
-toDo.deleteTask ('BROKE TASK');
-toDo.changeStatus ('Write a post', 'Done');
-toDo.showList();
+showVerticalMessage ('strada');
