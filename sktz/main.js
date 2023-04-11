4f49a0fefeb3df3e56e729ce3cb0cf02d5e6a80c
Vladimir <skittez@mail.ru>
2023-04-11 12:46:06 +0300

    Initial commit

--- sktz/main.js
+++ sktz/main.js
@@ -1,39 +1,28 @@
-const phoneBook = {
-    "A" : 1,
-    "B" : 2,
-    "C" : 3,
-    "D" : 4,
-    "E" : 5
-};
 
-phoneBook["C"] = 123;
-
-console.log (phoneBook);
-console.log (phoneBook.A);
-console.log (phoneBook["B"]);
-console.log (phoneBook["C"]);
-
-console.log ("----------------");
-
-phoneBook["F"] = 890;
-console.log (phoneBook["F"]);
-
-delete phoneBook["F"];
-console.log (phoneBook["F"]);
-
-console.log ("----------------");
-console.log ("****************");
-
-const Book = {
+const book = {
     list: {
-        "Z" : 100,
-        "X" : 200,
-        "C" : 300
+        "Za" : 100,
+        "Xr" : 200,
+        "Cz" : 300
     },
 
-    log() {
-        console.log (this.list);
+    add (symbol, number) {
+        this.list[symbol] = number;
+    },
+
+    delete (symbol) {
+         delete this.list[symbol];
     }
 };
 
-Book.log();
+book.add ('Vb', 400);
+console.log (book.list);
+
+console.log ('-------------');
+
+book.delete ('Za');
+console.log (book.list);
+
+for (const name in book.list) {
+    console.log (name, '-', book.list[name]);
+}
