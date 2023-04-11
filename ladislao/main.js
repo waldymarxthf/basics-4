ab33e69cc70e273cd023c2f636539a40f53fd619
Vladislao <vlmik@outlook.com>
2023-04-11 12:13:44 +0300

    third version of to Do

--- ladislao/main.js
+++ ladislao/main.js
@@ -1,34 +1,71 @@
-const toDoList = {// хранилище
+const STATUS_TO_DO = {
+    TO_DO: 'To Do',
+    IN_PROGRESS: 'in progress',
+    DONE: 'done',
+};// отдельная константа с объектом для повторяющихся значений
+
+const toDoList = {// хранилище   
     list: {
-        "to create to do list": "In progress", // имч задачи - ключ объекта значения - статусы
-        "to open VScode": "Done",
-        "to relax": "To DO",
+        "to create to do list": STATUS_TO_DO.TO_DO, // имч задачи - ключ объекта значения - статусы
+        "to open VScode": STATUS_TO_DO.DONE,
+        "to relax": STATUS_TO_DO.TO_DO,
     },
 
-    changeStatus (name, status) {// изменение статуса
-        this.list[name] = status;
-        console.log('Статус ' +`${status}` + ' для статуса ' + `${name}` + ' успешно изменен');
+
+    changeStatus (task, status) {// изменение статуса
+        if (task in this.list && status in STATUS_TO_DO ) {// проверка на то, находится ли изменяемая задача в листе, и существует ли статус
+            this.list[task] = STATUS_TO_DO[status];
+            console.log('Статус ' +`${status}` + ' для статуса ' + `${task}` + ' успешно изменен');
+        } else {
+            console.log('Error!!!');
+        }
+        
+        
     },
 
-    addTask (name, status) {// добавление элементов
-        this.list[name] = status;
-        console.log('Задача ' + `${name}` + ' со статусом ' + `${status}` + ' успешно добавлена')
+    addTask (task, status) {// добавление элементов
+        if (typeof task ==='string'){
+        this.list[task] = STATUS_TO_DO[status];
+        console.log('Задача ' + `${task}` + ' со статусом ' + `${status}` + ' успешно добавлена')
+        } else {
+            console.log("Error of types!")
+        };
     },
 
-    deleteTask(name) {// удаление элементов
-        delete this.list[name];
-        console.log('Задача ' + `${name}` + ' была удалена')
+    deleteTask(task) {// удаление элементов
+        delete this.list[task];
+        console.log('Задача ' + `${task}` + ' была удалена');
     },
 
     showList() {// чтение элементов
-        for (key in this.list){
-            console.log(`${key}: ${this.list[key]}`);
+        for (task in this.list){
+            switch(this.list[task]){ 
+            case STATUS_TO_DO.DONE:
+                console.log('Done: ' + `${task},\n`);
+                break;
+            case STATUS_TO_DO.TO_DO:
+                console.log('To Do: ' + `${task},\n`);
+                break;
+            case STATUS_TO_DO.IN_PROGRESS:
+                console.log('In progress: ' + `${task},\n`);
+                break;  
+            default:
+                console.log("Wrong information!");
             
-        }
-    }
+        };
+    }; 
+}
 }
 
-toDoList.addTask("to open VScode", "Done");
-toDoList.deleteTask("to relax");
+/* toDoList.addTask("to open VScode", "Done");
+ toDoList.deleteTask("to relax");
 toDoList.addTask("to work hard", "In progress");
-toDoList.showList();
+*/ 
+toDoList.changeStatus("to open VScode", 'DONE' );// меняем статус
+toDoList.changeStatus("to wash sneakers", 'IN_PROGRESS');// пример того, что ошибка задания нет
+toDoList.changeStatus('to open VScode', 'Ofcourse');// попытка добавления статуса невходящего в ту ду - ошибка
+toDoList.addTask("2", "IN_PROGRESS" ); // сторока с цифрой возможна
+toDoList.addTask("to sleep", "IN_PROGRESS");// добавление обычной задачи
+toDoList.deleteTask("2");// удаляем задачу
+toDoList.addTask("to create a task in progress", 'IN_PROGRESS');// еще одна задача в прогрессе
+toDoList.showList(); // вывод ту ду
