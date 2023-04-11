ebeba872489659e61995c6d1aa0962d7086d0bfa
Vladimir <skittez@mail.ru>
2023-04-11 12:02:52 +0300

    Final

---
+++ sktz/main.js
@@ -0,0 +1,21 @@
+let i = 1;
+
+while (i <= 19) {
+    console.log (i);
+    i++;
+}
+
+console.log ("------------------");
+
+k = 1;
+
+do  {
+    console.log (k);
+    k++;
+} while (k <= 19);
+
+console.log ("------------------");
+
+for (let l = 1; l <= 19; l++) {
+    console.log (l);
+}
