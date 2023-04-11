29f451fcb44afd6ec571db411384889558e6a4d2
Ivan Marahovskii <marahovskii81@gmail.com>
2023-04-12 00:17:42 +0500

    closure

--- istego/main.js
+++ istego/main.js
@@ -1,1 +1,17 @@
-// здесь пока пусто
+function createCounter() {
+    let count = 0;
+
+    return function () {
+        count++;
+        return count;
+    }
+}
+
+let counterA = createCounter();
+let counterB = createCounter();
+
+console.log(counterA()); // 1
+console.log(counterA()); // 2
+console.log(counterA()); // 3
+
+console.log(counterB()); // 1
