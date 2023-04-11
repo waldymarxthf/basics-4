96b819b4610cf85b42369adc3f1504770e301a23
Ruslan Polyanski <ruslapolyanski@yandex.by>
2023-04-11 22:10:07 +0300

    feat: add fucntion closur

---
+++ Ruslan-Palyanski/main.js
@@ -0,0 +1,15 @@
+function createCounter() {
+	let count = 0;
+    return () => {
+        return ++count;
+    }
+}
+
+let counterA = createCounter();
+let counterB = createCounter();
+
+console.log(counterA()); // 1
+console.log(counterA()); // 2
+console.log(counterA()); // 3
+
+console.log(counterB()); // 1
