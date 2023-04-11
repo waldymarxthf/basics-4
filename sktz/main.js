946bca0410103854f825e613b695bc5325807e25
Vladimir <skittez@mail.ru>
2023-04-11 17:00:22 +0300

    Initial commit

--- sktz/main.js
+++ sktz/main.js
@@ -1,16 +1,48 @@
-function showVerticalMessage (text) {
-    let newText = text.slice (0, 7);
 
-        if (newText.startsWith('s')) {
-            console.log ('S');
-            newText = newText.slice (1);
-            //console.log (newText.slice (1));
-        }
-            
-        for (let symbol of newText) {
-            console.log (symbol);
-        }
-
+let numbers = [];
+for (i = 10; i < 100; i = i + 10) {
+    numbers.push (i);
 }
 
-showVerticalMessage ('stradas');
+numbers.forEach (numberItem => {
+    console.log (`Number is ${numberItem}`);
+})
+
+console.log (`
+>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+`);
+
+const animals = ['cat', 'dog', 'elephant', 'tiger', 'lionzzzzzz'];
+const lenghtAnimals = animals.filter (word => word.length > 7);
+console.log (lenghtAnimals[0]);
+
+console.log (`
+>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+`);
+
+const a = [1, 11, -2, 3, -10, 4];
+
+a.forEach (aItem => {
+    if (aItem >= 0) {
+        console.log (`Number is ${aItem}`);
+    }
+})
+
+console.log (`>>>>>>>>>>>>>>>`);
+
+let b = [];
+a.forEach (aItem => {
+    b.push (Math.abs(aItem));
+})
+
+b.forEach (aItem => {
+    console.log (`Number is ${aItem}`);
+})
+
+console.log (`
+>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+`);
+
+const sortNumber = a.sort((c, b) => b - c);
+console.log (sortNumber);
+
