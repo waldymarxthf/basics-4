cea141d2c100ad377ead2f1ca162c786c667c8be
Dmitry Komarov <dmitrykomarov@MacBook-Pro-Dmitry.local>
2023-04-11 09:52:27 +0300

    add file

---
+++ dkomarov/main.js
@@ -0,0 +1,15 @@
+function createCounter() {
+	let count = 0;
+    return function() {
+        return ++count;
+    }
+}
+
+let counterA = createCounter();
+let counterB = createCounter();
+
+console.log(counterA()); 
+console.log(counterA());
+console.log(counterA());
+
+console.log(counterB()); 
