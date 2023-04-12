5824141ebd04e4a4870c2397ac425a0332dfce67
DariaYukseeva <ducha2005@mail.ru>
2023-04-12 17:30:19 +0500

    added setTimeout method

--- DariaYukseeva/main.js
+++ DariaYukseeva/main.js
@@ -1,11 +1,24 @@
+// function printNumbers(from, to) {
+//     let currentNum = from;
+//     let timerId = setInterval(() => {
+//         console.log(currentNum);
+//         if (currentNum === to) {
+//             clearInterval(timerId);
+//         }
+//         currentNum++;
+//     }, 1000);
+// }
+
+
 function printNumbers(from, to) {
     let currentNum = from;
-    let timerId = setInterval(() => {
+    let timerId = setTimeout(function count() {
         console.log(currentNum);
-        if (currentNum === to) {
-            clearInterval(timerId);
-        }
         currentNum++;
+        if (currentNum <= to) {
+            timerId = setTimeout(count, 1000);
+        }
+        
     }, 1000);
 }
 printNumbers(5, 10);
