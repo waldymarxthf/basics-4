0f7e30d25b74839f456c7c1d93cf0519f5ee0694
johnbeelow <beelow-dj@mail.ru>
2023-04-13 10:01:27 +0300

    add

--- johnbeelow/main.js
+++ johnbeelow/main.js
@@ -1,16 +1,30 @@
-function createCounter() {
-  let count = 0
-  return function () {
-    count++
-    return count
+// Напишите функцию printNumbers(from, to), которая выводит число каждую секунду, начиная от from и заканчивая to.
+
+// Сделайте два варианта решения.
+let countStart = 0
+let countEnd = 5
+
+
+// Используя setInterval.
+function printNumbers(from, to) {
+  from = ++countStart
+  to = countEnd
+  let timerId = setInterval(printNumbers, 1000, 0, 5)
+  console.log(countStart)
+  if (from === to) {
+    clearInterval(timerId)
   }
 }
 
-let counterA = createCounter()
-let counterB = createCounter()
+// Используя рекурсивный setTimeout.
+setTimeout(function printNumbers(from, to) {
+  from = ++countStart
+  to = countEnd
+  console.log(countStart)
+  let timerId = setTimeout(printNumbers, 1000)
+  if (from === to) {
+    clearTimeout(timerId)
+  }
+},1000)
 
-console.log(counterA()) 
-console.log(counterA()) 
-console.log(counterA()) 
 
-console.log(counterB()) 
