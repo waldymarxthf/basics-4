6be31ca2c926f93e2473a7d6bf5338f728904de3
Ivan Marahovskii <marahovskii81@gmail.com>
2023-04-13 10:09:52 +0500

    istego/practice-31

--- istego/main.js
+++ istego/main.js
@@ -1,1 +1,28 @@
-// здесь пока пусто
+// #31 Таймеры и Интервалы
+function printNumbers(from, to) {
+    let timerId = setInterval(() => {
+        console.log(from);
+        console.log(timerId);
+        if (from === to) {
+            clearInterval(timerId);
+        }
+        from++;
+    }, 1000)
+}
+
+printNumbers(5, 10);
+
+// =====================================================================
+
+function printNumbers(from, to) {
+    setTimeout(function go() {
+        console.log(from);
+        if (from < to) {
+            setTimeout(go, 1000)
+        }
+        from++;
+    }, 1000)
+}
+
+printNumbers(5, 10);
+
