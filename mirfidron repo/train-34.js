4a90a295a299e15fb644fbecd3417ddeeea40a84
mirfidron <mirfidron@mail.ru>
2023-04-17 13:05:06 +0300

    code wars ~ v.1

---
+++ mirfidron repo/train-34.js
@@ -0,0 +1,15 @@
+function buildFun(n) {
+  const res = []
+
+  for (let i = 0; i < n; i++) {
+      res.push(function () {
+          return i
+      })
+  }
+  return res
+}
+
+for (let i = 0; i < 10; i++) {
+  console.log(buildFun(10)[i]());
+}
+    
