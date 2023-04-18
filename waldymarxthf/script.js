6c7c2e9790d0cbc84f32c472a989118d3c1bea79
waldymarxthf <iliya.nov223@gmail.com>
2023-04-18 16:45:58 +0300

    [feat] feat: init project

---
+++ waldymarxthf/script.js
@@ -0,0 +1,64 @@
+const timer = document.querySelector('#timer');
+const btnResetTimer = document.querySelector('#resetTimer');
+
+let stopwatch;
+let isRunning = timer.dataset.include === 'off';
+let count = 0;
+
+function startTimer() {
+
+	stopwatch = setInterval(() => {
+		count++;
+		console.clear();
+		console.log(count);
+		updateTimerUI(count)
+	}, 1000);
+}
+
+function stopTimer() {
+
+	clearInterval(stopwatch);
+	console.clear();
+	console.log(`Вы остановили таймер на ${count}`);
+}
+
+function toggleButtonState(isRunning) {
+
+	if (isRunning) {
+		timer.dataset.include = 'off';
+		timer.innerText = 'Запуск';
+		timer.style.backgroundColor = '#addfad';
+	} else {
+		timer.dataset.include = 'on';
+		timer.innerText = 'Остановить';
+		timer.style.backgroundColor = '#e66761';
+	}
+
+}
+
+function addSizeEffect() {
+	timer.style.transform = 'scale(0.90)';
+	setTimeout(() => timer.style.transform = 'scale(1)', 100);
+}
+
+function updateTimerUI(count) {
+	timer.innerText = count;
+}
+
+function resetTimer() {
+	count = 0;
+	console.log(`Вы сбросили секундомер`);
+}
+
+function buttonClickTimer() {
+	isRunning ? startTimer() : stopTimer();
+	isRunning = !isRunning;
+	toggleButtonState(isRunning)
+}
+
+timer.addEventListener('click', () => {
+	buttonClickTimer(),
+	addSizeEffect()
+});
+btnResetTimer.addEventListener('click', resetTimer);
+
