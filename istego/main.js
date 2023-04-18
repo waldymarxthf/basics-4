d0faff2f45a15c28383f26079e5c04039361a3b4
Ivan Marahovskii <marahovskii81@gmail.com>
2023-04-18 17:35:16 +0500

    istego/practice-37

--- istego/main.js
+++ istego/main.js
@@ -1,1 +1,54 @@
-// здесь пока пусто
+// На странице есть кнопка “Секундомер”. 
+// При нажатии на кнопку необходимо запускать “секундомер” и выводить в консоль время каждую секунду. 
+// Также необходимо предусмотреть возможность паузы / возобновления при нажатии на кнопку еще раз.
+// Для решения этой задачи вам понадобится использовать функции setInterval и clearInterval для запуска и остановки таймера, их вы уже знаете
+
+const btnTime = document.querySelector('.btn-time');
+const btnReset = document.querySelector('.btn-reset');
+const result = document.querySelector('.result');
+
+let included = true;
+let countTime = 1;
+let timerId;
+
+btnTime.addEventListener('click', startProgramm);
+
+// Запуск программы
+function startProgramm() {
+    if (included) {
+        startTime();
+    } else {
+        stopTime();
+    }
+}
+
+// Запуск таймера
+function startTime() {
+    timerId = setInterval(() => {
+        result.textContent = `${countTime++}`
+    }, 1000);
+    included = false;
+    btnTime.style.backgroundColor = '#d4e114';
+    btnTime.textContent = 'Pause';
+    btnReset.addEventListener('click', resetTime);
+    btnReset.removeAttribute("disabled");
+    btnReset.style.backgroundColor = '#e04414';
+}
+
+// Остановка таймера
+function stopTime() {
+    clearInterval(timerId);
+    included = true;
+    btnTime.style.backgroundColor = '#8ee014';
+    btnTime.textContent = 'Start';
+}
+
+// Сброс программы и значения
+function resetTime() {
+    stopTime();
+    countTime = 1;
+    btnReset.removeEventListener('click', resetTime);
+    btnReset.setAttribute("disabled", "true");
+    result.textContent = "-- : -- : --";
+    btnReset.style.backgroundColor = '#acabab';
+}
