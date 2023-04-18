d3f8361abad5a73b5248dfdf96c31dc74ab0d829
johnbeelow <beelow-dj@mail.ru>
2023-04-18 16:51:47 +0300

    add 37

--- johnbeelow/js/main.js
+++ johnbeelow/js/main.js
@@ -1,1 +1,42 @@
-//SOON
+const UI_ELEMENTS = {
+    BUTTON_START: document.getElementById('stopwatch_start'),
+    BUTTON_PAUSE: document.getElementById('stopwatch_pause'),
+    BUTTON_STOP: document.getElementById('stopwatch_stop'),
+  }
+  
+  let counter = 0
+  let timerId
+  
+  function buttonClickPlay() {
+    timerId = setInterval(() => {
+      counter++
+      console.log(counter + ' sec.')
+    }, 1000)
+
+    UI_ELEMENTS.BUTTON_START.disabled = true
+    UI_ELEMENTS.BUTTON_PAUSE.disabled = false
+    UI_ELEMENTS.BUTTON_STOP.disabled = false
+  }
+  
+  function buttonClickPause() {
+    clearInterval(timerId)
+    console.log('Пауза')
+
+    UI_ELEMENTS.BUTTON_START.disabled = false
+    UI_ELEMENTS.BUTTON_PAUSE.disabled = true
+    UI_ELEMENTS.BUTTON_STOP.disabled = false
+  }
+  
+  function buttonClickStop() {
+    clearInterval(timerId)
+    counter = 0
+    console.log('Стоп')
+    
+    UI_ELEMENTS.BUTTON_START.disabled = false
+    UI_ELEMENTS.BUTTON_PAUSE.disabled = false
+    UI_ELEMENTS.BUTTON_STOP.disabled = true
+  }
+  
+  UI_ELEMENTS.BUTTON_START.addEventListener('click', buttonClickPlay)
+  UI_ELEMENTS.BUTTON_PAUSE.addEventListener('click', buttonClickPause)
+  UI_ELEMENTS.BUTTON_STOP.addEventListener('click', buttonClickStop)
