371765ffd411369aa30b776554ac9a77e1c5b6ba
Maria Verma <vermochka@gmail.com>
2023-04-19 00:41:26 +0600

    task 37 done

--- vermarvel/timer.js
+++ vermarvel/timer.js
@@ -1,23 +1,72 @@
-// via setTimeout
-const printNumbers = function (from, to) {
-  setTimeout(function log() {
-    if (from <= to) {
-      console.log(from++);
-      setTimeout(log, 1000);
-    }
-  }, 1000);
-};
+// UI Elements
+const startBtn = document.getElementById(`start`);
+const pauseBtn = document.getElementById(`pause`);
+const stopBtn = document.getElementById(`stop`);
+const timeField = document.getElementById(`time-field`);
 
-//printNumbers(1, 8);
+// State variables
+let time = 0;
+let timerId;
+// let pauseOn = false;
 
-// via setInterval
-const printNumbers2 = function (from, to) {
-  let timerId = setInterval(function log() {
-    if (from <= to) {
-      console.log(from++);
-    }
-  }, 1000);
-  from > to ? clearInterval(timerId) : ``;
-};
+// Process
+function addTime() {
+  timerId = setInterval(() => {
+    time++;
+    updTimeField();
+  }, 900);
+}
 
-//printNumbers2(1, 12);
+function start() {
+  addTime();
+  hideStartShowPause();
+  // pauseOn = false;
+}
+
+function pause() {
+  clearInterval(timerId);
+  hidePauseShowStart();
+  // pauseOn = true;
+}
+
+function stop() {
+  time = 0;
+  clearInterval(timerId);
+  // pauseOn = false;
+  timeField.textContent = "00.00.00";
+  hidePauseShowStart();
+}
+
+// HELPER FUNCTIONS
+
+function hideStartShowPause() {
+  startBtn.style.display = "none";
+  pauseBtn.style.display = "block";
+}
+
+function hidePauseShowStart() {
+  pauseBtn.style.display = "none";
+  startBtn.style.display = "block";
+}
+
+function timeToStr(time) {
+  const hours = Math.floor(time / 3600);
+  const minutes = Math.floor((time - hours * 3600) / 60);
+  const seconds = time - hours * 3600 - minutes * 60;
+
+  const strHours = hours.toString().padStart(2, "0");
+  const strMinutes = minutes.toString().padStart(2, "0");
+  const strSeconds = seconds.toString().padStart(2, "0");
+  return `${strHours}.${strMinutes}.${strSeconds}`;
+}
+
+function updTimeField() {
+  const timeString = timeToStr(time);
+  timeField.textContent = timeString;
+}
+
+// Events
+
+startBtn.addEventListener(`click`, start);
+pauseBtn.addEventListener(`click`, pause);
+stopBtn.addEventListener(`click`, stop);
