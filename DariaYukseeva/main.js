66b3c8be26ebc9421b3a8aa088e738ed9eb03000
DariaYukseeva <ducha2005@mail.ru>
2023-04-19 00:47:00 +0500

    added style.css, optimized main.js

--- DariaYukseeva/main.js
+++ DariaYukseeva/main.js
@@ -3,11 +3,11 @@ const stopwatchBtnStop = document.getElementById('stopwatch-btn-stop');
 const stopwatchBtnReset = document.getElementById('stopwatch-btn-reset');
 
 
+let stopwatchStatus = false;
+let time = 0;
 let sec = 0;
 let min = 0;
 let hour = 0;
-let limitStep = 59;
-let limitStepForHours = 23;
 let secOut = '';
 let minOut = '';
 
@@ -15,41 +15,34 @@ let timeId;
 
 // Запускаем отсчёт времени и вызываем функцию вывода в консоль каждую секунду
 function startCount() {
-    timeId  = setInterval(() => {
-        stepTime();
-        showStopwatch();
-    }, 1000)
+    if (stopwatchStatus === false) {
+        
+        timeId  = setInterval(() => {
+            stepTime();
+            refreshStopwatchDisplay();
+        }, 1000)
+        stopwatchStatus = true;
+    }
+        
 }
 
 // Останавливаем секундомер
 function stopCount() {
     clearInterval(timeId);
+    stopwatchStatus = false;
 }
 
 // Изменяем время секундомера
 function stepTime() {
-    if (sec < limitStep) {
-        sec++;
-    }
-    else if (sec >= limitStep) {
-        sec = sec - limitStep;
-        if (min < limitStep) {
-            min++;
-        }
-        else if (min >= limitStep) {
-            min = min - limitStep;
-            if (hour < limitStepForHours) {
-                hour++;
-            }
-            else if (hour >= limitStepForHours) {
-                hour = hour - limitStepForHours;
-            }
-        }
-    }
+    time++;
+    sec = time % 60;
+    min = Math.floor(time / 60);
+    hour = Math.floor(time / 3600);
+
 }
 
 // Если значение секунд и минут меньше 10, то добавляем к нему 0 в вывод. Выводим секундомер в консоль
-function showStopwatch() {
+function refreshStopwatchDisplay() {
     if (sec < 10 && min < 10) {
         secOut = `0${sec}`;
         minOut = `0${min}`;
@@ -66,19 +59,26 @@ function showStopwatch() {
         secOut = sec;
         minOut = min;
     }
-
+    console.clear();
     console.log(`${hour}:${minOut}:${secOut}`);
+    document.querySelector('.out').innerHTML = `${hour}:${minOut}:${secOut}`;
 }
 
 // Останавливаем отсчёт и сбрасываем показания секундомера
 function resetCount() {
     stopCount();
+    time = 0;
     sec = 0;
     min = 0;
     hour = 0;
-    showStopwatch()
+    refreshStopwatchDisplay()
+    
 }
+
+
 
 stopwatchBtnStart.addEventListener('click', startCount);
 stopwatchBtnStop.addEventListener('click', stopCount);
 stopwatchBtnReset.addEventListener('click', resetCount);
+
+
