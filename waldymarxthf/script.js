7b4ac56a67715c0d1a1761a102e64903a6443a80
waldymarxthf <iliya.nov223@gmail.com>
2023-04-19 16:59:23 +0300

    [fix] fix: some bugs

--- waldymarxthf/script.js
+++ waldymarxthf/script.js
@@ -6,52 +6,40 @@ let answer = document.querySelector('#answer')
 
 
 function sum(firstNumber, secondNumber) {
-	if (secondNumber.trim().length === 0) {
-		return isNumber(answer.textContent = firstNumber + firstNumber)
-	}
 	return isNumber(answer.textContent = firstNumber + secondNumber)
 }
 
-//* функция суммы 2 чисел с проверкой на пустую строку
-//* если во 2 значении передана пустая строка, то 1 число складывается само с собой
+//* функция суммы 2 чисел
 
 function difference(firstNumber, secondNumber) {
-	if (secondNumber.trim().length === 0) {
-		return isNumber(answer.textContent = firstNumber - firstNumber)
-	}
 	return isNumber(answer.textContent = firstNumber - secondNumber)
 }
 
-//* функция разницы 2 чисел с проверкой на пустую строку
-//* если во 2 значении передана пустая строка, то 1 число отнимается само от себя
+//* функция разницы 2 чисел
 
 function multiply(firstNumber, secondNumber) {
-	if (secondNumber.trim().length === 0) {
-		return isNumber(answer.textContent = firstNumber * firstNumber)
-	}
 	return isNumber(answer.textContent = firstNumber * secondNumber)
 }
 
-//* функция умножения 2 чисел с проверкой на пустую строку
-//* если во 2 значении передана пустая строка, то 1 число умножается само с собой
+//* функция умножения 2 чисел
 
 function divide(firstNumber, secondNumber) {
-	if (secondNumber.trim().length === 0) {
-		return isNumber(answer.textContent = firstNumber / firstNumber)
-	}
+
 	if (secondNumber === 0) {
 		return answer.textContent = 'На ноль делить нельзя'
 	}
+
 	return isNumber(answer.textContent = firstNumber / secondNumber)
 }
 
-//* функция деления 2 чисел c проверкой деления на ноль и также проверка на пустую строку
-//* если во 2 значении передана пустая строка, то 1 число делится само на себя
+//* функция деления 2 чисел c проверкой деления на ноль
 
 function isNumber(number) {
+
 	if (isNaN(number)) {
 		answer.textContent = 'Введите числовое значение'
 	}
+
 	return number
 }
 
@@ -64,8 +52,9 @@ function getInputValue(input) {
 //* функция которая получает value от элемента
 
 function math() {
+
 	let first = +getInputValue(firstNumber)
-	let second = +getInputValue(secondNumber) || ''
+	let second = +getInputValue(secondNumber)
 	let symbol = getInputValue(mathSymbol)
 
 	switch(symbol) {
@@ -82,6 +71,7 @@ function math() {
 			divide(first, second)
 			break
 	}
+	
 }
 
 //* функция для подсчета всех операций
