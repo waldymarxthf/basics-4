678e4cddf83416d5358742f83272f5d792eab38b
waldymarxthf <iliya.nov223@gmail.com>
2023-04-19 16:23:48 +0300

    [chore] chore: add func getInputValue and some fixes

--- waldymarxthf/script.js
+++ waldymarxthf/script.js
@@ -1,33 +1,52 @@
+const firstNumber = document.querySelector('#firstNumber')
+const secondNumber = document.querySelector('#secondNumber')
+const mathSymbol = document.querySelector('#mathSymbol')
 const equals = document.querySelector('#button')
 let answer = document.querySelector('#answer')
 
 
 function sum(firstNumber, secondNumber) {
+	if (secondNumber.trim().length === 0) {
+		return isNumber(answer.textContent = firstNumber + firstNumber)
+	}
 	return isNumber(answer.textContent = firstNumber + secondNumber)
 }
 
-//* функция суммы 2 чисел
+//* функция суммы 2 чисел с проверкой на пустую строку
+//* если во 2 значении передана пустая строка, то 1 число складывается само с собой
 
 function difference(firstNumber, secondNumber) {
+	if (secondNumber.trim().length === 0) {
+		return isNumber(answer.textContent = firstNumber - firstNumber)
+	}
 	return isNumber(answer.textContent = firstNumber - secondNumber)
 }
 
-//* функция разницы 2 чисел
+//* функция разницы 2 чисел с проверкой на пустую строку
+//* если во 2 значении передана пустая строка, то 1 число отнимается само от себя
 
 function multiply(firstNumber, secondNumber) {
+	if (secondNumber.trim().length === 0) {
+		return isNumber(answer.textContent = firstNumber * firstNumber)
+	}
 	return isNumber(answer.textContent = firstNumber * secondNumber)
 }
 
-//* функция умножения 2 чисел
+//* функция умножения 2 чисел с проверкой на пустую строку
+//* если во 2 значении передана пустая строка, то 1 число умножается само с собой
 
 function divide(firstNumber, secondNumber) {
+	if (secondNumber.trim().length === 0) {
+		return isNumber(answer.textContent = firstNumber / firstNumber)
+	}
 	if (secondNumber === 0) {
 		return answer.textContent = 'На ноль делить нельзя'
 	}
 	return isNumber(answer.textContent = firstNumber / secondNumber)
 }
 
-//* функция деления 2 чисел
+//* функция деления 2 чисел c проверкой деления на ноль и также проверка на пустую строку
+//* если во 2 значении передана пустая строка, то 1 число делится само на себя
 
 function isNumber(number) {
 	if (isNaN(number)) {
@@ -38,23 +57,29 @@ function isNumber(number) {
 
 //* функция для проверки числа на число
 
-function math() {
-	const firstNumber = Number(document.querySelector('#firstNumber').value)
-	const secondNumber = Number(document.querySelector('#secondNumber').value)
-	const mathSymbol = document.querySelector('#mathSymbol').value
+function getInputValue(input) {
+	return input.value
+}
 
-	switch(mathSymbol) {
+//* функция которая получает value от элемента
+
+function math() {
+	let first = +getInputValue(firstNumber)
+	let second = +getInputValue(secondNumber) || ''
+	let symbol = getInputValue(mathSymbol)
+
+	switch(symbol) {
 		case '+':
-			sum(firstNumber, secondNumber)
+			sum(first, second)
 			break;
 		case '-':
-			difference(firstNumber, secondNumber)
+			difference(first, second)
 			break;
 		case '*':
-			multiply(firstNumber, secondNumber)
+			multiply(first, second)
 			break;
 		case '/':
-			divide(firstNumber, secondNumber)
+			divide(first, second)
 			break
 	}
 }
