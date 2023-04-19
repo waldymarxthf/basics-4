3473e33a2870086c729f05858d287d159b5003d0
YAcanai <nikitacanai@yandex.ru>
2023-04-19 22:43:18 +0700

    #38 Калькулятор

---
+++ script.js
@@ -0,0 +1,57 @@
+const firstNum = document.getElementById('numb1');
+const secondNum = document.getElementById('numb2');
+const select = document.getElementById('select');
+const equal = document.getElementById('resultButton');
+const result = document.getElementById('result');
+
+const OPERATIONS = {
+    sum: '+',
+    minus: '-',
+    mult: '*',
+    del: '/',
+}
+
+function sum(a,b){
+    return a+b
+}
+
+function minus(a,b){
+    return a-b
+}
+
+function mult(a,b){
+    return a*b
+}
+
+function del(a,b){
+    return a/b
+}
+
+function calc(a,b,operation) {
+    let give = null;
+    switch (operation) {
+        case OPERATIONS.sum:
+            give = sum(a,b);
+            break;
+        case OPERATIONS.minus:
+            give = minus(a,b);
+            break;
+        case OPERATIONS.mult:
+            give = mult(a,b);
+            break;
+        case OPERATIONS.del:
+            give = del(a,b);  
+            break;
+        default:
+            break;
+    }
+    return give;
+}
+
+equal.addEventListener('click', function(){
+    const a = Number(firstNum.value);
+    const b = Number(secondNum.value);
+    const operation = select.value;
+    const give = calc(a,b,operation);
+    result.innerHTML = give;
+})
