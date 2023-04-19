c086f836c30a5182b3f0951a2ef91ff37088e5ed
Kirill-Tkachenko242 <110831941+Kirill-Tkachenko242@users.noreply.github.com>
2023-04-19 10:26:25 +0300

    naming

--- kirilltk/main.js
+++ kirilltk/main.js
@@ -1,33 +1,14 @@
 
-// let hours = 0;
-// let minutes = 0;
-// let sec = 0;
-// function startCalculator() {
-
-// 	sec++;
-// 	if (sec >= 60) {
-// 		sec = 0;
-// 		minutes += 1;
-// 	};
-// 	if (minutes >= 60) {
-// 		minutes += 0;
-// 		hours +=1;
-// 	};
-// 	console.log(`${hours}:${minutes}:${sec}`);
-// };
-// const calc = setInterval(startCalculator, 1000);
-
-
-let calculator = document.getElementById("calculatorBtn");
-let stop = document.getElementById("stopCalc");
-let drop = document.getElementById("stopCalc");
+let timer = document.getElementById("timerBtn");
+let stop = document.getElementById("stopTimer");
+let drop = document.getElementById("stopTimer");
 let hours = 0;
 let minutes = 0;
 let sec = 0;
-let CalcId;
-let isTimerRunning = false;
+let timerId;
 
-function calc() {
+
+function timer() {
     sec++;
     if (sec >= 60) {
         sec = 0;
@@ -39,19 +20,19 @@ function calc() {
     };
     console.log(`${hours}:${minutes}:${sec}`);
 };
-function startCalc() {
-    CalcId = setInterval(calc, 1000);
+function startTimer() {
+    timerId = setInterval(timer, 1000);
 
 };
-function pauseCalc() {
-	clearInterval(CalcId);
+function pauseTimer() {
+	clearInterval(timerId);
 
 };
-function dropCalc() {
+function dropTimer() {
 	hours = 0;
 	minutes = 0;
 	sec = 0;
 };
-calculator.addEventListener('click', startCalc);
-stop.addEventListener('click', pauseCalc);
-drop.addEventListener('click', dropCalc);
+timer.addEventListener('click', startTimer);
+stop.addEventListener('click', pauseTimer);
+drop.addEventListener('click', dropTimer);
