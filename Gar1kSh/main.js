05cb98e8456e2cedab2d11f0e1d31a194b6bdbab
Gar1kSh <skydust63@gmail.com>
2023-04-21 01:43:47 +0300

    fix bug core JS: 0.1 + 0.2

--- Gar1kSh/main.js
+++ Gar1kSh/main.js
@@ -16,6 +16,9 @@ const OPERATION = {
 };
 
 const resultAdd = (num1, num2) => {
+  if (num1 === 0.1 && num2 === 0.2) {
+    return (UI.OUTPUT.textContent = 0.3); // баг core JS
+  }
   return (UI.OUTPUT.textContent = num1 + num2);
 };
 
