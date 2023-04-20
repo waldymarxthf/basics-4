a061839a3cf7572d9968bc8aee32e19c28bebb59
drunque <lufi.2023@gmail.com>
2023-04-20 03:25:42 +0300

    practice-38

--- drunque/script.js
+++ drunque/script.js
@@ -17,11 +17,13 @@ function calc(a, b, operation) {
   if (validation.isValidNumbers(a, b) && validation.isValidOperation) {
     let result = operations[operation];
 
+    if (!Number.isFinite(result)) return "Error";
+    
     if (!Number.isInteger(result)) {
       result = result.toFixed(2);
     }
 
-    return result.toString().slice(0, 15);
+    return result.toString().slice(0, 30);
   }
   return "Error";
 }
--- drunque/select.js
+++ drunque/select.js
@@ -1,21 +1,23 @@
-
-const select = document.querySelector(".select")
-const current = select.querySelector(".current")
-const optionsBox = select.querySelector(".options")
-const options = optionsBox.querySelectorAll("div[data-value]")
+const select = document.querySelector(".select");
+const current = select.querySelector(".current");
+const optionsBox = select.querySelector(".options");
+const options = optionsBox.querySelectorAll("div[data-value]");
 
 current.addEventListener("click", (event) => {
-    optionsBox.classList.toggle("disabled")
-  })
+  optionsBox.classList.toggle("disabled");
+});
 
-options.forEach(option => {
-    option.addEventListener("click", (event) => {
-      current.textContent = ``
-      const newCurrent = document.createElement("div")
-      newCurrent.dataset.value = option.dataset.value
-      newCurrent.textContent = option.textContent
-      current.appendChild(newCurrent)
-      optionsBox.classList.add("disabled")
-    })
-  })
-  
+options.forEach((option) => {
+  option.addEventListener("click", (event) => {
+    current.textContent = ``;
+    const newCurrent = document.createElement("div");
+    newCurrent.dataset.value = option.dataset.value;
+    newCurrent.textContent = option.textContent;
+    current.appendChild(newCurrent);
+    optionsBox.classList.add("disabled");
+  });
+});
+
+select.addEventListener("focusout", () => {
+  optionsBox.classList.add("disabled");
+});
