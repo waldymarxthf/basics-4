aa267a4eff10753601e2aac03beed58bc0e9b6ce
Stikast <coldstikast@gmail.com>
2023-04-20 17:30:11 +0300

    add styles and HTML output

--- stikast/main.js
+++ stikast/main.js
@@ -1,26 +1,84 @@
 const startButton = document.querySelector('.start');
-const pauseButton = document.querySelector('.pause');
-const stopButton = document.querySelector('.stop');
+const stopButton = document.querySelector('.reset');
+const output = document.querySelector('.output');
 
-let i = 0;
+let seconds = 0;
+let minutes = 0;
+let splitSeconds = 0;
+
 let timerId;
+let isStarted = false;
+
+function  updatingOutput() {
+	if (splitSeconds > 99) {
+		seconds += 1;
+		splitSeconds = 0;
+	}
+
+	if (seconds > 59) {
+		minutes += 1;
+		seconds = 0;
+	}
+
+	if (minutes < 10 && seconds < 10 && splitSeconds < 10) {
+		return output.textContent = `0${minutes} : 0${seconds}, 0${splitSeconds}`
+	}
+	if (minutes < 10 && seconds < 10 && splitSeconds >= 10) {
+		return output.textContent = `0${minutes} : 0${seconds}, ${splitSeconds}`
+	}
+	if (minutes < 10 && seconds >= 10 && splitSeconds < 10) {
+		return output.textContent = `0${minutes} : ${seconds}, 0${splitSeconds}`
+	}
+	if (minutes < 10 && seconds >= 10 && splitSeconds >= 10) {
+		return output.textContent = `0${minutes} : ${seconds}, ${splitSeconds}`
+	}
+	if (minutes >= 10 && seconds < 10 && splitSeconds < 10) {
+		return output.textContent = `${minutes} : 0${seconds}, 0${splitSeconds}`
+	}
+	if (minutes >= 10 && seconds >= 10 && splitSeconds < 10) {
+		return output.textContent = `${minutes} : ${seconds}, 0${splitSeconds}`
+	}
+	if (minutes >= 10 && seconds < 10 && splitSeconds >= 10) {
+		return output.textContent = `${minutes} : 0${seconds}, ${splitSeconds}`
+	}
+	if (minutes >= 10 && seconds >= 10 && splitSeconds >= 10) {
+		return output.textContent = `0${minutes} : 0${seconds},${splitSeconds}`
+	}
+}
 
 function start() {
-	timerId = setInterval(() => {
-		++i;
-		console.log(i);
-	}, 1000)
+	if (!isStarted) {
+		timerId = setInterval(() => {
+			++splitSeconds;
+			updatingOutput();
+	}, 10)
+
+		isStarted = true;
+		startButton.style.background = '#D8493D'
+		startButton.textContent = 'Stop'
+
+	} else {
+		clearInterval(timerId);
+		isStarted = false;
+		startButton.style.background = '#69B67A'
+		startButton.textContent = 'Start'
+	}
 }
 
 function pause() {
 	clearInterval(timerId)
 }
 
-function stop() {
+function reset() {
 	clearInterval(timerId);
-	i = 0;
+	splitSeconds = 0;
+	seconds = 0;
+	minutes = 0;
+	isStarted = false;
+	startButton.textContent = 'Start';
+	startButton.style.background = '#69B67A'
+	output.textContent = `0${minutes} : 0${seconds}, 0${splitSeconds}`
 }
 
 startButton.addEventListener('click', start)
-pauseButton.addEventListener('click', pause)
-stopButton.addEventListener('click', stop)
+stopButton.addEventListener('click', reset)
