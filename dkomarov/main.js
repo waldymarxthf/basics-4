b89403577fe1d4e4619ce1d49310d7eeab164046
Dmitry Komarov <dmitrykomarov@MacBook-Pro-Dmitry.local>
2023-04-20 22:01:12 +0300

    add change color button

--- dkomarov/main.js
+++ dkomarov/main.js
@@ -1,27 +1,41 @@
 const buttonStart = document.querySelector("#btn-start");
 const buttonStop = document.querySelector("#btn-stop");
 const btnWrap = document.querySelector("#btnWrap");
+const btnWrapStart = document.querySelector("#wrap-btn_start");
+const btnWrapReset = document.querySelector("#wrap-btn_reset");
+const hoursValue = document.querySelector("#value-hours");
+const minutesValue = document.querySelector("#value-minutes");
+const secondsValue = document.querySelector("#value-seconds");
+const changeColorBtn = document.querySelector("#change-color");
 
 let timer;
 let hrs = 0;
 let min = 0;
-let sec = 0;
+let sec = 1;
 
 function runTimer() {
     timer = setTimeout(function run() {
-        console.log(`hrs: ${hrs} min: ${min} sec: ${++sec}`);
+        console.log(`hrs: ${hrs} min: ${min} sec: ${sec}`);
+        hoursValue.textContent = hrs.toString().padStart(2, '0');
+        minutesValue.textContent = min.toString().padStart(2, '0');
+        secondsValue.textContent = sec.toString().padStart(2, '0');
+        sec++;
+        
         if (sec >= 60) {
-            console.log(++min);
+            console.log(min);
+            min++
             sec = 0;
         };
+        
         if (min >= 60) {
-            console.log(++hrs);
+            hrs++
+            console.log(hrs);
             min = 0;
+            sec = 0;
         }
         timer = setTimeout(run, 1000);
     }, 1000)
 };
-
 
 buttonStart.addEventListener('click', () => {
     runTimer();
@@ -35,8 +49,29 @@ buttonStop.addEventListener('click', () => {
     buttonStop.classList.add('hidden');
     btnWrap.classList.remove('hidden');
     buttonStart.classList.add('hidden');
-   
+});
 
+function buttonClickReset() {
+    sec = (secondsValue.textContent = "00") ? 0 : false;
+    min = (minutesValue.textContent = "00") ? 0 : false; 
+    hrs = (hoursValue.textContent = "00") ? 0 : false; 
+
+};
+
+btnWrapStart.addEventListener('click', () => {
+    runTimer();
+    btnWrap.classList.add('hidden');
+    buttonStop.classList.remove('hidden');
+});
+btnWrapReset.addEventListener('click', () => {
+    buttonClickReset();
+    btnWrap.classList.add('hidden');
+    buttonStart.classList.remove('hidden');
+});
+
+changeColorBtn.addEventListener('click', () => {
+    
+    console.log("color")
 })
 
 
