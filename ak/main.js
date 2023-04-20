eeebb954b067f26e1415b847179d637c96eb65ca
AndreyKubarenko <akubarenko1986@gmail.com>
2023-04-20 21:00:06 +0200

    #26 TODO на массивах

--- ak/main.js
+++ ak/main.js
@@ -14,22 +14,97 @@ const list = [
   {name: 'test', status: STATUS.DONE, priority: PRIORITY.HIGH} 
 ];
 
-function addTask(taskName, priority){
-   // const isTaskInArray = list.includes(); как найти задачу через метод includes?
-   const isTaskInArray = list.find(item => item.name === taskName);
+function addTask(name, priority){
+   // const isTaskInArray = list.map(item => item.name).includes(taskName);
+   const isTaskInArray = list.find(item => item.name === name);
 
    if (isTaskInArray){
-      console.log(`Задача "${taskName}" уже есть в списке`)
+      console.log(`Задача "${name}" уже есть в списке`)
    } else {
       const newTask = {
-         name : taskName ,
+         name ,
          status : STATUS.TODO ,
          priority ,
       }
       list.push(newTask);
+      console.log(`Задача "${name}" добавлена`)
    }
 }
 
-addTask('test','high');
-addTask('to do the task','high');
+function changeStatus(taskName, statusName){
+   const isTaskInArray = list.findIndex(item => item.name === taskName);
+
+   if(isTaskInArray!=-1){
+      list[isTaskInArray].status = statusName;
+      console.log(`Статус задачи "${taskName}" изменён на "${statusName}" `)
+   } else {
+      console.log(`Задачи "${taskName}" нет в списке`)
+   }
+}
+
+function deleteTask (taskName) {
+   let task = list.findIndex(item => item.name === taskName);
+   if(task !== -1){
+      list.splice(task,1);
+      console.log(`Задача "${taskName}" удалена`)
+   } else {
+      console.log(`Задачи "${taskName}" нет в списке`)
+   }
+}
+
+function showList () {
+   let todo = '';
+   let in_progress = '';
+   let done = '';
+
+   console.log('To Do:')
+   list.forEach(item => {
+      if (item.status === STATUS.TODO){
+         todo += '\t' + item.name + '\n'
+      }
+   })
+
+   if(todo == false) {
+      console.log(todo + '\t-')
+   }
+   console.log(todo)
+
+   console.log('In Progress:')
+   list.forEach(item => {
+      if (item.status === STATUS.IN_PROGRESS){
+         in_progress += '\t' + item.name + '\n'
+      }
+   })
+
+   if(in_progress == false) {
+      console.log(in_progress + '\t-')
+   }
+   console.log(in_progress)
+
+   console.log('Done:')
+   list.forEach(item => {
+      if (item.status === STATUS.DONE){
+         done += '\t' + item.name + '\n'
+      }
+   })
+
+   if(done == false) {
+      console.log(done + '\t-')
+   }
+   console.log(done)
+}
+
+addTask('test',PRIORITY.HIGH);
+addTask('to do the task',PRIORITY.HIGH);
+addTask('to do the task2',PRIORITY.HIGH);
+
+changeStatus('test111', STATUS.IN_PROGRESS);
+changeStatus('test', STATUS.IN_PROGRESS);
+
+deleteTask('Sleep');
+deleteTask('test');
+
 console.log(list);
+showList();
+
+
