61a91c476e104fe312304724a4f2a77295a1e084
mirfidron <mirfidron@mail.ru>
2023-04-20 14:42:09 +0300

    calc ~ v.1

---
+++ mirfidron repo/train-38.js
@@ -0,0 +1,42 @@
+const firstNum = document.querySelector('.firstNum');
+const secondNum = document.querySelector('.secondNum');
+const select = document.querySelector('.select');
+const btnEqual = document.querySelector('.btn-equal');
+const calcResult = document.querySelector('.calc-result');
+
+function calc() {
+    let result = null;
+    const num_1 = +firstNum.value;
+    const num_2 = +secondNum.value;
+    const operation = select.value;
+    
+    switch(operation){
+        case '+':
+            result = num_1 + num_2;
+            break;
+        case '-':
+            result = num_1 - num_2;
+            break;
+        case '*':
+            result = num_1 * num_2;
+            break;
+        case '/':
+            result = num_1 / num_2;
+            break;
+        default:
+            alert('Что-то не так');
+    }
+    return result;
+}
+
+
+btnEqual.addEventListener('click', function (){
+    result = calc();
+    calcResult.innerHTML = result;
+});
+
+
+
+
+
+
