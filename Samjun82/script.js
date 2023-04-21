1187e78277a959765168d971514d354f151dca51
Samj <samjun82gmail.com>
2023-04-21 16:44:51 +0300

    att. and prop., style and class

--- Samjun82/script.js
+++ Samjun82/script.js
@@ -1,22 +1,8 @@
-const result = document.getElementById('res')
+const changeBg = document.getElementById('change-bg')
+const body = document.getElementById('body')
 
-function calculated(num1, num2, operation) {
-    switch (operation) {
-    case 'add':
-        return num1 + num2
-    case 'multi':
-        return num1 * num2
-    case 'subtract':
-        return num1 - num2
-    case 'division':
-        return num1 / num2
-    } 
-}
-function getResult () {
-    const num1 = document.getElementById('num1').value,
-    num2 = document.getElementById('num2').value,
-    select = document.getElementById('tog').value
-    document.getElementById('ans').textContent = calculated(+num1, +num2, select)
+function changeBackgoundColor() {
+    body.style.backgroundColor = changeBg.getAttribute('data-color')
 }
 
-result.addEventListener('click', getResult)
+changeBg.addEventListener('click', changeBackgoundColor)
