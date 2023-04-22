27cf9f89f28d77272a8c6f32baec0869f35cc48c
Anastasia <tgmonresu@gmail.com>
2023-04-22 07:56:32 +0300

    v1

--- monresu/main.js
+++ monresu/main.js
@@ -1,62 +1,13 @@
-const answerNode = document.querySelector('.answer');
-const historyNode = document.querySelector('.history');
+const changeBg = document.querySelector('.changeBg')
+const colors = ['red', 'blue', 'green', 'orange'];
 
-const num1 = document.querySelector('.num1')
-const num2 = document.querySelector('.num2')  
-
-const operations = document.querySelector('#operations')
-
-const equal = document.querySelector('.eq')
-
-equal.addEventListener('click', equalFunc);
-
-function sum() {
-  return +num1.value + +num2.value;
-}
-function sub() {
-  return +num1.value - +num2.value;
-}
-function multi() {
-  return +num1.value * +num2.value;
-}
-function div() {
-  return +num1.value / +num2.value;
-}
-function mod() {
-  return +num1.value % +num2.value;
+function getRandomColor() {
+  return colors[Math.floor(Math.random() * colors.length)];
 }
 
-function saveAnswer(oper, res) {
-  const ansHistory = document.createElement('div');
-  ansHistory.innerHTML = `${num1.value} ${oper} ${num2.value} = ${res}`;
-  historyNode.insertAdjacentElement('beforeend',ansHistory);
-  ansHistory.addEventListener('click', () => ansHistory.remove())
-}
-
-function equalFunc() {
-  const oper = operations.value;
-  let res = 0;
-  switch (oper) {
-    case '+':
-      res = sum();
-      answerNode.textContent = res;
-      break 
-    case '-':
-      res = sub();
-      answerNode.textContent = res;
-      break 
-    case '*':
-      res = multi();
-      answerNode.textContent = res;
-      break 
-    case '/':
-      res = div();
-      answerNode.textContent = res;
-      break 
-    case '%':
-      res = mod();
-      answerNode.textContent = res;
-      break 
-  }
-  saveAnswer(oper, res)
-}
+changeBg.addEventListener('click', () => {
+  document.body.style.backgroundColor = getRandomColor();
+  const interval = setInterval(() => {
+    document.body.style.backgroundColor = getRandomColor();
+  }, 2000)
+})
