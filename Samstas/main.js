c3df060383d8517ce653cf55011fac448414de3b
Stas <121170610+Samstas@users.noreply.github.com>
2023-04-22 03:21:14 +0200

    Update main.js

--- Samstas/main.js
+++ Samstas/main.js
@@ -1,3 +1,68 @@
+// // manipulations
+// const selectCalc = document.getElementById("select-calc");
+
+// const addition = document.getElementById("addition");
+// const subtraction = document.getElementById("subtraction");
+// const multiplication = document.getElementById("multiplication");
+// const division = document.getElementById("division");
+// // number fields
+// const firstNumber = document.querySelector(".first-number");
+// const secondNumber = document.querySelector(".second-number");
+// // button and output
+// const equalBtn = document.getElementById("equal");
+// const outputNumber = document.getElementById("output-number");
+
+// let first, second;
+// let result;
+
+// // 1. Если выбор знака равен + то добавляем значение firstNumber + secondNumber и выводим в outputNumber
+
+// // ADDITION
+// function add() {
+//   if (selectCalc.value === "add") {
+//     result = first + second;
+//     outputNumber.textContent = result;
+//   }
+// }
+// // SUBTRACTION
+// function sub() {
+//   if (selectCalc.value === "sub") {
+//     result = first - second;
+//     outputNumber.textContent = result;
+//   }
+// }
+
+// // MULTIPLICATION
+// function mult() {
+//   if (selectCalc.value === "mult") {
+//     result = first * second;
+//     outputNumber.textContent = result;
+//   }
+// }
+// // DIVISION
+// function div() {
+//   if (selectCalc.value === "div") {
+//     result = first / second;
+//     outputNumber.textContent = result;
+//   }
+// }
+// // button equal to
+// equalBtn.addEventListener("click", () => {
+//   first = Number(firstNumber.value);
+//   second = Number(secondNumber.value);
+
+//   if (selectCalc.value === "add") {
+//     add();
+//   } else if (selectCalc.value === "sub") {
+//     sub();
+//   } else if (selectCalc.value === "mult") {
+//     mult();
+//   } else if (selectCalc.value === "div") {
+//     div();
+//   }
+// });
+// ===================================
+
 // manipulations
 const selectCalc = document.getElementById("select-calc");
 
@@ -15,6 +80,17 @@ const equalBtn = document.getElementById("equal");
 const outputNumber = document.getElementById("output-number");
 const results = document.getElementById("results");
 
+function addResultToDOM(result) {
+  const newResult = document.createElement("div");
+  newResult.textContent = result;
+  results.appendChild(newResult);
+
+  newResult.addEventListener("click", () => {
+    results.removeChild(newResult);
+  });
+}
+
+
 let first, second;
 let result;
 
@@ -24,14 +100,7 @@ function add() {
     result = first + second;
     outputNumber.textContent = result;
 
-    const newResult = document.createElement("div");
-    newResult.textContent = result;
-    results.appendChild(newResult);
-
-    // удаление элемента из DOM при клике на него
-    newResult.addEventListener("click", () => {
-      results.removeChild(newResult);
-    });
+    addResultToDOM(result)
   }
 }
 
@@ -41,14 +110,7 @@ function sub() {
     result = first - second;
     outputNumber.textContent = result;
 
-    const newResult = document.createElement("div");
-    newResult.textContent = result;
-    results.appendChild(newResult);
-
-    // удаление элемента из DOM при клике на него
-    newResult.addEventListener("click", () => {
-      results.removeChild(newResult);
-    });
+    addResultToDOM(result)
   }
 }
 
@@ -58,14 +120,7 @@ function mult() {
     result = first * second;
     outputNumber.textContent = result;
 
-    const newResult = document.createElement("div");
-    newResult.textContent = result;
-    results.appendChild(newResult);
-
-    // удаление элемента из DOM при клике на него
-    newResult.addEventListener("click", () => {
-      results.removeChild(newResult);
-    });
+    addResultToDOM(result)
   }
 }
 
@@ -75,14 +130,7 @@ function div() {
     result = first / second;
     outputNumber.textContent = result;
 
-    const newResult = document.createElement("div");
-    newResult.textContent = result;
-    results.appendChild(newResult);
-
-    // удаление элемента из DOM при клике на него
-    newResult.addEventListener("click", () => {
-      results.removeChild(newResult);
-    });
+    addResultToDOM(result)
   }
 }
 
