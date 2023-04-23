d504850041a311ccd183b627b2118e7516dc87b8
drunque <lufi.2023@gmail.com>
2023-04-23 10:23:26 +0000

    add calc.js module

---
+++ calc.js
@@ -0,0 +1,28 @@
+export function calc(a, b, operation) {
+  const operations = {
+    add: { action: a + b, sign: "+" },
+    sub: { action: a - b, sign: "-" },
+    mul: { action: a * b, sign: "*" },
+    div: { action: a / b, sign: "/" },
+  };
+
+  const validation = {
+    isValidOperation: Object.keys(operations).includes(operation),
+
+    isValidNumbers(...numbers) {
+      return numbers.filter(Number.isFinite).length === numbers.length;
+    },
+  };
+
+  if (validation.isValidNumbers(a, b) && validation.isValidOperation) {
+    let result = operations[operation].action;
+
+    if (!Number.isFinite(result)) return "Error";
+    if (!Number.isInteger(result)) {
+      result = result.toFixed(2);
+    }
+
+    return { sign: operations[operation].sign, value: result.toString().slice(0, 30) };
+  }
+  return [operations[operation], "Error"];
+}
