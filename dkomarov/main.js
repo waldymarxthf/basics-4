063295474bdecd4c08f4df4fa4645d7038dfa0e8
Dmitry Komarov <dmitrykomarov@MacBook-Pro-Dmitry.local>
2023-04-23 20:54:10 +0300

    modules

--- dkomarov/main.js
+++ dkomarov/main.js
@@ -1,9 +1,4 @@
-const numberOne = document.querySelector('.number-one');
-const numberTwo = document.querySelector('.number-two');
-const operationSelect = document.querySelector('.select');
-const resultBtn = document.querySelector('.button-res');
-const resultNumber = document.querySelector('.result');
-const parent = document.querySelector("#parent");
+import {numberOne, numberTwo, operationSelect, resultBtn, resultNumber, parent} from "/basics-4/dkomarov/modules/constants.js"
 
 function calc() {
     const number_1 = Number(numberOne.value);
---
+++ dkomarov/modules/constants.js
@@ -0,0 +1,8 @@
+const numberOne = document.querySelector('.number-one');
+const numberTwo = document.querySelector('.number-two');
+const operationSelect = document.querySelector('.select');
+const resultBtn = document.querySelector('.button-res');
+const resultNumber = document.querySelector('.result');
+const parent = document.querySelector("#parent");
+
+export {numberOne, numberTwo, operationSelect, resultBtn, resultNumber, parent};
