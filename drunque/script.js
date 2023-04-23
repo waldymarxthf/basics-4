80f69718efab8dab12388605a0725ee875cd0e31
drunque <lufi.2023@gmail.com>
2023-04-23 09:16:52 +0000

    fix scroll and some styles

--- drunque/script.js
+++ drunque/script.js
@@ -1,9 +1,16 @@
+const operations = {
+  add: "+",
+  sub: "-",
+  mul: "*",
+  div: "/",
+};
+
 function calc(a, b, operation) {
   const operations = {
-    add: a + b,
-    sub: a - b,
-    mul: a * b,
-    div: a / b,
+    add: {action: a + b, sign: "+"},
+    sub: {action: a - b, sign: "-"},
+    mul:{action:  a * b, sign: "*"},
+    div: {action: a / b, sign: "/"},
   };
 
   const validation = {
@@ -15,7 +22,7 @@ function calc(a, b, operation) {
   };
 
   if (validation.isValidNumbers(a, b) && validation.isValidOperation) {
-    let result = operations[operation];
+    let result = operations[operation].action;
 
     if (!Number.isFinite(result)) return "Error";
     
@@ -29,13 +36,19 @@ function calc(a, b, operation) {
 }
 
 const resultButton = document.querySelector("#result-button");
+const results = document.querySelector(".results")
 resultButton.addEventListener("click", () => {
   const numberNodes = Array.from(document.querySelectorAll("input"));
   const numbers = numberNodes.map((node) => Number.parseFloat(node.value) );
-
   const operation = document.querySelector(".current div").dataset.value;
   const result = calc(...numbers, operation);
   const resultNode = document.querySelector("#result");
 
   if (result) resultNode.textContent = result;
+
+  const historyResult = document.createElement("div")
+  historyResult.classList.add("rectangle")
+  historyResult.textContent = `${numbers[0]} ${operations[operation]} ${numbers[1]} = ${result}`;
+  results.appendChild(historyResult)
+  results.scrollTop = results.scrollHeight
 });
