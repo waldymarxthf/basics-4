38bcb1a3e2b78819fa3012965c2ff2637494da8c
johnbeelow <beelow-dj@mail.ru>
2023-04-23 14:52:37 +0300

    add 40

--- johnbeelow/js/main.js
+++ johnbeelow/js/main.js
@@ -4,25 +4,50 @@ const UI_ELEMENTS = {
   OPERATOR: document.querySelector('.operators'),
   BUTTON_RESULT: document.querySelector('.button_result'),
   RESULT: document.querySelector('.result'),
+  HISTORY_RESULT: document.querySelector('.history_result_container'),
 }
 
-function sumCalculator(operation, numberOne, numberTwo) {
+const OPERATIONS = {
+  ADD: 'add',
+  SUBTRACT: 'subtract',
+  MULTI: 'multi',
+  DIV: 'div',
+}
+
+function calcSum(operation, numberOne, numberTwo) {
   switch (operation) {
-    case 'add':
+    case OPERATIONS.ADD:
       return +numberOne + +numberTwo
-    case 'subtract':
+    case OPERATIONS.SUBTRACT:
       return numberOne - numberTwo
-    case 'multi':
+    case OPERATIONS.MULTI:
       return numberOne * numberTwo
-    case 'div':
+    case OPERATIONS.DIV:
       return numberOne / numberTwo
   }
 }
 
-UI_ELEMENTS.BUTTON_RESULT.addEventListener('click', () => {
-  UI_ELEMENTS.RESULT.textContent = sumCalculator(
+function updateHistoryConstructor(valueResult) {
+  const newDiv = document.createElement('div')
+  newDiv.setAttribute('class', 'text_history')
+  newDiv.textContent = valueResult
+
+  UI_ELEMENTS.HISTORY_RESULT.appendChild(newDiv)
+
+  newDiv.addEventListener('click', () => {
+    newDiv.remove()
+  })
+}
+
+function getResultConstructor() {
+  UI_ELEMENTS.RESULT.textContent = calcSum(
     UI_ELEMENTS.OPERATOR.value,
     UI_ELEMENTS.NUMBER_ONE.value,
     UI_ELEMENTS.NUMBER_TWO.value
   )
-})
+
+  updateHistoryConstructor(UI_ELEMENTS.RESULT.textContent)
+}
+
+UI_ELEMENTS.BUTTON_RESULT.addEventListener('click', getResultConstructor)
+
