ce680aebe59c474241d19176e309784ab3f72e8b
drunque <lufi.2023@gmail.com>
2023-04-23 10:42:49 +0000

    practice-41

--- drunque/script.js
+++ drunque/script.js
@@ -1,56 +1,22 @@
-const operations = {
-  add: "+",
-  sub: "-",
-  mul: "*",
-  div: "/",
-};
-
-function calc(a, b, operation) {
-  const operations = {
-    add: {action: a + b, sign: "+"},
-    sub: {action: a - b, sign: "-"},
-    mul:{action:  a * b, sign: "*"},
-    div: {action: a / b, sign: "/"},
-  };
-
-  const validation = {
-    isValidOperation: Object.keys(operations).includes(operation),
-
-    isValidNumbers(...numbers) {
-      return numbers.filter(Number.isFinite).length === numbers.length;
-    },
-  };
-
-  if (validation.isValidNumbers(a, b) && validation.isValidOperation) {
-    let result = operations[operation].action;
-
-    if (!Number.isFinite(result)) return "Error";
-    
-    if (!Number.isInteger(result)) {
-      result = result.toFixed(2);
-    }
-
-    return result.toString().slice(0, 30);
-  }
-  return "Error";
-}
+import { calc } from "./calc.js";
 
 const resultButton = document.querySelector("#result-button");
-const results = document.querySelector(".results")
+const results = document.querySelector(".results");
 resultButton.addEventListener("click", () => {
-  const numberNodes = Array.from(document.querySelectorAll("input"));
-  const numbers = numberNodes.map((node) => Number.parseFloat(node.value) );
+  const numbers = Array.from(document.querySelectorAll("input")).map(
+    (node) => Number.parseFloat(node.value)
+  );
   const operation = document.querySelector(".current div").dataset.value;
   const result = calc(...numbers, operation);
   const resultNode = document.querySelector("#result");
 
-  if (result) resultNode.textContent = result;
+  if (result) resultNode.textContent = result.value;
 
-  const historyResult = document.createElement("div")
-  historyResult.classList.add("rectangle")
-  historyResult.textContent = `${numbers[0]} ${operations[operation]} ${numbers[1]} = ${result}`;
-  results.appendChild(historyResult)
-  results.scrollTop = results.scrollHeight
+  const historyResult = document.createElement("div");
+  historyResult.classList.add("rectangle");
+  historyResult.textContent = `${numbers[0]} ${result.sign} ${numbers[1]} = ${result.value}`;
+  results.appendChild(historyResult);
+  results.scrollTop = results.scrollHeight;
 
-  historyResult.addEventListener("click", historyResult.remove)
+  historyResult.addEventListener("click", historyResult.remove);
 });
