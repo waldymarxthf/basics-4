c177c6ae434dcf1aea7735fdf3d8f8aec7699c40
2Metallum <k.akshentsev04@mail.ru>
2023-04-23 11:37:43 +0500

    add 1st module(var)

--- kakshentsev/main.js
+++ kakshentsev/main.js
@@ -1,9 +1,11 @@
-const firstInputNumber = document.querySelector(".calculator-num1");
-const secondInputNumber = document.querySelector(".calculator-num2");
-const mathOperation = document.querySelector("#math-operation");
-const buttonForCounting = document.querySelector(".calc_result_btn");
-const result = document.querySelector(".calc_result_block");
-const resultsTable = document.querySelector(".calc_result_table");
+import {
+  firstInputNumber,
+  secondInputNumber,
+  mathOperation,
+  buttonForCounting,
+  result,
+  resultsTable,
+} from "./modules/variables.js";
 
 function clickOnButton() {
   const result = calculation();
---
+++ kakshentsev/modules/variables.js
@@ -0,0 +1,15 @@
+const firstInputNumber = document.querySelector(".calculator-num1");
+const secondInputNumber = document.querySelector(".calculator-num2");
+const mathOperation = document.querySelector("#math-operation");
+const buttonForCounting = document.querySelector(".calc_result_btn");
+const result = document.querySelector(".calc_result_block");
+const resultsTable = document.querySelector(".calc_result_table");
+
+export {
+  firstInputNumber,
+  secondInputNumber,
+  mathOperation,
+  buttonForCounting,
+  result,
+  resultsTable,
+};
