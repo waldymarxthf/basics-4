5a04b77254eaf420b30334378d09eef6a6a36408
anatolii-samurai <goddevil22@yandex.ru>
2023-04-24 05:10:03 +0400

    Запись результатов

---
+++ base.js
@@ -0,0 +1,7 @@
+export const OPERATION = {
+    sum:'+',
+    minus:'-',
+    mult:'*',
+    divider:'/'
+};
+
--- main.js
+++ main.js
@@ -1,48 +1,52 @@
-const OPERATION = {
-    sum:'+',
-    minus:'-',
-    mult:'*',
-    divider:'/'
-};
-
+'use strict';
+import { OPERATION } from "./base.js";
 function calculate ({a,b,operation}){
-  switch(operation){
-    case OPERATION.sum:
-       return  a + b;
+    switch(operation){
+      case OPERATION.sum:
+         return  a + b;
+         
+      case OPERATION.minus:
+         return a -b; 
        
-    case OPERATION.minus:
-       return a -b; 
-     
-    case OPERATION.mult:
-        return a * b;
-    
-    case OPERATION.divider:
-       return a / b;
-       
-        default:
-            break;
+      case OPERATION.mult:
+          return a * b;
+      
+      case OPERATION.divider:
+         return a / b;
+         
+          default:
+              break;
+    }
   }
-}
 const numberA = document.querySelector('.js-a-number');
 const numberB = document.querySelector('.js-b-number');
 const selectOperation = document.querySelector('.js-select-operation');
 const resultOperation = document.querySelector('.result');
 const btnResultNode = document.querySelector('.js-button');
+const history = document.querySelector(".history");
+const added = document.querySelector('.added');
+
 
 btnResultNode.addEventListener('click', function(){
     const a = +numberA.value;
     const b = +numberB.value;
     const operation = selectOperation.value;
-    
-    if (b === 0){
-        resultOperation.textContent = "На ноль делить нельзя";
-    }else{
-        const result = calculate({
-            a,
-            b,
-            operation
-        });
-        resultOperation.textContent = result;
-    }
-    
+     const result = calculate({ a, b, operation });
+      
+     
+     resultOperation.textContent = result;
+     const saveResult = document.createElement("div");
+        saveResult.classList.add('added');
+        saveResult.textContent = result;
+        history.appendChild(saveResult);
 });
+history.addEventListener('click',(event)=>{
+    if (event.target.classList.contains("added")){
+     event.stopPropagation();
+     event.target.remove();
+    }
+ }); 
+
+
+  
+
