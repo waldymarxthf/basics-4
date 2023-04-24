987d5f9787b71ee5154354efb9d0eaa101ad5a6e
Gar1kSh <skydust63@gmail.com>
2023-04-24 04:22:20 +0300

    modified main.js for modules

--- Gar1kSh/main.js
+++ Gar1kSh/main.js
@@ -1,3 +1,6 @@
+import { getInputValue } from "./modules/getInputValue.js";
+import { OPERATION } from "./modules/operation.js";
+
 const UI = {
   FIRST_NUMBER: document.querySelector("#firstNumber"),
   SECOND_NUMBER: document.querySelector("#secondNumber"),
@@ -9,12 +12,6 @@ const UI = {
 };
 
 const error = "ERROR:Number is Infinity!";
-const OPERATION = {
-  ADD: "+",
-  SUBT: "-",
-  MULTI: "*",
-  DIV: "/",
-};
 
 const resultAdd = (num1, num2) => {
   if (num1 === 0.1 && num2 === 0.2) {
@@ -41,8 +38,6 @@ const resultDiv = (num1, num2) => {
     return (UI.OUTPUT.textContent = num1 / num2);
   }
 };
-
-const getInputValue = (inp) => inp.value;
 
 const resetInput = () => {
   UI.OUTPUT.textContent = "";
