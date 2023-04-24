855e0c3897c062405204be69c968941caee447d4
Vitalii Miagkov <maposia@gmail.com>
2023-04-24 07:41:51 +0300

    refactoring code and added result table

--- maposia/main.js
+++ maposia/main.js
@@ -2,6 +2,8 @@ const buttonsNum = document.querySelectorAll('.numBtn')
 const display = document.querySelector('.result-block')
 const operators = document.querySelectorAll('.operator')
 const clearBtn = document.querySelector('.clear')
+const results = document.querySelector('.results')
+const delResults = document.querySelectorAll('.del_btn')
 
 const OPERATORS = {
   PLUS: '+',
@@ -12,35 +14,33 @@ const OPERATORS = {
 
 let isClicked = false
 
-buttonsNum.forEach((button) => {
-  button.addEventListener('click', () => {
-    console.log(button.textContent)
-    const outputNum = display.textContent
-    if (outputNum.length < 14) {
-      if (button.textContent === ',') {
-        display.textContent = `${display.textContent}.`
-        return
-      }
-      if (button.textContent === '0' && isClicked) {
-        display.textContent = ''
-        display.textContent += button.textContent
-        return
-      }
-      if (display.textContent === '0' || isClicked) {
-        display.textContent = ''
-        display.textContent += button.textContent
-        isClicked = false
-        return
-      } else {
-        display.textContent = display.textContent += button.textContent
-      }
-    }
-  })
-})
-
 let operandA
 let operandB
 let currentOperator
+const resultList = []
+
+function numbers() {
+  const outputNum = display.textContent
+  if (outputNum.length < 14) {
+    if (this.textContent === ',') {
+      display.textContent = `${display.textContent}.`
+      return
+    }
+    if (this.textContent === '0' && isClicked) {
+      display.textContent = ''
+      display.textContent += this.textContent
+      return
+    }
+    if (display.textContent === '0' || isClicked) {
+      display.textContent = ''
+      display.textContent += this.textContent
+      isClicked = false
+      return
+    } else {
+      display.textContent = display.textContent += this.textContent
+    }
+  }
+}
 
 function сalculation(symbol) {
   operandA = Number(display.textContent)
@@ -48,44 +48,79 @@ function сalculation(symbol) {
   isClicked = true
 }
 
+function resultItem() {
+  const resultBlock = document.createElement('div')
+  const result = document.createElement('p')
+  const delBtn = document.createElement('button')
+
+  resultBlock.classList.add('result')
+  result.textContent = display.textContent
+  delBtn.classList.add('del_btn')
+  delBtn.textContent = 'Удалить'
+  results.appendChild(resultBlock)
+  resultBlock.appendChild(result)
+  resultBlock.appendChild(delBtn)
+}
+
+function calcutale(operator) {
+  switch (operator) {
+    case OPERATORS.PLUS:
+      сalculation(operator)
+      return
+    case OPERATORS.MUNIS:
+      сalculation(operator)
+      return
+    case OPERATORS.MULTIPLY:
+      сalculation(operator)
+      return
+    case OPERATORS.DIVISION:
+      сalculation(operator)
+      return
+    case '=':
+      operandB = Number(display.textContent)
+      if (currentOperator === OPERATORS.PLUS) {
+        display.textContent = operandA + operandB
+      }
+      if (currentOperator === OPERATORS.MUNIS) {
+        display.textContent = operandA - operandB
+      }
+      if (currentOperator === OPERATORS.MULTIPLY) {
+        display.textContent = operandA * operandB
+      }
+      if (currentOperator == OPERATORS.DIVISION) {
+        display.textContent = operandA / operandB
+      }
+      resultItem()
+      isClicked = true
+      console.log(delResults)
+      return
+  }
+}
+
+function delBlockResult() {
+  console.log('удалить')
+}
+
+console.log(delResults)
+delResults.forEach((delResult) => {
+  delResult.addEventListener('click', delBlockResult)
+})
+
+buttonsNum.forEach((button) => {
+  button.addEventListener('click', numbers)
+})
+
 operators.forEach((operator) => {
   const operatorValue = operator.textContent
   operator.addEventListener('click', () => {
-    switch (operatorValue) {
-      case OPERATORS.PLUS:
-        сalculation(operatorValue)
-        return
-      case OPERATORS.MUNIS:
-        сalculation(operatorValue)
-        return
-      case OPERATORS.MULTIPLY:
-        сalculation(operatorValue)
-        return
-      case OPERATORS.DIVISION:
-        сalculation(operatorValue)
-        return
-      case '=':
-        operandB = Number(display.textContent)
-        if (currentOperator === OPERATORS.PLUS) {
-          display.textContent = operandA + operandB
-        }
-        if (currentOperator === OPERATORS.MUNIS) {
-          display.textContent = operandA - operandB
-        }
-        if (currentOperator === OPERATORS.MULTIPLY) {
-          display.textContent = operandA * operandB
-        }
-        if (currentOperator == OPERATORS.DIVISION) {
-          display.textContent = operandA / operandB
-          console.log(operandA)
-          console.log(operandB)
-        }
-
-        return
-    }
+    calcutale(operatorValue)
   })
 })
 
 clearBtn.addEventListener('click', () => {
   display.textContent = '0'
 })
+
+// delResult.addEventListener('click', () => {
+//   console.log('click')
+// })
