daa116f9c731ddf27d2fc90d30211ec8bb59cdb2
Dmitry Komarov <dmitrykomarov@MacBook-Pro-Dmitry.local>
2023-04-24 13:57:23 +0300

    add progress bar

--- dkomarov/main.js
+++ dkomarov/main.js
@@ -1,13 +1,7 @@
-const buttonStart = document.querySelector("#btn-start");
-const buttonStop = document.querySelector("#btn-stop");
-const btnWrap = document.querySelector("#btnWrap");
-const btnWrapStart = document.querySelector("#wrap-btn_start");
-const btnWrapReset = document.querySelector("#wrap-btn_reset");
-const hoursValue = document.querySelector("#value-hours");
-const minutesValue = document.querySelector("#value-minutes");
-const secondsValue = document.querySelector("#value-seconds");
-const changeColorBtn = document.querySelector("#change-color");
-const bgBody = document.querySelector("body");
+import { bgBody, btnWrap, btnWrapReset, btnWrapStart, buttonStart, buttonStop, hoursValue, minutesValue, secondsValue, changeColorBtn } from './modules/constants.js';
+
+const progress = document.querySelector('#progress-bar_left');
+
 
 let timer;
 let hrs = 0;
@@ -34,9 +28,53 @@ function runTimer() {
             min = 0;
             sec = 0;
         }
+
+        let progressValue = secondsValue.textContent 
+        switch (progressValue) {
+            case "01":
+                progress.style.width = "100px";
+                break;
+            case "02":
+                progress.style.width = "200px";
+                break;
+            case "03":
+                progress.style.width = "300px";
+                break;
+            case "04":
+                progress.style.width = "400px";
+                break;
+            case "05":
+                progress.style.width = "500px";
+                break;
+            case "06":
+                progress.style.width = "600px";
+                break;
+            case "07":
+                progress.style.width = "700px";
+                break;
+            case "08":
+                progress.style.width = "800px";
+                break;
+            case "09":
+                progress.style.width = "900px";
+                break;
+            case "10":
+                progress.style.width = "1000px";
+                break;
+            default:
+                progress.style.width = "0px";
+
+        }
+       
+
+
+
         timer = setTimeout(run, 1000);
     }, 1000)
+
+
 };
+
 
 buttonStart.addEventListener('click', () => {
     runTimer();
@@ -64,7 +102,7 @@ function randomBgColor() {
     const randomColor = colors[Math.floor(Math.random() * colors.length)];
     bgBody.style.backgroundColor = randomColor;
     console.log("color");
-}
+};
 
 function bgColorSetInt() {
     let timer = setInterval(() => {
@@ -73,7 +111,8 @@ function bgColorSetInt() {
     setTimeout(() => {
         clearInterval(timer);
     }, 10000);
-}
+};
+
 
 
 btnWrapStart.addEventListener('click', () => {
@@ -81,6 +120,8 @@ btnWrapStart.addEventListener('click', () => {
     btnWrap.classList.add('hidden');
     buttonStop.classList.remove('hidden');
 });
+
+
 
 btnWrapReset.addEventListener('click', () => {
     buttonClickReset();
---
+++ dkomarov/modules/constants.js
@@ -0,0 +1,12 @@
+const buttonStart = document.querySelector("#btn-start");
+const buttonStop = document.querySelector("#btn-stop");
+const btnWrap = document.querySelector("#btnWrap");
+const btnWrapStart = document.querySelector("#wrap-btn_start");
+const btnWrapReset = document.querySelector("#wrap-btn_reset");
+const hoursValue = document.querySelector("#value-hours");
+const minutesValue = document.querySelector("#value-minutes");
+const secondsValue = document.querySelector("#value-seconds");
+const changeColorBtn = document.querySelector("#change-color");
+const bgBody = document.querySelector("body");
+
+export {bgBody, btnWrap, btnWrapReset, btnWrapStart, buttonStart, buttonStop, hoursValue, minutesValue, secondsValue, changeColorBtn};
