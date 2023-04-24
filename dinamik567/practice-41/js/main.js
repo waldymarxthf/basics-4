7536a9e7201a3d34663796a1aaf931d1f3eeca31
Vladislav Irhin <irhin2011@yandex.ru>
2023-04-25 01:29:22 +0300

    My first module

---
+++ dinamik567/practice-41/js/main.js
@@ -0,0 +1,55 @@
+'use strict';
+
+import {numberOne, numberTwo, listMathActions, buttonResult, calculatorResult} from './modules/UI.js'
+
+
+function calculator(operation, a, b) {
+    switch (operation) {
+        case '+':
+            return (a * 10 + b * 10) / 10;
+        case '-':
+            return a - b;
+        case '*':
+            return a * b;
+        case '/':
+            return (a  / b);
+        default:
+            console.log('Вы вели некоректное значение');
+            return false;
+    }
+}
+
+buttonResult.addEventListener('click', () => {
+   const num1 = +numberOne.value;
+   const num2 = +numberTwo.value;
+   const index = listMathActions.options.selectedIndex;
+   const action = listMathActions.options[index].value;
+
+   console.log(action)
+   if (!typeof num1 === 'number') {
+       console.log(`Некоректное значение ${num1}`)
+       return false
+   }
+
+    if (!typeof num2 === 'number') {
+        console.log(`Некоректное значение ${num2}`)
+        return false
+    }
+
+
+   const result = calculator(action, num1, num2);
+
+    calculatorResult.textContent = result;
+
+    const div = document.createElement('div');
+
+    div.textContent = result;
+    div.style.padding = '5px';
+    div.style.border = '1px solid black';
+
+    document.body.appendChild(div);
+
+    div.addEventListener('click', () => div.remove());
+});
+
+
---
+++ dinamik567/practice-41/js/modules/UI.js
@@ -0,0 +1,5 @@
+export const numberOne = document.querySelector('.numberOne');
+export const numberTwo = document.querySelector('.numberTwo');
+export const listMathActions = document.querySelector('.calculator-actions');
+export const buttonResult = document.querySelector('.result-btn');
+export const calculatorResult = document.querySelector('.calculator__result');
