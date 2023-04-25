6a2a10c0afb285c517c1a05a82b7c96782ba5fa5
johnbeelow <beelow-dj@mail.ru>
2023-04-25 23:11:38 +0300

    add 44

--- johnbeelow/js/main.js
+++ johnbeelow/js/main.js
@@ -1,11 +1,33 @@
-// // .eslintrc
-// {
-//     "rules": {
-//       "semi": ["error", "always"],
-//       "quotes": ["error", "double"]
-//     }
-//   }
+import { UI_ELEMENTS } from "./module/ui_elements.js"
 
-//   // код для проверки
-//   const myString = 'Hello world';
-//   console.log(myString)
+function constructorTaskUi(textInput, priorityTask) {
+  const containerTask = document.createElement('div')
+  const labelTask = document.createElement('label')
+  const checkboxTask = document.createElement('input')
+  const textTask = document.createElement('span')
+  const closeTask = document.createElement('div')
+
+  containerTask.setAttribute('class', 'task')
+  checkboxTask.setAttribute('class', 'checkbox')
+  checkboxTask.setAttribute('type', 'checkbox')
+  textTask.setAttribute('class', 'task_text')
+  textTask.textContent = textInput
+  closeTask.setAttribute('class', 'button_close_task')
+  closeTask.textContent = '×'
+
+  priorityTask.appendChild(containerTask)
+  containerTask.appendChild(labelTask)
+  labelTask.appendChild(checkboxTask)
+  labelTask.appendChild(textTask)
+  containerTask.appendChild(closeTask)
+}
+
+UI_ELEMENTS.INPUT_FORM_HIGHT.addEventListener('submit', function (event) {
+  event.preventDefault()
+  constructorTaskUi(UI_ELEMENTS.INPUT_TEXT_HIGH.value, UI_ELEMENTS.PRIORITY_HIGH)
+})
+
+UI_ELEMENTS.INPUT_FORM_LOW.addEventListener('submit', function (event) {
+  event.preventDefault()
+  constructorTaskUi(UI_ELEMENTS.INPUT_TEXT_LOW.value, UI_ELEMENTS.PRIORITY_LOW)
+})
