9e5c68f15fe075f2be9530eaad1771f52fd6fc91
drunque <lufi.2023@gmail.com>
2023-04-25 07:30:23 +0300

    practice-44

--- drunque/script.js
+++ drunque/script.js
@@ -1,26 +1,24 @@
+const form = document.querySelector(".task-creator");
+
 function addTask() {
-  const form = document.querySelector(".task-creator");
   const tasks = document.querySelector(".task-list .tasks")
   const taskTemplate = document.querySelector(".task-template")
   const newTask = taskTemplate.cloneNode(true);
   newTask.classList.remove("disabled")
 
   const taskInfo = newTask.querySelector(".task-info");
-  const label = taskInfo.querySelector(".label");
+  const label = taskInfo.querySelector(".label span");
   const description = taskInfo.querySelector(".description");
   label.textContent = form.querySelector(
     ".select .current .option"
   ).textContent;
-  description.textContent = form.querySelector(".description").textContent;
+  description.textContent = form.querySelector(".description").value;
 
   tasks.appendChild(newTask)
+  form.querySelector(".description").value = ""
 }
 
-document.addEventListener("keydown", (event) => {
-  if (event.key === "Enter") {
-    addTask();
-  }
-});
-
-const submitButton = form.querySelector(".button")
-submitButton.addEventListener("click", addTask)
+form.addEventListener("submit", event => {
+  event.preventDefault()
+  addTask()
+})
