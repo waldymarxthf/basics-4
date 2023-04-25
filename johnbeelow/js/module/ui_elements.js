55b58c7075bc3a3c632a7fa9b861ed4e3c642e7f
John <98343581+johnbeelow@users.noreply.github.com>
2023-04-25 23:42:59 +0300

    UPD DOM

--- johnbeelow/js/module/ui_elements.js
+++ johnbeelow/js/module/ui_elements.js
@@ -1,8 +1,13 @@
+export const DOM = {
+  GET_ELEMENT: (selector) => document.querySelector(selector),
+  CREATE_TAG: (tag) => document.createElement(tag),
+}
+
 export const UI_ELEMENTS = {
-    PRIORITY_HIGH: document.querySelector('.task_priority_high'),
-    PRIORITY_LOW: document.querySelector('.task_priority_low'),
-    INPUT_FORM_HIGHT: document.querySelector('.task_form_high'),
-    INPUT_FORM_LOW: document.querySelector('.task_form_low'),
-    INPUT_TEXT_HIGH: document.querySelector('.input_text_high'),
-    INPUT_TEXT_LOW: document.querySelector('.input_text_low'),
-  }
+  PRIORITY_HIGH: DOM.GET_ELEMENT('.task_priority_high'),
+  PRIORITY_LOW: DOM.GET_ELEMENT('.task_priority_low'),
+  INPUT_FORM_HIGHT: DOM.GET_ELEMENT('.task_form_high'),
+  INPUT_FORM_LOW: DOM.GET_ELEMENT('.task_form_low'),
+  INPUT_TEXT_HIGH: DOM.GET_ELEMENT('.input_text_high'),
+  INPUT_TEXT_LOW: DOM.GET_ELEMENT('.input_text_low'),
+}
