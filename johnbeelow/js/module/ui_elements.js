0141c643358fcd2c6ab674a296ae92f547c24cbc
johnbeelow <beelow-dj@mail.ru>
2023-04-26 21:32:45 +0300

    add 45

--- johnbeelow/js/module/ui_elements.js
+++
@@ -1,8 +0,0 @@
-export const UI_ELEMENTS = {
-    PRIORITY_HIGH: document.querySelector('.task_priority_high'),
-    PRIORITY_LOW: document.querySelector('.task_priority_low'),
-    INPUT_FORM_HIGHT: document.querySelector('.task_form_high'),
-    INPUT_FORM_LOW: document.querySelector('.task_form_low'),
-    INPUT_TEXT_HIGH: document.querySelector('.input_text_high'),
-    INPUT_TEXT_LOW: document.querySelector('.input_text_low'),
-  }
