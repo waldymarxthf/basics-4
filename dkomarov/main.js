e5254842b9781fa52c3b5c95901ce44c98b6b068
Dmitry Komarov <dmitrykomarov@MacBook-Pro-Dmitry.local>
2023-04-26 16:06:36 +0300

    delete element

--- dkomarov/main.js
+++ dkomarov/main.js
@@ -1,10 +1,13 @@
 const inputValueHigh = document.querySelector('.inputHigh');
 const addTaskButtonHigh = document.querySelector('.button_addHigh');
 const parentHigh = document.querySelector('.high_priority');
+const delButton = document.querySelector('.button_del');
 
 const inputValueLow = document.querySelector('.inputLow');
 const addTaskButtonLow = document.querySelector('.button_addLow');
 const parentLow = document.querySelector('.low_priority');
+
+
 
 
 const STATUS = {
@@ -57,18 +60,32 @@ function clearInput() {
 
 
 
+
+
 function addTaskHigh(event) {
     event.preventDefault()
     console.log(inputValueHigh.value);
     const newElement = document.createElement('form');
     parentHigh.appendChild(newElement);
-    newElement.classList.add('form'); 
+    newElement.classList.add('form');
     newElement.insertAdjacentHTML('afterbegin', `<p>${inputValueHigh.value}</p>`)
     newElement.insertAdjacentHTML('afterbegin', '<input class="radio" type="radio">');
     newElement.insertAdjacentHTML('beforeend', '<button id="del" type="submit" class="button_del"><img src="./image/free-icon-close-151882-444.svg" alt=""></button>');
     addTask();
     clearInput();
-}; 
+
+    const delButton = newElement.querySelector('.button_del');
+
+    delButton.addEventListener('click', function deleteTask(event) {
+        event.preventDefault();
+        taskName = toDoList.findIndex(tasks => tasks.task === inputValueHigh.value);
+        toDoList.splice(taskName, 1);
+        newElement.remove();
+
+        console.log(toDoList);
+    });
+
+};
 
 
 function addTaskLow(event) {
@@ -76,17 +93,27 @@ function addTaskLow(event) {
     console.log(inputValueLow.value);
     const newElement = document.createElement('form');
     parentLow.appendChild(newElement);
-    newElement.classList.add('form'); 
+    newElement.classList.add('form');
     newElement.insertAdjacentHTML('afterbegin', `<p>${inputValueLow.value}</p>`)
     newElement.insertAdjacentHTML('afterbegin', '<input class="radio" type="radio">');
     newElement.insertAdjacentHTML('beforeend', '<button id="del" type="submit" class="button_del"><img src="./image/free-icon-close-151882-444.svg" alt=""></button>');
     addTask();
     clearInput();
+
+    const delButton = newElement.querySelector('.button_del');
+
+    delButton.addEventListener('click', function deleteTask(event) {
+        event.preventDefault();
+        taskName = toDoList.findIndex(tasks => tasks.task === inputValueLow.value);
+        toDoList.splice(taskName, 1);
+        newElement.remove();
+
+        console.log(toDoList);
+    });
 };
 
 
 
 
-
-addTaskButtonHigh.addEventListener('click', addTaskHigh)
-addTaskButtonLow.addEventListener('click', addTaskLow)
+addTaskButtonHigh.addEventListener('click', addTaskHigh);
+addTaskButtonLow.addEventListener('click', addTaskLow);
