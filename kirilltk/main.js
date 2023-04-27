8974149390aff69f8245b58f47d1f441be665d3e
Kirill-Tkachenko242 <110831941+Kirill-Tkachenko242@users.noreply.github.com>
2023-04-27 09:58:29 +0300

    czh

--- kirilltk/main.js
+++ kirilltk/main.js
@@ -1,10 +1,11 @@
 const form = document.querySelector('#addTask');
 const input = document.querySelector('#taskName');
+const input2 = document.querySelector('#taskName2');
 
 input.addEventListener('keydown', (event) => {
     if (event.key === 'Enter') {
         event.preventDefault();
-        const highPriorityTask = document.querySelector('#high-priority #tasks')
+        const highPriorityTask = document.querySelector('#high-priority #tasks');
         const taskName = input.value;
         input.value = '';
 
@@ -24,5 +25,31 @@ input.addEventListener('keydown', (event) => {
         `;
 
         highPriorityTask.appendChild(newTask);
+    }
+});
+
+input2.addEventListener('keydown', (event) => {
+    if (event.key === 'Enter') {
+        event.preventDefault();
+        const lowPriorityTask = document.querySelector('#low-priority #tasks');
+        const taskName = input2.value;
+        input2.value = '';
+
+        const newTask = document.createElement('div');
+        newTask.setAttribute('id', 'task');
+        newTask.innerHTML = `
+        <form action="">
+			<label>
+				<input type="checkbox">
+                <span class="checkmark"></span>
+				<p>
+					${taskName}
+				</p>
+			</label>
+			<button id="deleteButton">x</button>
+		</form>
+        `;
+
+        lowPriorityTask.appendChild(newTask);
     }
 });
