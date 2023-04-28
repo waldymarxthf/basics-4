f33fd04b62667d5177994c4997e1c7219b4cff6a
mirfidron <mirfidron@mail.ru>
2023-04-28 17:51:01 +0300

    added an error handler to todo ~ v.4

--- mirfidron repo/todo.js
+++ mirfidron repo/todo.js
@@ -27,77 +27,91 @@ function clearInput2() {
 };
 
 function addTask(event) {
-    event.preventDefault();   
-    const newDiv = document.createElement('div');
-    list.appendChild(newDiv);
-    newDiv.classList.add("todo-list");
-    newDiv.insertAdjacentHTML('afterbegin', `<div class="todo-box"><input class="checkbox" type="checkbox" name="todo" id=""><div class="space"><label class="todo-ht" for="todo">${inp.value}</label><button type="submit" class="button-del"><img class="del" src="del.jpg" alt=""></button></div></div>`);
-    addObject(`${inp.value}`,'To Do', 'high');
-    console.log(ToDo);
-    const delDiv = newDiv.querySelector('.button-del');
-    const checkbox = newDiv.querySelector('.checkbox');
-    checkbox.addEventListener('change',() => {
-        const check = checkbox.checked;
-        if (check) {
-            newDiv.style.backgroundColor = "#d0bdf4";
-            const text = newDiv.textContent;
-            const index = ToDo.findIndex(task => text == task.name);
-            ToDo[index].status = "Done";
-            console.log(ToDo)
-       
-        } else {
-            newDiv.style.backgroundColor = "white";
-            const text = newDiv.textContent;
-            const index = ToDo.findIndex(task => text == task.name);
-            ToDo[index].status = "To Do";
-            console.log(ToDo)
-        }
-    });
-    delDiv.addEventListener('click',() => {
-        const text = newDiv.textContent;
-        const index = ToDo.findIndex(task => text == task.name);
-        ToDo.splice(index,1);
-        list.removeChild(newDiv);
+    event.preventDefault(); 
+    try {
+        if (inp.value == '') {
+        throw new SyntaxError("Поле не может быть пустым");
+          };  
+        const newDiv = document.createElement('div');
+        list.appendChild(newDiv);
+        newDiv.classList.add("todo-list");
+        newDiv.insertAdjacentHTML('afterbegin', `<div class="todo-box"><input class="checkbox" type="checkbox" name="todo" id=""><div class="space"><label class="todo-ht" for="todo">${inp.value}</label><button type="submit" class="button-del"><img class="del" src="del.jpg" alt=""></button></div></div>`);
+        addObject(`${inp.value}`,'To Do', 'high');
         console.log(ToDo);
-    });
-    clearInput();
+        const delDiv = newDiv.querySelector('.button-del');
+        const checkbox = newDiv.querySelector('.checkbox');
+        checkbox.addEventListener('change',() => {
+            const check = checkbox.checked;
+            if (check) {
+                newDiv.style.backgroundColor = "#d0bdf4";
+                const text = newDiv.textContent;
+                const index = ToDo.findIndex(task => text == task.name);
+                ToDo[index].status = "Done";
+                console.log(ToDo)
+        
+            } else {
+                newDiv.style.backgroundColor = "white";
+                const text = newDiv.textContent;
+                const index = ToDo.findIndex(task => text == task.name);
+                ToDo[index].status = "To Do";
+                console.log(ToDo)
+            }
+        });
+        delDiv.addEventListener('click',() => {
+            const text = newDiv.textContent;
+            const index = ToDo.findIndex(task => text == task.name);
+            ToDo.splice(index,1);
+            list.removeChild(newDiv);
+            console.log(ToDo);
+        });
+        clearInput();
+    } catch(e) {
+        alert(e.message);
+        }
     
 }
 
 function addTask2(event) {
     event.preventDefault();
-    const newDiv = document.createElement('div');
-    list2.appendChild(newDiv);
-    newDiv.classList.add("todo-list");
-    newDiv.insertAdjacentHTML('afterbegin', `<div class="todo-box"><input class="checkbox" type="checkbox" name="todo" id=""><div class="space"><label class="todo-ht" for="todo">${inp2.value}</label><button type="submit" class="button-del2"><img class="del2" src="del.jpg" alt=""></button></div></div>`);
-    addObject(`${inp2.value}`,'To Do', 'low');
-    const delDiv = newDiv.querySelector('.button-del2');
-    const checkbox = newDiv.querySelector('.checkbox'); 
-    checkbox.addEventListener('change',() => {
-        const check = checkbox.checked;
-        if (check) {
-            newDiv.style.backgroundColor = "#d0bdf4";
+    try {
+        if (inp2.value == '') {
+        throw new SyntaxError("Поле не может быть пустым");
+          };
+        const newDiv = document.createElement('div');
+        list2.appendChild(newDiv);
+        newDiv.classList.add("todo-list");
+        newDiv.insertAdjacentHTML('afterbegin', `<div class="todo-box"><input class="checkbox" type="checkbox" name="todo" id=""><div class="space"><label class="todo-ht" for="todo">${inp2.value}</label><button type="submit" class="button-del2"><img class="del2" src="del.jpg" alt=""></button></div></div>`);
+        addObject(`${inp2.value}`,'To Do', 'low');
+        const delDiv = newDiv.querySelector('.button-del2');
+        const checkbox = newDiv.querySelector('.checkbox'); 
+        checkbox.addEventListener('change',() => {
+            const check = checkbox.checked;
+            if (check) {
+                newDiv.style.backgroundColor = "#d0bdf4";
+                const text = newDiv.textContent;
+                const index = ToDo.findIndex(task => text == task.name);
+                ToDo[index].status = "Done";
+                console.log(ToDo)
+        
+            } else {
+                newDiv.style.backgroundColor = "white";
+                const text = newDiv.textContent;
+                const index = ToDo.findIndex(task => text == task.name);
+                ToDo[index].status = "To Do";
+                console.log(ToDo)
+            }
+        });
+        delDiv.addEventListener('click',() => {
             const text = newDiv.textContent;
             const index = ToDo.findIndex(task => text == task.name);
-            ToDo[index].status = "Done";
-            console.log(ToDo)
-       
-        } else {
-            newDiv.style.backgroundColor = "white";
-            const text = newDiv.textContent;
-            const index = ToDo.findIndex(task => text == task.name);
-            ToDo[index].status = "To Do";
-            console.log(ToDo)
-        }
-    });
-    delDiv.addEventListener('click',() => {
-        const text = newDiv.textContent;
-        const index = ToDo.findIndex(task => text == task.name);
-        ToDo.splice(index,1);
-        list2.removeChild(newDiv);
-        console.log(ToDo);
-    });
-    clearInput2();  
+            ToDo.splice(index,1);
+            list2.removeChild(newDiv);
+            console.log(ToDo);
+        });
+        clearInput2();
+    } catch(e) {
+        alert(e.message);
+      } 
 }
 
 
