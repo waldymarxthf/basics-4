41fe1e055820d7d64994ddec059ef14ab0ad4b3e
Vadim Sysoev <sysoev.dev@gmail.com>
2023-04-28 18:15:18 +0300

    complete task

---
+++ sysoev-dev/main.mjs
@@ -0,0 +1,16 @@
+import data from './data.json' assert { type: 'json' };
+
+function showFirstNames(array) {
+  array.forEach(item => {
+    console.log(item.firstName);
+  });
+}
+
+function showUsersDetails(array) {
+  array.forEach(item => {
+    console.log(`${item.firstName}, born at ${item.dateOfBirth} and ${item.knowsAs}`);
+  });
+}
+
+showFirstNames(data.users);
+showUsersDetails(data.users);
