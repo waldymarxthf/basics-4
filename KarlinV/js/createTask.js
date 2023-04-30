c6245db38fd6390393116b531970fad87992c295
Владимир Карлин <vow.carlin@yandex.ru>
2023-04-30 13:13:04 +0300

    add function

--- KarlinV/js/createTask.js
+++ KarlinV/js/createTask.js
@@ -1,77 +1,26 @@
-import { setTasksLocalStorage } from "./localStorage.js";
-const KEY_LOCAL_STORAGE = "taskUser";
+export const createTask = (task) => {
+  const taskItem = document.createElement("li");
+  const taskCheckBoxContainer = document.createElement("div");
+  const taskName = document.createElement("span");
+  const taskButtonDelete = document.createElement("button");
+  const taskItemScreen = document.createElement("div");
 
-export function createTasks(tasks) {
-  const highList = document.querySelector("#high-list");
-  const lowList = document.querySelector("#low-list");
-  highList.innerHTML = "";
-  lowList.innerHTML = "";
+  taskItem.classList.add("todo__item");
+  if (task.done) {
+    taskItem.classList.add("checked");
+  }
 
-  tasks.forEach((task) => {
-    const taskItem = document.createElement("li");
-    const taskCheckBoxContainer = document.createElement("div");
-    const taskInputCheckbox = document.createElement("input");
-    const taskLabelCheckBox = document.createElement("label");
-    const taskButtonDelete = document.createElement("button");
-    const taskItemScreen = document.createElement("div");
+  taskCheckBoxContainer.classList.add("todo__check");
+  taskButtonDelete.classList.add("todo__btn-delete-item");
+  taskItemScreen.classList.add("todo__item-screen");
 
-    taskItem.classList.add("todo__item");
-    taskCheckBoxContainer.classList.add("todo__check");
-    taskButtonDelete.classList.add("todo__btn-delete-item");
-    taskItemScreen.classList.add("todo__item-screen");
+  taskItem.setAttribute("id", task.id);
+  taskName.textContent = task.name;
 
-    taskInputCheckbox.type = "checkbox";
-    taskInputCheckbox.name = task.id;
-    taskInputCheckbox.id = task.id;
+  taskItem.append(taskCheckBoxContainer);
+  taskItem.append(taskName);
+  taskItem.append(taskButtonDelete);
+  taskItem.append(taskItemScreen);
 
-    taskLabelCheckBox.for = task.id;
-    taskLabelCheckBox.textContent = task.name;
-
-    taskItem.addEventListener("click", () => {
-      const task = tasks.find((task) => task.id === taskInputCheckbox.id);
-
-      if (taskItem.classList.contains("checked")) {
-        taskItem.classList.remove("checked");
-        taskInputCheckbox.removeAttribute("checked");
-
-        if (task && task.done !== undefined) {
-          task.done = false;
-          setTasksLocalStorage(KEY_LOCAL_STORAGE, tasks);
-        }
-      } else {
-        taskItem.classList.add("checked");
-        taskInputCheckbox.setAttribute("checked", "");
-
-        if (task && task.done !== undefined) {
-          task.done = true;
-          setTasksLocalStorage(KEY_LOCAL_STORAGE, tasks);
-        }
-      }
-    });
-
-    taskButtonDelete.addEventListener("click", () => {
-      const index = tasks.findIndex((task) => task.id === taskInputCheckbox.id);
-
-      taskItem.remove();
-      tasks.splice(index, 1);
-      setTasksLocalStorage(KEY_LOCAL_STORAGE, tasks);
-    });
-
-    taskCheckBoxContainer.append(taskInputCheckbox);
-    taskItem.append(taskCheckBoxContainer);
-    taskItem.append(taskLabelCheckBox);
-    taskItem.append(taskButtonDelete);
-    taskItem.append(taskItemScreen);
-
-    if (task.status === "high") {
-      highList.append(taskItem);
-    } else {
-      lowList.append(taskItem);
-    }
-
-    if (task.done) {
-      taskItem.classList.add("checked");
-      taskInputCheckbox.setAttribute("checked", "");
-    }
-  });
-}
+  return taskItem;
+};
--- KarlinV/js/localStorage.js
+++ KarlinV/js/localStorage.js
@@ -1,8 +1,21 @@
 export function setTasksLocalStorage(key, arr) {
-  localStorage.setItem(key, JSON.stringify(arr));
+  try {
+    localStorage.setItem(key, JSON.stringify(arr));
+  } catch (error) {
+    console.error(
+      `Не удалось сохранить задачи в локальное хранилище: ${error}`
+    );
+  }
 }
 
 export function getTasksLocalStorage(key) {
-  const value = localStorage.getItem(key);
-  return JSON.parse(value);
+  try {
+    const value = localStorage.getItem(key);
+    return JSON.parse(value);
+  } catch (error) {
+    console.error(
+      `Не удалось получить задачи из локального хранилища: ${error}`
+    );
+    return null;
+  }
 }
