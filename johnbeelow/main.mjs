bde8df08fcb50c8f0929e1ba95f6f3a399a062d3
johnbeelow <beelow-dj@mail.ru>
2023-04-30 14:25:04 +0300

    add 47

---
+++ johnbeelow/main.mjs
@@ -0,0 +1,8 @@
+import data from './data.json' assert { type: 'json' }
+
+
+let person = data.users[0].firstName + ", born at " + data.users[0].dateOfBirth + " and " + data.users[0].knowsAs;
+
+console.log(person); 
+
+
