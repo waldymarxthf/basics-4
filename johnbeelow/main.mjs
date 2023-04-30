9e0ea6e28296105b2891d53fdb49deea9051a05c
John <98343581+johnbeelow@users.noreply.github.com>
2023-04-30 15:49:45 +0300

    Update main.mjs

--- johnbeelow/main.mjs
+++ johnbeelow/main.mjs
@@ -1,8 +1,10 @@
 import data from './data.json' assert { type: 'json' }
 
+let person =
+  data.users[0].firstName +
+  ', born at ' +
+  data.users[0].dateOfBirth +
+  ' and ' +
+  data.users[0].knowsAs
 
-let person = data.users[0].firstName + ", born at " + data.users[0].dateOfBirth + " and " + data.users[0].knowsAs;
-
-console.log(person); 
-
-
+console.log(person)
