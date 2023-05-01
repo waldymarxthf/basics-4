ad7970a9b808185315433a63828b82e976540ee3
Vadim Sysoev <sysoev.dev@gmail.com>
2023-05-01 15:30:52 +0300

    add collback

--- main.js
+++ main.js
@@ -1,11 +1,36 @@
-// Синхронный колбэк
-function greeting(name) {
-  console.log(`Hello ${name}`);
+const UI_ELEMENTS = {
+  IMG_BOX: document.querySelector('.img-box'),
+};
+
+function loadImage(src, callback) {
+  const img = document.createElement('img');
+  setTimeout(() => {
+    img.src = src;
+  }, 3000);
+  console.log('Какой-то код');
+  img.onload = () => callback(null, img);
+  img.onerror = () => callback(new Error('Ошибка'));
 }
 
-function getName(callback) {
-  let name = 'Vadim';
-  callback(name);
+function showError(error) {}
+
+function callback(error, image) {
+  if (error) {
+    showError(error);
+  }
+  UI_ELEMENTS.IMG_BOX.append(image);
 }
 
-getName(greeting);
+loadImage('https://avatars.githubusercontent.com/u/24270731?v=4', callback);
+
+// // Синхронный колбэк
+// function greeting(name) {
+//   console.log(`Hello ${name}`);
+// }
+
+// function getName(callback) {
+//   let name = 'Vadim';
+//   callback(name);
+// }
+
+// getName(greeting);
