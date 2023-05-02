2fd9ad2714379c18fb9669e06637f4a8029889f1
Vitalii Miagkov <maposia@gmail.com>
2023-05-02 12:25:45 +0300

    del files

--- maposia/main.mjs
+++
@@ -1,9 +0,0 @@
-import data from './data/data.json' assert { type: 'json' }
-
-const users = data.users
-
-users.forEach((user) => {
-  console.log(
-    `Пользователь ${user.firstName} ${user.lastName} родился ${user.dateOfBirth} известен как ${user.knowsAs}`
-  )
-})
