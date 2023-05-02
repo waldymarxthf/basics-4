43065c49f107b44f6c88ecec69f16772cedd8564
Vadim Sysoev <sysoev.dev@gmail.com>
2023-05-02 21:12:45 +0300

    add code

--- sysoev-dev/main.js
+++ sysoev-dev/main.js
@@ -39,32 +39,81 @@
 
 // delay(3000).then(result => console.log(`Выполнилось через 3 секунды, результат: ${result}`));
 
-const btn = document.querySelector('.btn');
+// const btn = document.querySelector('.btn');
 
-function imgLoad(src) {
+// function imgLoad(src) {
+//   return new Promise(function (resolve, reject) {
+//     const img = document.createElement('img');
+//     img.src = src;
+//     img.onload = () => {
+//       resolve(img);
+//     };
+//     img.onerror = () => {
+//       reject(new Error('Ошибка загрузки'));
+//     };
+//   });
+// }
+
+// btn.addEventListener('click', () => {
+//   imgLoad('https://w.wallhaven.cc/full/ex/wallhaven-ex9gwo.png').then(
+//     function (result) {
+//       document.body.append(result);
+//     },
+//     function (error) {
+//       alert(error);
+//     }
+//   );
+// });
+
+// setTimeout(() => {
+//   console.log('3');
+// }, 2000);
+
+// console.log('1');
+
+// setTimeout(function () {
+//   console.log('2');
+// }, 5000);
+
+// console.log('3');
+
+// setTimeout(function () {
+//   console.log('4');
+// }, 5000);
+// setTimeout(function () {
+//   console.log('5');
+// }, 5000);
+
+// let promise = new Promise(function (resolve, reject) {
+//   setTimeout(() => {
+//     resolve(10);
+//   }, 500);
+// });
+
+// promise
+//   .then(value => {
+//     console.log(value);
+//     console.log(typeof value);
+//     return value;
+//   })
+//   .then(value => value * 5)
+//   .then(value => {
+//     console.log(value); //50
+//   })
+//   .finally(() => {
+//     console.log('preloader');
+//   });
+
+function withPromise() {
   return new Promise(function (resolve, reject) {
-    const img = document.createElement('img');
-    img.src = src;
-    img.onload = () => {
-      resolve(img);
-    };
-    img.onerror = () => {
-      reject(new Error('Ошибка загрузки'));
-    };
+    reject('OKK');
   });
 }
 
-btn.addEventListener('click', () => {
-  imgLoad('https://w.wallhaven.cc/full/ex/wallhaven-ex9gwo.png').then(
-    function (result) {
-      document.body.append(result);
-    },
-    function (error) {
-      alert(error);
-    }
-  );
-});
-
-setTimeout(() => {
-  console.log('3');
-}, 2000);
+withPromise()
+  .then(value => {
+    console.log(`Успех ${value}`);
+  })
+  .catch(value => {
+    console.log(`Ошибка ${value}`);
+  });
