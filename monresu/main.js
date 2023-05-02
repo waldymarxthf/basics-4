54b8db54f5bb23bd879c5da7d9815b0acd1fa8f9
Anastasia <tgmonresu@gmail.com>
2023-05-02 06:37:57 +0300

    a

--- monresu/main.js
+++ monresu/main.js
@@ -1,11 +1,40 @@
-const form = document.querySelector('form')
-const input = document.querySelector('input')
+/* Task 2 from learn.js */
+function delay(ms) {
+  let promise = new Promise(function(resolve) {
+    setTimeout(() => resolve(), ms)
+  })
+  return promise;
+}
 
-form.addEventListener('submit', () => foo(e, user.name))
+delay(3000).then(() => console.log('выполнилось через 3 секунды'));
 
-function foo(e, name) {
-  e.preventDefault();
-  console.log(text)
-  const text = input.value;
-  document.body.insertAdjacentHTML('beforeend', text);
+/* Task 3 from learn.js */
+
+function go() {
+  showCircle(150, 150, 100)
+  .then( div => {
+    div.classList.add('message-ball');
+    div.append("Привет, мир!");
+  });
+}
+
+function showCircle(cx, cy, radius) {
+  let div = document.createElement('div');
+  div.style.width = 0;
+  div.style.height = 0;
+  div.style.left = cx + 'px';
+  div.style.top = cy + 'px';
+  div.className = 'circle';
+  document.body.append(div);
+
+  return new Promise(resolve => setTimeout(() => {
+    div.style.width = radius * 2 + 'px';
+    div.style.height = radius * 2 + 'px';
+
+    div.addEventListener('transitionend', function handler() {
+      div.removeEventListener('transitionend', handler);
+      resolve(div);
+    });
+  }, 0)
+  );
 }
