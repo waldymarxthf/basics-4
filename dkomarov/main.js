b585a2dc75da4b0ae11429db2c123d91a1aca7ac
Dmitry Komarov <dmitrykomarov@MacBook-Pro-Dmitry.local>
2023-05-04 12:29:45 +0300

    gendegize

---
+++ dkomarov/main.js
@@ -0,0 +1,26 @@
+const inputValue = document.querySelector('.inputName');
+const button = document.querySelector('.btn');
+const form = document.querySelector('.form');
+
+
+function getRequest() {
+    const firstName = inputValue.value;
+    const serverUrl = 'https://api.genderize.io';
+    const url = `${serverUrl}?name=${firstName}`;
+    
+    fetch(url)
+        .then(response => response.json()) 
+        .then(data => alert(`${firstName} - ${data.gender}`));
+
+}
+
+function clearInpt() {
+    inputValue.value = '';
+}
+
+
+form.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    getRequest();
+    clearInpt();
+})
