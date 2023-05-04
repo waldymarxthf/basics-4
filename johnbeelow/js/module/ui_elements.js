8bc3854f69d66370bbbcd6bb1ebae85f1da43040
John <98343581+johnbeelow@users.noreply.github.com>
2023-05-04 23:07:27 +0300

    Update ui_elements.js

--- johnbeelow/js/module/ui_elements.js
+++ johnbeelow/js/module/ui_elements.js
@@ -1,14 +1,14 @@
 export const createTag = (tag) => document.createElement(tag)
 
 export const CLASS_GENDER = {
-MALE: 'result_male',
-FEMALE: 'result_female',
-UNKOWN: 'result_null',
+  MALE: 'result_male',
+  FEMALE: 'result_female',
+  UNKOWN: 'result_null',
 }
 
 export const UI_ELEMENTS = {
-    INPUT_FORM: document.querySelector('.input_form'),
-    INPUT_TEXT: document.querySelector('.input_text'),
-    RESULT_NAME: document.querySelector('.result_name'),
-    RESULT_CONTAINER: document.querySelector('.result_container'),
-  }
+  INPUT_FORM: document.querySelector('.input_form'),
+  INPUT_TEXT: document.querySelector('.input_text'),
+  RESULT_NAME: document.querySelector('.result_name'),
+  RESULT_CONTAINER: document.querySelector('.result_container'),
+}
