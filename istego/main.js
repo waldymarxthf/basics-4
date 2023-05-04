dab95a90aa94384953b5992f8464bb5fcc5c4b1f
Ivan Marahovskii <marahovskii81@gmail.com>
2023-05-04 11:20:23 +0500

    #53 Genderize

--- istego/main.js
+++ istego/main.js
@@ -1,1 +1,33 @@
-// здесь пока пусто
+const form = document.querySelector('form');
+const inputName = form.querySelector('#inputName');
+const btn = form.querySelector('#btn');
+const resultUi = document.querySelector('.result-ui');
+const preload = document.querySelector('.preload-wrapper');
+
+const serverUrl = 'https://api.genderize.io';
+
+
+form.addEventListener('submit', async (event) => {
+    event.preventDefault();
+
+    const firstName = inputName.value;
+    const url = `${serverUrl}?name=${firstName}`;
+
+    try {
+        preload.style.display = 'flex';
+        const response = await fetch(url);
+        const genderData = await response.json();
+        
+        if (genderData.gender) {
+            resultUi.textContent = `${firstName} is ${genderData.gender}`;
+            inputName.value = '';
+            preload.style.display = 'none';
+        } else {
+            resultUi.textContent = `Не удалось определить пол`;
+            preload.style.display = 'none';
+        }
+    } catch (error) {
+        resultUi.textContent = 'Error: ' + error.message;
+        preload.style.display = 'none';
+    }
+})
