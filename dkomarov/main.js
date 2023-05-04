d02d27de6ebbfef1dad549b4702106625a89bc94
Dmitry Komarov <dmitrykomarov@MacBook-Pro-Dmitry.local>
2023-05-04 17:33:53 +0300

    async await

--- dkomarov/main.js
+++ dkomarov/main.js
@@ -2,25 +2,34 @@ const inputValue = document.querySelector('.inputName');
 const button = document.querySelector('.btn');
 const form = document.querySelector('.form');
 
-
-function getRequest() {
-    const firstName = inputValue.value;
-    const serverUrl = 'https://api.genderize.io';
-    const url = `${serverUrl}?name=${firstName}`;
-    
-    fetch(url)
-        .then(response => response.json()) 
-        .then(data => alert(`${firstName} - ${data.gender}`));
-
-}
-
 function clearInpt() {
     inputValue.value = '';
 }
 
 
-form.addEventListener('submit', (e) => {
+form.addEventListener('submit',  (e) => {
     e.preventDefault();
     getRequest();
     clearInpt();
 })
+
+
+async function getRequest() {
+    const firstName = inputValue.value;
+    const serverUrl = 'https://api.genderize.io';
+    const url = `${serverUrl}?name=${firstName}`;
+    try {
+        const response = await fetch(url);
+        const data = await response.json()
+        alert(`${firstName} is ${data.gender}`);
+        
+    } catch(Err) {
+        console.error(Err);
+    }
+     
+
+}
+
+
+
+
