445aa0f23823ee0eb26140f65b3bb4d0aeae7e6c
waldymarxthf <iliya.nov223@gmail.com>
2023-05-04 17:42:58 +0300

    [chore] chore: add try catch

--- waldymarxthf/js/script.js
+++ waldymarxthf/js/script.js
@@ -1,51 +1,65 @@
 import { formElement, genderize } from "./modules/ui-variables.js";
-import { getLink, getIndex} from "./modules/utils.js";
+import { getLink, getIndex } from "./modules/utils.js";
 import { saveGenderizeToLocalStorage, loadPaul } from "./modules/localStorage.js";
 
-export let history = []
+export let history = [];
 
-function createGenderize({name, gender}) {
-	let newGenderize = document.createElement('p')
-	newGenderize.classList.add('genderize-item')
-	newGenderize.textContent = `${name} is ${gender}`
-	newGenderize.addEventListener('click', () => {
-		deleteGenderize(newGenderize)
-	})
-	return newGenderize
+function createGenderize(paul) {
+	let newGenderize = document.createElement("p");
+	newGenderize.classList.add("genderize-item");
+	newGenderize.textContent = `${paul.name} is ${paul.gender}`;
+	newGenderize.addEventListener("click", () => {
+		deleteGenderize(newGenderize);
+	});
+	return newGenderize;
 }
 
 export function render() {
-	genderize.innerHTML = ''
+	genderize.innerHTML = "";
 	history.forEach((paul) => {
-		const genderizeElem = createGenderize(paul)
-		genderize.append(genderizeElem)
-	})
+		const genderizeElem = createGenderize(paul);
+		genderize.append(genderizeElem);
+	});
 }
 
-function deleteGenderize(paul) {
-	let index = getIndex(paul)
-	history.splice(index, 1)
-	console.log(history)
-	render()
-	saveGenderizeToLocalStorage()
+function deleteGenderize(newGenderize) {
+	let index = getIndex(newGenderize);
+	history.splice(index, 1);
+	render();
+	saveGenderizeToLocalStorage();
 }
 
 async function getPaul() {
-	let response = await fetch(getLink());
-	let paul = await response.json();
-	history.push({
-		name: paul.name,
-		gender: paul.gender
-	})
-	console.log(history)
-	render()
-	saveGenderizeToLocalStorage()
+
+	try {
+		let response = await fetch(getLink());
+		let paul = await response.json();
+
+		if (paul.gender) {
+
+			history.push({
+				name: paul.name,
+				gender: paul.gender,
+			});
+
+		} else {
+			alert("Не удалось определить пол");
+			console.error("Не удалось определить пол")
+		}
+
+	} catch (error) {
+		alert(`Error: ${error.message}`);
+		console.error(error)
+	}
+
+	render();
+	saveGenderizeToLocalStorage();
 }
 
-formElement.addEventListener('submit', (event) => {
-	event.preventDefault()
-	getPaul()
-	event.target.reset()
-})
+formElement.addEventListener("submit", (event) => {
+	event.preventDefault();
+	getPaul();
+	event.target.reset();
+});
 
-loadPaul()
+loadPaul();
