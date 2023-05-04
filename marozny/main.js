5222f5cb55fb9133ba214e8c6addf9da7be105ea
Ruslan Maroz <ich.nemoroz@gmail.com>
2023-05-04 18:43:29 +0200

    rewrite

--- marozny/main.js
+++ marozny/main.js
@@ -1,8 +1,10 @@
-const btnHighTask = document.querySelector(".btn-adding-high");
-const highTaskValue = document.querySelector(".highTaskValue");
-const lowTaskValue = document.querySelector(".lowTaskValue");
-const taskHighList = document.querySelector(".tasks-high-priority");
-const btnDelete = document.querySelector(".btn-task-delete");
+const qS = (element) => document.querySelector(element);
+const highInput = qS(".input_text_high");
+const lowInput = qS(".input_text_low");
+const formHigh = qS(".task_form_high");
+const formLow = qS(".task_form_low");
+const highTask = qS(".task_priority_high");
+const lowTask = qS(".task_priority_low");
 
 const list = [];
 
@@ -16,58 +18,78 @@ const STATUS = {
     DONE: "Done"
   };  
 
-const updateStatus = (name, status) => {
-const indexTask = list.findIndex(task => task.name === name);
-list[indexTask].status = status;
-render();
+function addTask(name, priority, status) {
+    list.push({name,priority,status})
 }
 
-const deleteTask = (name) => {
-const indexTask = list.findIndex(task => task.name === name);
-list.splice(indexTask, 1);
-render()
-}
+// const updateStatus = (name, status) => {
+// const indexTask = list.findIndex(task => task.name === name);
+// list[indexTask].status = status;
+// render();
+// }
+
+// const deleteTask = (name) => {
+// const indexTask = list.findIndex(task => task.name === name);
+// list.splice(indexTask, 1);
+// render()
+// }
 
 function render() {
-    taskHighList.innerHTML = "";
-    taskLowList.innerHTML = "";
+    highTask.innerHTML = "";
+    lowTask.innerHTML = "";
     for (const task of list) {
-        if (task.PRIORITY === HIGH) {
-            let taskValue = highTaskValue.value;
-    taskHighList.insertAdjacentHTML('beforeend', `<div class="task-item task-high-priority">
-    <label>
-        <input type="checkbox" class="input-checkbox">
-        <div class="task-text">
-            ${taskValue}
-        </div>
-    </label>
-    <button class="btn-task-delete"></button>
-</div>`);
+        if (task.priority === PRIORITY.HIGH) {
+            createUI(task.name, task.status, PRIORITY.HIGH);
         }
-        let taskValue = lowTaskValue.value;
-        taskHighList.insertAdjacentHTML('beforeend', `<div class="task-item task-low-priority">
-        <label>
-            <input type="checkbox" class="input-checkbox">
-            <div class="task-text">
-                ${taskValue}
-            </div>
-        </label>
-        <button class="btn-task-delete"></button>
-    </div>`); 
+        if (task.priority === PRIORITY.LOW) {
+            createUI(task.name, task.status, PRIORITY.LOW);
+        }
     }
 }
 
-function addTask (event, priority, status) {
-    list.push({
-        name: event,
-        status: status,
-        priority: priority
-    });
+function createUI(name, status, priority) {
+    const containerTask = document.createElement('div');
+    const labelTask = document.createElement('label');
+    const statusTask = document.createElement('input');
+    const textTask = document.createElement('span');
+    const deleteTaskButton = document.createElement('button');
+  
+    containerTask.classList.add('task');
+    statusTask.classList.add('checkbox');
+    statusTask.type = 'checkbox';
+    textTask.classList.add('task_text');
+    textTask.textContent = name;
+    deleteTaskButton.classList.add('button_close_task');
+    deleteTaskButton.textContent = '×';
+  
+    priority.appendChild(containerTask);
+    containerTask.appendChild(labelTask);
+    labelTask.appendChild(statusTask);
+    labelTask.appendChild(textTask);
+    containerTask.appendChild(deleteTaskButton);
+
+    if (status === STATUS.DONE) {
+        statusTask.setAttribute('checked', true)
+      };
+     deleteTaskButton.addEventListener('click', () => {
+        deleteTask(name)
+        render()
+      })
+      statusTask.addEventListener('change', () => {
+        changeStatus(name, statusTask.checked ? STATUS.DONE : STATUS.TODO)
+        render()
+      })
+}
+            
+
+formHigh.addEventListener("submit", (event) => {
+    event.preventDefault();
+    addTask(highInput.value, PRIORITY.HIGH, STATUS.TODO);
     render();
-    }
+});
 
-
-btnHighTask.addEventListener("submit", (event) => addTask(event, PRIORITY.HIGH, STATUS.TODO));
-btnLowTask.addEventListener("submit", (event) => addTask(event, PRIORITY.LOW, STATUS.TODO));
-
-
+formLow.addEventListener("submit", (event) => {
+    event.preventDefault();
+    addTask(lowInput.value, PRIORITY.LOW, STATUS.TODO);
+    render();
+});
