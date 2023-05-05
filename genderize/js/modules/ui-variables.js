20b74554b6fa3a5d82d2119a89c90771352a310d
waldymarxthf <iliya.nov223@gmail.com>
2023-05-05 12:40:04 +0300

    [fix] fix: icon search

--- genderize/js/modules/ui-variables.js
+++
@@ -1,6 +0,0 @@
-const formElement = document.querySelector('.name-form')
-const genderize = document.querySelector('.genderize')
-const inputElement = document.querySelector('.name-input')
-const preloader = document.querySelector('.preloader')
-
-export {formElement, genderize, inputElement, preloader}
