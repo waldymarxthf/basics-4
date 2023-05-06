130902bc270179949c15085ff5c2228df1c3e7b1
Vadim Sysoev <sysoev.dev@gmail.com>
2023-05-06 17:53:48 +0300

    переделал пример на цепочку промисов

---
+++ sysoev-dev/main.js
@@ -0,0 +1,15 @@
+new Promise(function (resolve, reject) {
+  setTimeout(() => resolve(1), 1000);
+})
+  .then(function (result) {
+    alert(result); // 1
+    return result * 2;
+  })
+  .then(function (result) {
+    alert(result); // 1
+    return result * 2;
+  })
+  .then(function (result) {
+    alert(result); // 1
+    return result * 2;
+  });
