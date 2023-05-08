b433cfde02ab2df93db64f5c86fe874e0dbac0c6
mirfidron <mirfidron@mail.ru>
2023-05-08 14:18:01 +0300

    weather ~ v.3

--- mirfidron repo/main.js
+++ mirfidron repo/main.js
@@ -9,7 +9,7 @@ function addObj(weather) {
     weatherNow.insertBefore(newDiv, weatherNow.children[0]);
     newDiv.classList.add('tab-hidden');
     newDiv.id = 'tab-1'
-    newDiv.insertAdjacentHTML("afterbegin", `<div class="temperature">${weather.main.temp}&deg;</div>
+    newDiv.insertAdjacentHTML("afterbegin", `<div class="temperature">${Math.floor(weather.main.temp)}&deg;</div>
     <div class="img">
         <img src="./img/cloud.svg" alt="cloud">
     </div>
@@ -25,7 +25,7 @@ async function add() {
     const serverUrl = 'http://api.openweathermap.org/data/2.5/weather';
     const cityName = addPlace.value;
     const apiKey = 'f660a2fb1e4bad108d6160b7f58c555f';
-    const url = `${serverUrl}?q=${cityName}&appid=${apiKey}`;
+    const url = `${serverUrl}?q=${cityName}&appid=${apiKey}&units=metric`;
     let response = await fetch(url);
     let weather = await response.json();
     addObj(weather);
