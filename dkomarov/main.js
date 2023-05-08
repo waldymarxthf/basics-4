1b476b1df683e237b803111b73c4fee2bc6244fa
Dmitry Komarov <dmitrykomarov@MacBook-Pro-Dmitry.local>
2023-05-09 01:09:20 +0300

    weather v2.1

--- dkomarov/main.js
+++ dkomarov/main.js
@@ -12,7 +12,10 @@ function clearInput() {
 
 tabs.forEach((tab, index) => {
     tab.addEventListener('click', () => {
+        tabs.forEach(t => t.classList.remove('active-tab'))
         displayItem.forEach(el => el.classList.remove('active'))
+
+        tab.classList.add('active-tab')
         displayItem[index].classList.add('active')
     })
 });
@@ -22,7 +25,7 @@ tabs.forEach((tab, index) => {
 async function getRequest() {
     const serverUrl = 'http://api.openweathermap.org/data/2.5/weather';
     const cityName = inputValue.value;
-    const apiKey = 'f660a2fb1e4bad108d6160b7f58c555f';
+    const apiKey = '2de34209accba46efc52dfd946a3c2b3';
     const url = `${serverUrl}?q=${cityName}&appid=${apiKey}&units=metric`;
 
     const response = await fetch(url);
@@ -30,22 +33,33 @@ async function getRequest() {
     console.log(data);
 
     createDisplayNow(data);
+    createDisplayDetails(data);
     clearInput();
 
 };
 
 function createDisplayNow(data) {
-    const tempSpan = document.querySelector('.weather-temp span');
-    const temperature = data.main.temp;
-    tempSpan.textContent = temperature.toFixed(0);
+    try {
+        const tempSpan = document.querySelector('.weather-temp span');
+        const temperature = data.main.temp;
+        tempSpan.textContent = temperature.toFixed(0);
 
 
-    const iconId = data.weather[0].icon
-    const iconImg = `https://openweathermap.org/img/wn/${iconId}@4x.png`;
-    const iconWeather = document.querySelector('.weather-icon img');
-    iconWeather.src = iconImg;
+        const iconId = data.weather[0].icon
+        const iconImg = `https://openweathermap.org/img/wn/${iconId}@4x.png`;
+        const iconWeather = document.querySelector('.weather-icon img');
+        iconWeather.src = iconImg;
 
-    const cityName = document.querySelector('.location-target span');
+        const cityName = document.querySelector('.location-target span');
+        cityName.textContent = data.name;
+    }
+    catch(Err) {
+        console.error(Err);
+    }
+};
+
+function createDisplayDetails(data) {
+    const cityName = document.querySelector('.details-city');
     cityName.textContent = data.name;
 }
 
