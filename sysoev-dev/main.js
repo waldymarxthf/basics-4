cf8da466a726b4e9671aac83d0ca0441e228cbd3
Vadim Sysoev <sysoev.dev@gmail.com>
2023-05-09 00:07:35 +0300

    Добавил try catch

--- sysoev-dev/main.js
+++ sysoev-dev/main.js
@@ -1,16 +1,25 @@
 import { UI_ELEMENTS } from './js/ui.js';
 import { showWeatherData } from './js/show.js';
 
+function showError(error) {
+  alert(error);
+}
+
 async function getWeatherData(cityName) {
   const serverUrl = 'http://api.openweathermap.org/data/2.5/weather';
   const apiKey = 'f660a2fb1e4bad108d6160b7f58c555f';
   const url = `${serverUrl}?q=${cityName}&appid=${apiKey}&units=metric`;
-  let response = await fetch(url);
-  if (response.ok) {
-    const json = await response.json();
-    showWeatherData(json);
-  } else {
-    alert(response.status);
+
+  try {
+    let response = await fetch(url);
+    if (response.ok) {
+      const json = await response.json();
+      showWeatherData(json);
+    } else {
+      throw new Error((await response.json()).message);
+    }
+  } catch (error) {
+    showError(error);
   }
 }
 
