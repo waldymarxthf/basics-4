afeb1ab37d7ce09fc62135bb7fae7b742c03dc59
Ruslan Maroz <ich.nemoroz@gmail.com>
2023-05-09 17:54:03 +0200

    v1

--- marozny/script.js
+++ marozny/script.js
@@ -1,5 +1,10 @@
 const tabs = document.querySelectorAll('.tabs__item')
 const weatherBlock = document.querySelectorAll('.weather__block')
+const searchForm = document.querySelector('.weather__search-form')
+const inputCity = document.querySelector('.weather__search-form-input')
+const temp = document.querySelector('.weather__block-temp')
+const currentCity = document.querySelector('.weather__block-content-city')
+const currentIcon = document.querySelector('.weather__block-cloud')
 
 tabs.forEach((tab, index) => {
 	tab.addEventListener('click', () => {
@@ -10,3 +15,27 @@ tabs.forEach((tab, index) => {
 		weatherBlock[index].classList.add('active')
 	})
 })
+
+async function showWeather(cityName) {
+	try {
+	const serverUrl = 'http://api.openweathermap.org/data/2.5/weather';
+	const apiKey = 'f660a2fb1e4bad108d6160b7f58c555f';
+	const url = `${serverUrl}?q=${cityName}&appid=${apiKey}&units=metric`;
+	let response = await fetch(url);
+	let json = await response.json();
+	const temperature = Math.round(json.main.temp);
+	temp.textContent = temperature + '°';
+	currentCity.textContent = json.name;
+	const icon = json.weather[0].icon
+	const iconUrl = `https://openweathermap.org/img/wn/${icon}@4x.png`;
+	currentIcon.setAttribute('src', iconUrl);
+	} catch(err) {
+		throw new Error(alert(err));
+	}
+}
+
+searchForm.addEventListener('submit', (event) => {
+	event.preventDefault();
+	const cityName = inputCity.value;
+	showWeather(cityName);
+});
