203ab5ce67ea8626c8b7132d5a94203c45fd5b25
Borise <starcenkoboris2@gmail.com>
2023-05-09 15:53:21 +0300

    Second version,but don	 work change icon

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ INPUT_CITY: document.querySelector('.weather__search-form-input'),
 FORM: document.querySelector('.weather__search-form'),
 ADD__CITY_BTN : document.querySelector('.weather__search-form-btn'),
 WEATHER_BLOCK_TEMP_NOW:document.querySelector('.weather__block-temp'),
-WEATHER_BLOCK_ICON_NOW:document.querySelector('.weather-now-precipitation-img'),
+WEATHER_BLOCK_ICON_NOW:document.querySelector('.weather__block-cloud'),
 WEATHER_BLOCK_CITY_NOW:document.querySelector('.weather__block-content-city')
 }
 
