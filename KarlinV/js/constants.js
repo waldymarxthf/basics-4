9e6b896b7707f0b652420d35245a41d99790d906
Владимир Карлин <vow.carlin@yandex.ru>
2023-05-13 21:04:40 +0300

    init

---
+++ KarlinV/js/constants.js
@@ -0,0 +1,12 @@
+import { storage } from "./saveLocalStorage.js";
+
+export const form = document.querySelector(".form");
+export const cityNameInputValue = document.querySelector(".form__input");
+export const weatherCards = document.querySelector(".weather__cards");
+export const weatherLocationList = document.querySelector(".weather__location-list");
+export const tabs = document.querySelectorAll(".tab");
+
+export const startCity = storage.getCurrentCity("currentCity") || "Moscow";
+storage.setCurrentCity(startCity);
+
+export const arrCityList = storage.getFavoriteCities("favoriteCities") || [];
