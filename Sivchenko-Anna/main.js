781f6b50987bde207eae016837dcbf5a8e149fac
Sivchenko-Anna <anya.malakhova.99@mail.ru>
2023-05-13 13:46:17 +1000

    feat: add solution for practice 40

--- Sivchenko-Anna/main.js
+++ Sivchenko-Anna/main.js
@@ -2,33 +2,43 @@ const firstOperand = document.querySelector(".js-operand-a");
 const secondOperand = document.querySelector(".js-operand-b");
 const operator = document.querySelector(".js-select-operation");
 const btnResult = document.querySelector(".js-btn-result");
-const outPut = document.querySelector(".js-output");
+const output = document.querySelector(".js-output");
+
+let result;
 
 function calculate () {
-  let result;
   switch (operator.value) {
     case "+":
       result = +firstOperand.value + +secondOperand.value;
-      outPut.textContent = result;
+      addResult();
       break;
     case "-":
       result = +firstOperand.value - +secondOperand.value;
-      outPut.textContent = result;
+      addResult();
       break;
     case "*":
       result = +firstOperand.value * +secondOperand.value;
-      outPut.textContent = result;
+      addResult();
       break;
     case "/":
-      if (secondOperand.value === '0') {
-        outPut.textContent = "Делить на 0 нельзя!"
-        break
-      } else {
-        result = +firstOperand.value / +secondOperand.value;
-        outPut.textContent = result;
-        break;
-      }
+      result = +firstOperand.value / +secondOperand.value;
+      addResult();
+      break;
   }
 }
 
+function addResult () {
+  const newOutputDiv = document.createElement("div");
+  if (result === Infinity || result === -Infinity) {
+    newOutputDiv.textContent = "Делить на 0 нельзя!";
+  } else {
+    newOutputDiv.textContent = result;
+  }
+  output.insertAdjacentElement("afterbegin", newOutputDiv);
+}
+
 btnResult.addEventListener('click', calculate);
+
+output.addEventListener('click', (event) => {
+  event.target.remove();
+})
