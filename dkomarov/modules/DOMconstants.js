5de2dc210c8229afac7b215ae7c8628ebd148a99
Dmitry Komarov <dmitrykomarov@MacBook-Pro-Dmitry.local>
2023-05-13 19:23:26 +0300

    Рефакторинг

---
+++ dkomarov/modules/DOMconstants.js
@@ -0,0 +1,10 @@
+const tabs = document.querySelectorAll('.tabs_item');
+const displayItem = document.querySelectorAll('.content-area_display');
+const inputValue = document.querySelector('.header-input');
+const formNode = document.querySelector('.header');
+
+const nameShapeCity = document.querySelector('.love-city');
+const addButtonShape = document.querySelector('.now-btn-shape-img');
+const sectionItem = document.querySelector('.locations');
+
+export {tabs, displayItem, inputValue, formNode, nameShapeCity, addButtonShape, sectionItem}; 
