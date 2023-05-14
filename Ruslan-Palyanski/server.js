26ee7054ecacd51f664ee2a4d8efb6f20b49cb44
Ruslan-Palyanski <ruslapolyanski@yandex.ru>
2023-05-14 18:58:38 +0300

    feat: add some function

---
+++ Ruslan-Palyanski/server.js
@@ -0,0 +1,9 @@
+const serverData = {
+    serverUrl: 'http://api.openweathermap.org/data/2.5/weather',
+    apiKey: 'afc9f2df39f9e9e49eeb1afac7034d35',
+    // 'f660a2fb1e4bad108d6160b7f58c555f'
+    serverImg: 'https://openweathermap.org/img/wn/',
+    serverForecastUrl: 'http://api.openweathermap.org/data/2.5/forecast',
+}
+
+export {serverData};
