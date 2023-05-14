1203992bc1f200bc85372b954ff8e1a4c06bd38a
Ruslan Maroz <ich.nemoroz@gmail.com>
2023-05-15 00:06:16 +0200

    v1
    without modules

--- marozny/script.js
+++ marozny/script.js
@@ -1,4 +1,9 @@
-import { addToFavorite, loadData } from "./render.js";
+import {
+  loadLocations,
+  saveLocationToLocalStorage,
+  saveLastLocationToLocalStorage,
+  loadLastLocation,
+} from "./localstorage.js";
 import { UI_ELEMENTS } from "./ui.js";
 import { round } from "./utils.js";
 
@@ -11,10 +16,12 @@ UI_ELEMENTS.TABS.forEach((tab, index) => {
   });
 });
 
+const storage = [] || loadLocations(storage);
+
 async function getData(cityName) {
   try {
     const serverUrl = "http://api.openweathermap.org/data/2.5/weather";
-    const apiKey = "f660a2fb1e4bad108d6160b7f58c555f";
+    const apiKey = "afc9f2df39f9e9e49eeb1afac7034d35";
     const url = `${serverUrl}?q=${cityName}&appid=${apiKey}&units=metric`;
     let response = await fetch(url);
     if (response.ok) {
@@ -31,6 +38,70 @@ async function getData(cityName) {
   }
 }
 
+function loadData(temperature, name, icon) {
+  UI_ELEMENTS.TEMP.textContent = temperature + "°";
+  UI_ELEMENTS.CURRENT_CITY.textContent = name;
+  const iconUrl = `https://openweathermap.org/img/wn/${icon}@4x.png`;
+  UI_ELEMENTS.CURRENT_ICON.setAttribute("src", iconUrl);
+}
+
+function addToFavorite(name) {
+  storage.push({
+    location: name,
+  });
+  saveLocationToLocalStorage(storage);
+  saveLastLocationToLocalStorage(name);
+  renderLocations();
+}
+
+function renderLocations() {
+  UI_ELEMENTS.LIST_FAV_CITIES.innerHTML = "";
+  storage.forEach((element) => {
+    const newFavCity = createLocationElement(element);
+    UI_ELEMENTS.LIST_FAV_CITIES.append(newFavCity);
+  });
+}
+
+function createLocationElement(element) {
+  const newFavCity = document.createElement("li");
+  newFavCity.classList.add("list-locations__item");
+  newFavCity.textContent = element.location;
+
+  const deleteFavCity = document.createElement("button");
+  deleteFavCity.classList.add("list-locations__item-btn");
+  newFavCity.append(deleteFavCity);
+
+  newFavCity.addEventListener("click", () => {
+    getData(element.location);
+  });
+  deleteFavCity.addEventListener("click", () => {
+    deleteLocation(newFavCity);
+  });
+  return newFavCity;
+}
+
+function deleteLocation(name) {
+  const index = findLocationIndex(storage, name);
+  storage.splice(index, 1);
+  saveLocationToLocalStorage(storage);
+  renderLocations();
+}
+
+function findLocationIndex(storage, name) {
+  return storage.findIndex((el) => el.location === name.textContent);
+}
+
+addEventListener("DOMContentLoaded", async () => {
+  let savedLocation = loadLastLocation();
+
+  if (!savedLocation) {
+    savedLocation = "Poznań";
+    saveLastLocationToLocalStorage(savedLocation);
+  }
+
+  await getData(savedLocation);
+});
+
 UI_ELEMENTS.SEARCH_FORM.addEventListener("submit", (event) => {
   event.preventDefault();
   const cityName = UI_ELEMENTS.INPUT_CITY.value;
@@ -43,4 +114,5 @@ UI_ELEMENTS.FAVORITE_BUTTON.addEventListener("click", () => {
   addToFavorite(name);
 });
 
-export { getData };
+loadLocations(storage);
+renderLocations();
