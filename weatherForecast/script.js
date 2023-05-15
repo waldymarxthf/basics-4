d5b8b0469014a0930f70f7fee29310c3bf97cc8f
anatolii-samurai <goddevil22@yandex.ru>
2023-05-15 05:20:54 +0400

    add and delete

--- weatherForecast/script.js
+++ weatherForecast/script.js
@@ -32,28 +32,50 @@ window.addEventListener('DOMContentLoaded',() => {
         const form = document.querySelector('.searching'),
         inputValue = document.querySelector('#search'),
         btnSend = document.querySelector('.btn-send');
-        const cityName = document.querySelector('.city-name'),
-        gradeCel=document.querySelector('.grade')
+        const cityNames = document.querySelector('.city-name'),
+        gradeCel=document.querySelector('.grade');
+        const heartImg = document.querySelector(".img-heart");
+        let cityList = document.querySelector('.city-list');
+        
     async function f(url){
        try {let response = await fetch(url);
         let json = await response.json();
         gradeCel.textContent = `${json.main.temp}`
-        cityName.textContent = `${json.name}`;
-        
+        cityNames.textContent = `${json.name}`;
+        heartImg.addEventListener("click",(e)=>{
+            e.preventDefault();
+            const li = document.createElement('li');
+            li.classList.add('list-item');
+            cityList.appendChild(li);
+            li.textContent = `${json.name}`;
+            const closeList = new Image();
+            closeList.src='./img/icon-close.png';
+            closeList.classList.add('close-list');
+            li.append(closeList);
+            closeList.addEventListener('click',(e)=>{
+                if(e.target.classList.contains('list-item')){
+                    e.stopPropagation();
+                    e.target.remove();
+                }
+            })
+           
+        })  
        } catch (error) {
         console.log(error);
        } 
         }
     form.addEventListener('submit',(event)=>{
         event.preventDefault();
-    
+        inputValue.value.innerHTML='';
         const serverUrl = 'http://api.openweathermap.org/data/2.5/weather';
-const cityName = inputValue.value;
-const apiKey = 'f660a2fb1e4bad108d6160b7f58c555f'; 
-const url = `${serverUrl}?q=${cityName}&appid=${apiKey}&units=metric`;
-    f(url) 
-    })    
-
+        const cityName = inputValue.value;
+        const apiKey = 'f660a2fb1e4bad108d6160b7f58c555f'; 
+        const url = `${serverUrl}?q=${cityName}&appid=${apiKey}&units=metric`;
+        
+            f(url) 
+            })    
+    
+   
 
 
 
