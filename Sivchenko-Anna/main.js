0b65f504bc46f44029c299a84c8a0fb9108a9955
Sivchenko-Anna <anya.malakhova.99@mail.ru>
2023-05-15 20:24:45 +1000

    feat: add functionality to add a task

--- Sivchenko-Anna/main.js
+++ Sivchenko-Anna/main.js
@@ -1,7 +1,23 @@
-const btnAdd = document.querySelector(".btn-add");
-const toDoTask = document.querySelector(".todo-task");
-const inputValue = document.querySelector(".todo-input");
+const form = document.querySelector(".priority-container__form");
+const list = document.querySelector(".priority-container");
+const formInput = document.querySelector(".priority-container__form-input");
 
-function addTask() {
-  
+function clearInput() {
+  formInput.value = "";
 }
+
+function addTask(event) {
+  event.preventDefault();
+  const newDiv = document.createElement("div");
+  list.appendChild(newDiv);
+  newDiv.classList.add("priority-container__task");
+  newDiv.insertAdjacentHTML(
+    "afterbegin",
+    `<input type="checkbox" class="priority-container__checkbox"><p class=" priority-container__task-text">${formInput.value}</p><button class="priority-container__form-button" type="submit">
+          <img src="./assets/close-icon.svg" alt="close-icon" class=" priority-container__delete">
+        </button></div>`
+  );
+  clearInput();
+}
+
+form.addEventListener("submit", addTask);
