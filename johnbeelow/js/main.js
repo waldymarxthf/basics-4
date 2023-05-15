5c294af0ca169645e7b9df787214fc204e52cabd
John <98343581+johnbeelow@users.noreply.github.com>
2023-05-15 12:02:31 +0300

    Update main.js

--- johnbeelow/js/main.js
+++ johnbeelow/js/main.js
@@ -2,7 +2,7 @@ import { UI_ELEMENTS, clearInput, animateIcon } from './module/ui_elements.js'
 
 import {
   handleContentLoaded,
-  processingInputRequest,
+  handleSendingData,
   getToggleLikeAction,
   changeActiveButton,
 } from './module/business-logic.js'
@@ -13,21 +13,13 @@ document.addEventListener('DOMContentLoaded', () => {
 
 document.addEventListener('DOMContentLoaded', handleContentLoaded)
 
-window.addEventListener('online', () => {
-  UI_ELEMENTS.LOADER.style.display = 'none'
-})
-
-window.addEventListener('offline', () => {
-  UI_ELEMENTS.LOADER.style.display = 'flex'
-})
-
 for (let button of UI_ELEMENTS.BUTTONS_ALL) {
   button.addEventListener('click', changeActiveButton)
 }
 
 UI_ELEMENTS.INPUT_FORM.addEventListener('submit', (event) => {
   event.preventDefault()
-  processingInputRequest(UI_ELEMENTS.INPUT_TEXT.value)
+  handleSendingData(UI_ELEMENTS.INPUT_TEXT.value)
   animateIcon()
   clearInput()
 })
@@ -35,4 +27,12 @@ UI_ELEMENTS.INPUT_FORM.addEventListener('submit', (event) => {
 UI_ELEMENTS.LIKE.addEventListener('click', (event) => {
   event.preventDefault()
   getToggleLikeAction()
+})
+
+window.addEventListener('online', () => {
+  UI_ELEMENTS.LOADER.style.display = 'none'
+})
+
+window.addEventListener('offline', () => {
+  UI_ELEMENTS.LOADER.style.display = 'flex'
 })
