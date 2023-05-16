b886cdb7b2751b2165dd2948e5cedd8d2bdf6aa2
Sivchenko-Anna <anya.malakhova.99@mail.ru>
2023-05-16 19:06:14 +1000

    feat: add js

--- Sivchenko-Anna/main.js
+++ Sivchenko-Anna/main.js
@@ -1,23 +1,165 @@
-const form = document.querySelector(".priority-container__form");
-const list = document.querySelector(".priority-container");
-const formInput = document.querySelector(".priority-container__form-input");
+const STATUS = {
+  TODO: "To Do",
+  DONE: "Done",
+};
 
-function clearInput() {
+const PRIORITY = {
+  HIGH: "High",
+  LOW: "Low",
+};
+
+const formHigh = document.querySelector(".high-priority-container__form");
+const formLow = document.querySelector(".low-priority-container__form");
+const formHighInput = document.querySelector(".high-priority-container__form-input");
+const formLowInput = document.querySelector(".low-priority-container__form-input");
+const listHigh = document.querySelector(".priority-container-high");
+const listLow = document.querySelector(".priority-container-low");
+const toDoList = [];
+
+//* функция проверки на пустую строку
+
+function isInputEmpty(task) {
+  return !task.trim();
+}
+
+//* функция проверки на наличие задачи в списке
+
+function isTaskExist(task) {
+  if (toDoList.find((item) => item.name === task)) {
+    console.log(`Задача '${task}' уже есть в списке`);
+    return false;
+  }
+  return true;
+}
+
+//* функция нахождения индекса задачи в массиве
+
+function findIndexOfTask(task) {
+  return toDoList.findIndex((item) => item.name === task);
+}
+
+//* функция добавления задачи в массив
+
+function addTask(task, status = STATUS.TODO, priority = PRIORITY.HIGH) {
+  if (isInputEmpty(task)) {
+    console.log("Введите задачу");
+    return
+  }
+
+  if (!isTaskExist(task)) {
+    return;
+  }
+
+  toDoList.push({
+    name: task,
+    status: status,
+    priority: priority,
+  })
+}
+
+//* функция удаления задачи из массива
+
+function deleteTask(task) {
+  const deleteTaskIndex = findIndexOfTask(task);
+  toDoList.splice(deleteTaskIndex, 1);
+  console.log(`Задачи '${task}' успешно удалена`);
+  render();
+}
+
+//* функция изменения статуса задачи в массиве
+
+function changeStatus(task, status) {
+  const taskIndex = findIndexOfTask(task);
+  toDoList[taskIndex].status = status;
+  render();
+  return;
+}
+
+//* функция изменения чекбокса
+
+function checkboxActions(checkbox, task) {
+  const isChecked = checkbox.checked;
+  changeStatus(task, isChecked ? STATUS.DONE : STATUS.TODO);
+  render();
+}
+
+//* функция создания блока задачи
+
+function createTaskList(text, status) {
+  const newTask = document.createElement("div");
+  newTask.classList.add("priority-container__task");
+
+  const checkbox = document.createElement("input");
+  checkbox.type = "checkbox";
+  checkbox.classList.add("priority-container__checkbox");
+
+  const taskText = document.createElement("p");
+  taskText.classList.add("priority-container__task-text");
+  taskText.innerText = text;
+
+  const btnClose = document.createElement("button");
+  btnClose.classList.add("priority-container__form-button");
+
+  const btnCloseImg = document.createElement("img");
+  btnCloseImg.src = "./assets/close-icon.svg";
+  btnCloseImg.classList.add("priority-container__delete");
+
+  btnClose.appendChild(btnCloseImg);
+
+  /* Cлушатель на кнопку закрытия задания и на чекбокс */
+  btnClose.addEventListener("click", deleteTask.bind(null, text));
+  checkbox.addEventListener(
+    "change",
+    checkboxActions.bind(null, checkbox, text)
+  );
+
+  if (status === STATUS.DONE) {
+    checkbox.checked = true;
+  }
+
+  /* Собираем элемент задачи */
+  newTask.appendChild(checkbox);
+  newTask.appendChild(taskText);
+  newTask.appendChild(btnClose);
+
+  return newTask;
+}
+
+//* Функция рендера задач 
+
+function render() {
+  listHigh.innerHTML = '';
+  listLow.innerHTML = '';
+
+  for (const elem of toDoList) {
+    const nodeForAddTask = elem.priority == PRIORITY.HIGH ? listHigh : listLow;
+    const task = createTaskList(elem.name, elem.status);
+
+    nodeForAddTask.appendChild(task);
+  }
+}
+
+//* Функция добавления задачи при отправке формы
+
+function addHighTask(event) {
+  event.preventDefault();
+  addTask(formHighInput.value, STATUS.TODO, PRIORITY.HIGH);
+  clearInput(formHighInput);
+  render();
+}
+
+function addLowTask(event) {
+  event.preventDefault();
+  addTask(formLowInput.value, STATUS.TODO, PRIORITY.LOW);
+  clearInput(formLowInput);
+  render();
+}
+
+//* Функция очистки поля ввода задачи
+
+function clearInput(formInput) {
   formInput.value = "";
 }
 
-function addTask(event) {
-  event.preventDefault();
-  const newDiv = document.createElement("div");
-  list.appendChild(newDiv);
-  newDiv.classList.add("priority-container__task");
-  newDiv.insertAdjacentHTML(
-    "afterbegin",
-    `<input type="checkbox" class="priority-container__checkbox"><p class=" priority-container__task-text">${formInput.value}</p><button class="priority-container__form-button" type="submit">
-          <img src="./assets/close-icon.svg" alt="close-icon" class=" priority-container__delete">
-        </button></div>`
-  );
-  clearInput();
-}
-
-form.addEventListener("submit", addTask);
+formHigh.addEventListener("submit", addHighTask);
+formLow.addEventListener("submit", addLowTask);
