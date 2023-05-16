00586aae47843f119d15d0bf0153b376d4e7ce29
Nikolay R <ras.nikolay@mail.ru>
2023-05-16 14:19:32 +0300

    40 task

--- main.js
+++ main.js
@@ -46,28 +46,14 @@ function calc(event) {
     let result = getOperator(operationDOM, firstNumberDOM, secondNumberDOM);
     console.log(result);
     document.body.querySelector(".result").textContent = result;
-}
 
-
-function changeColor() {
-
-    let changeColorTime = setTimeout(function ColorTime() {
-
-        const colors = ['red', 'blue', 'green', 'orange', 'white'];
-
-        const randomColor = colors[Math.floor(Math.random() * colors.length)];
-
-        const colorBtn = document.body.querySelector(".colorBtn");
-        const valueBtn = colorBtn.getAttribute('data-color');
-
-        const colorBody = document.body;
-        console.log(valueBtn);
-        colorBody.style.backgroundColor = valueBtn;
-
-        setTimeout(() => colorBody.style.backgroundColor = randomColor,2000);
-
-        changeColorTime = setTimeout(ColorTime, 4000);
-    },2000);
+    const conclusion = document.querySelector(".conclusion");
+    const newConclusion = document.createElement('div');
+    newConclusion.textContent = result;
+    conclusion.appendChild(newConclusion);
+    newConclusion.addEventListener('click', () => {
+        conclusion.removeChild(newConclusion);
+    })
 }
 
 
