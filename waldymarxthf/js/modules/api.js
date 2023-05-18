a5f6110ef977a16c508d347b37ab338182bad196
waldymarxthf <iliya.nov223@gmail.com>
2023-05-18 16:53:18 +0300

    [fix] fix: api token

--- waldymarxthf/js/modules/api.js
+++ waldymarxthf/js/modules/api.js
@@ -3,7 +3,7 @@ import { hideLoader, showLoader } from "./preloader.js";
 import { errorHandler } from "./utils.js";
 
 const serverUrl = "http://api.openweathermap.org/data/2.5";
-const apiKey = "afc9f2df39f9e9e49eeb1afac7034d3";
+const apiKey = "afc9f2df39f9e9e49eeb1afac7034d35";
 
 export async function getData(endpoint, location) {
 	showLoader()
