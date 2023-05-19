6da3d068152348aa18a79fdbe922db6fe6f24077
Ruslan Maroz <ich.nemoroz@gmail.com>
2023-05-19 18:39:44 +0200

    fix
    added description to weather icons, refactored data from getData

--- marozny/api.js
+++ marozny/api.js
@@ -1,25 +1,18 @@
 import { loadDataDetails, loadDataNow } from "./ui.js";
-import { round, timeConverter } from "./utils.js";
+
+const serverUrl = "http://api.openweathermap.org/data/2.5/weather";
+const apiKey = "afc9f2df39f9e9e49eeb1afac7034d35";
 
 async function getData(cityName) {
   try {
-    const serverUrl = "http://api.openweathermap.org/data/2.5/weather";
-    const apiKey = "afc9f2df39f9e9e49eeb1afac7034d35";
     const url = `${serverUrl}?q=${cityName}&appid=${apiKey}&units=metric`;
-    let response = await fetch(url);
+    const response = await fetch(url);
+
     if (response.ok) {
-      let json = await response.json();
-      const temperature = round(json.main.temp);
-      const icon = json.weather[0].icon;
-      const name = json.name;
-      const feels = json.main.feels_like;
-      const status = json.weather[0].main;
-      const timezone = json.timezone;
-      const sunrise = timeConverter(json.sys.sunrise, timezone);
-      const sunset = timeConverter(json.sys.sunset, timezone);
-      loadDataNow(temperature, name, icon);
-      loadDataDetails(temperature, name, feels, status, sunrise, sunset);
-      return response.json;
+      const data = await response.json();
+      loadDataNow(data);
+      loadDataDetails(data);
+      return data;
     } else {
       throw new Error((await response.json()).message);
     }
--- marozny/ui.js
+++ marozny/ui.js
@@ -1,7 +1,7 @@
 import { getData } from "./api.js";
 import { loadLocations, saveLocationToLocalStorage } from "./localstorage.js";
 import { UI_ELEMENTS } from "./ui-elements.js";
-import { findLocationIndex } from "./utils.js";
+import { findLocationIndex, round, timeConverter } from "./utils.js";
 
 const storage = [] || loadLocations(storage);
 
@@ -14,20 +14,41 @@ UI_ELEMENTS.NOW.TABS.forEach((tab, index) => {
   });
 });
 
-function loadDataDetails(temperature, name, feels, status, sunrise, sunset) {
-  UI_ELEMENTS.DETAILS.TEMPERATURE.textContent = temperature;
-  UI_ELEMENTS.DETAILS.CITY.textContent = name;
-  UI_ELEMENTS.DETAILS.FEELS_LIKE.textContent = feels;
-  UI_ELEMENTS.DETAILS.SKY.textContent = status;
-  UI_ELEMENTS.DETAILS.SUNRISE.textContent = sunrise;
-  UI_ELEMENTS.DETAILS.SUNSET.textContent = sunset;
-}
+function loadDataNow(data) {
+  const {
+    main: { temp },
+    name,
+    weather: [{ icon }],
+    weather: [{ description }],
+  } = data;
+  const temperature = round(temp);
+  const iconUrl = `https://openweathermap.org/img/wn/${icon}@4x.png`;
 
-function loadDataNow(temperature, name, icon) {
   UI_ELEMENTS.NOW.TEMP.textContent = temperature + "°";
   UI_ELEMENTS.NOW.CURRENT_CITY.textContent = name;
-  const iconUrl = `https://openweathermap.org/img/wn/${icon}@4x.png`;
   UI_ELEMENTS.NOW.CURRENT_ICON.setAttribute("src", iconUrl);
+  UI_ELEMENTS.NOW.CURRENT_ICON.setAttribute("title", description);
+  UI_ELEMENTS.NOW.CURRENT_ICON.setAttribute("alt", description);
+}
+
+function loadDataDetails(data) {
+  const {
+    main: { temp },
+    main: { feels_like },
+    name,
+    weather: [{ main }],
+    timezone,
+    sys: { sunrise, sunset },
+  } = data;
+  const sunriseTime = timeConverter(sunrise, timezone);
+  const sunsetTime = timeConverter(sunset, timezone);
+
+  UI_ELEMENTS.DETAILS.TEMPERATURE.textContent = temp;
+  UI_ELEMENTS.DETAILS.CITY.textContent = name;
+  UI_ELEMENTS.DETAILS.FEELS_LIKE.textContent = feels_like;
+  UI_ELEMENTS.DETAILS.SKY.textContent = main;
+  UI_ELEMENTS.DETAILS.SUNRISE.textContent = sunriseTime;
+  UI_ELEMENTS.DETAILS.SUNSET.textContent = sunsetTime;
 }
 
 function renderLocations() {
