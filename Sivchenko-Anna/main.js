f63d54f6fa1f6d1fb09b81944bd46dc10c09230a
Sivchenko-Anna <anya.malakhova.99@mail.ru>
2023-05-21 20:05:28 +1000

    feat: add functions for add and remove favourite city

--- Sivchenko-Anna/main.js
+++ Sivchenko-Anna/main.js
@@ -4,39 +4,131 @@ const API = {
 };
 
 const form = document.querySelector(".search");
-const cityName = document.querySelector(".search-input");
+const inputName = document.querySelector(".search-input");
 
-const outCityName = document.querySelector(".city");
+const cityName = document.querySelector(".city");
 const temperature = document.querySelector(".temperature");
-const weatherIcon = document.querySelector(".weather-icon");;
+const weatherIcon = document.querySelector(".weather-icon");
 
+const btnFavourite = document.querySelector(".city-like");
+const savedCities = document.querySelector(".cities");
+const cities = [];
+
+//* функция добавения города в массив
+
+function addFavouriteCity () {
+  const favouriteCity = cityName.textContent;
+
+  if (isInputEmpty(favouriteCity)) {
+    alert("Введите название города");
+    return;
+  }
+
+  if(isCityExist(favouriteCity)) {
+    alert("Выбранный город уже добавен в избранное");
+    return
+  }
+
+  cities.push({location: favouriteCity});
+  render();
+}
+
+//* функция удаления города из массива
+
+function removeFavouriteCity(city) {
+  const index = findIndex(city);
+  cities.splice(index, 1);
+  render()
+}
+
+//* функция поиска индекса в массиве
+
+function findIndex(name) {
+  return cities.findIndex((item) => item.location === name)
+}
+
+//* функция создания элемента списка избранных городов
+
+function cleateElement(city) {
+  const favouriteCity = document.createElement("li");
+  favouriteCity.className = "city-item";
+  favouriteCity.textContent = city.location;
+
+  const btnCloseCity = document.createElement("button");
+  btnCloseCity.className = "close-city";
+
+  btnCloseCity.addEventListener("click", () => {
+    removeFavouriteCity(city.location);
+  } );
+
+  favouriteCity.append(btnCloseCity);
+
+  return favouriteCity;
+}
+
+//* функция рендера блока с избранными городами
+
+function render() {
+  savedCities.innerHTML = '';
+  cities.forEach((item) => {
+    const newFavouriteCity = cleateElement(item);
+    savedCities.append(newFavouriteCity);
+  })
+}
+
+//* функция проверки на пустую строку
+
+function isInputEmpty(name) {
+  return !name.trim();
+}
+
+//* функция проверки наличия города в массиве
+
+function isCityExist(name) {
+  return cities.find((item) => item.location === name);
+}
+
+// * функция получения данных погоды с API
 
 async function getWeather() {
   try {
-    const city = cityName.value;
+    const city = inputName.value;
     const url = `${API.SERVER_URL}?q=${city}&appid=${API.API_KEY}&units=metric`;
 
     let response = await fetch(url);
     if (response.ok) {
       let data = await response.json();
-      setWeather(data);
+      setWeatherNow(data);
+    } else if (isInputEmpty(city)) {
+      throw new Error("Название города не введено");
     } else {
-      throw new Error("Введите верное название города");
+      throw new Error((await response.json()).message);
     }
   } catch (error) {
     alert(error.message);
   }
 }
 
-function setWeather(data) {
+// * функция установки данных погоды в раздел Now
+
+function setWeatherNow(data) {
   temperature.textContent = `${Math.trunc(data.main.temp)}°`;
   const icon = data.weather[0].icon;
   const iconSrc = `https://openweathermap.org/img/wn/${icon}@2x.png`;
   weatherIcon.src = iconSrc;
-  outCityName.textContent = data.name;
+  cityName.textContent = data.name;
+}
+
+// * функция очистки поля ввода города
+
+function cleanInput() {
+  form.reset();
 }
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
   getWeather();
+  cleanInput();
 });
+
+btnFavourite.addEventListener("click", addFavouriteCity);
