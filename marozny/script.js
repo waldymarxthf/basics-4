749da43103114f15f9dafe7350171a467ddc810b
Ruslan Maroz <ich.nemoroz@gmail.com>
2023-05-23 21:11:49 +0200

    refactor
    chenged massive fav cities to set

---
+++ marozny/script.js
@@ -0,0 +1,27 @@
+import { loadLocations, saveLocationToLocalStorage } from "./localstorage.js";
+import { UI_ELEMENTS } from "./ui-elements.js";
+import { addToFavorite, renderLocations, updateBlock } from "./ui.js";
+
+window.addEventListener("DOMContentLoaded", async () => {
+  let savedLocation = loadLocations("LastLocation");
+
+  if (!savedLocation) {
+    savedLocation = "Poznań";
+    saveLocationToLocalStorage("LastLocation", savedLocation);
+  }
+
+  await updateBlock(savedLocation);
+});
+
+UI_ELEMENTS.NOW.SEARCH_FORM.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const cityName = UI_ELEMENTS.NOW.INPUT_CITY.value;
+  updateBlock(cityName);
+  event.target.reset();
+});
+
+UI_ELEMENTS.NOW.FAVORITE_BUTTON.addEventListener("click", () => {
+  let name = UI_ELEMENTS.NOW.CURRENT_CITY.textContent;
+  addToFavorite(name);
+  renderLocations();
+});
---
+++ marozny/ui.js
@@ -0,0 +1,102 @@
+import { getData } from "./api.js";
+import { loadLocations, saveLocationToLocalStorage } from "./localstorage.js";
+import { UI_ELEMENTS } from "./ui-elements.js";
+import { round, timeConverter } from "./utils.js";
+
+const storage = new Set(loadLocations("NewFavCity") || []);
+
+UI_ELEMENTS.NOW.TABS.forEach((tab, index) => {
+  tab.addEventListener("click", () => {
+    UI_ELEMENTS.NOW.TABS.forEach((t) => t.classList.remove("active"));
+    UI_ELEMENTS.NOW.WEATHER_BLOCK.forEach((w) => w.classList.remove("active"));
+    tab.classList.add("active");
+    UI_ELEMENTS.NOW.WEATHER_BLOCK[index].classList.add("active");
+  });
+});
+
+function addToFavorite(name) {
+  storage.add(name);
+  saveLocationToLocalStorage("NewFavCity", [...storage]);
+  renderLocations();
+}
+
+export async function updateBlock(cityName) {
+  const city = await getData(cityName);
+  loadDataNow(city);
+  loadDataDetails(city);
+  saveLocationToLocalStorage("LastLocation", cityName);
+}
+
+function loadDataNow(data) {
+  const {
+    main: { temp },
+    name,
+    weather: [{ icon }],
+    weather: [{ description }],
+  } = data;
+  const temperature = round(temp);
+  const iconUrl = `https://openweathermap.org/img/wn/${icon}@4x.png`;
+
+  UI_ELEMENTS.NOW.TEMP.textContent = temperature + "°";
+  UI_ELEMENTS.NOW.CURRENT_CITY.textContent = name;
+  UI_ELEMENTS.NOW.CURRENT_ICON.setAttribute("src", iconUrl);
+  UI_ELEMENTS.NOW.CURRENT_ICON.setAttribute("title", description);
+  UI_ELEMENTS.NOW.CURRENT_ICON.setAttribute("alt", description);
+}
+
+function loadDataDetails(data) {
+  const {
+    main: { temp },
+    main: { feels_like },
+    name,
+    weather: [{ main }],
+    timezone,
+    sys: { sunrise, sunset },
+  } = data;
+  const sunriseTime = timeConverter(sunrise, timezone);
+  const sunsetTime = timeConverter(sunset, timezone);
+
+  UI_ELEMENTS.DETAILS.TEMPERATURE.textContent = temp;
+  UI_ELEMENTS.DETAILS.CITY.textContent = name;
+  UI_ELEMENTS.DETAILS.FEELS_LIKE.textContent = feels_like;
+  UI_ELEMENTS.DETAILS.SKY.textContent = main;
+  UI_ELEMENTS.DETAILS.SUNRISE.textContent = sunriseTime;
+  UI_ELEMENTS.DETAILS.SUNSET.textContent = sunsetTime;
+}
+
+function renderLocations() {
+  UI_ELEMENTS.NOW.LIST_FAV_CITIES.innerHTML = "";
+  for (const element of storage) {
+    const newFavCity = createLocationElement(element);
+    UI_ELEMENTS.NOW.LIST_FAV_CITIES.append(newFavCity);
+  }
+}
+
+function createLocationElement(element) {
+  const newFavCity = document.createElement("li");
+  newFavCity.classList.add("list-locations__item");
+  newFavCity.textContent = element;
+
+  const deleteFavCity = document.createElement("button");
+  deleteFavCity.classList.add("list-locations__item-btn");
+  newFavCity.append(deleteFavCity);
+
+  newFavCity.addEventListener("click", () => {
+    updateBlock(element);
+  });
+  deleteFavCity.addEventListener("click", (event) => {
+    event.stopPropagation();
+    deleteLocation(element);
+  });
+  return newFavCity;
+}
+
+function deleteLocation(name) {
+  storage.delete(name);
+  saveLocationToLocalStorage("NewFavCity", [...storage]);
+  renderLocations();
+}
+
+loadLocations("NewFavCity");
+renderLocations();
+export { loadDataDetails, loadDataNow, renderLocations, createLocationElement, deleteLocation, storage, addToFavorite };
