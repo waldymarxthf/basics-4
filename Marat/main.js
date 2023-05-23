15ae1ea87f41edf3082980b78bec8082f716104c
Marat <maratmirzabalaev@gmail.com>
2023-05-24 00:41:18 +0300

    Cooment

--- Marat/main.js
+++ Marat/main.js
@@ -80,15 +80,8 @@ async function fetchServer() {
 
 
 }
-function createElement2() {
-    // const url = `${serverUrl}?q=${city.value}&appid=${apiKey}`;
-    // const response = await fetch(url);
-    // const data = await response.json();
-    // console.log(data);
-    fetchServer();
-    const data = fetchServer();
-    console.log(data);
-    console.log(data.main);
+async function calculations(){
+    const data = await fetchServer();
     const gender = Math.floor(data.main.temp - 273);
     const feels_Like = Math.floor(data.main.feels_like - 273);
     const Weather = data.weather[0].main;
@@ -100,20 +93,26 @@ function createElement2() {
     const sunset = data.sys.sunset;
     const datess = new Date(sunset * 1000);
     const dateess = datess.getHours() + ":" + datess.getMinutes();
+    let array = [gender, feels_Like, Weather, dates, dateess];
+    return array;
+}
+async function createElement2(city) {
+    let array =  await calculations();
+    console.log(array);
 
     Aktobe.textContent = city.value;
     left_content_temperature2_Aktobe.innerHTML = city.value;
-    text_temperature.innerHTML = gender + "&#176";
-    Details_Temperature.innerHTML = "Temperature: " + gender + "&#176";
-    Details_Feels.innerHTML = "Feels like: " + feels_Like + "&#176";
-    Details_Weather.innerHTML = "Weather: " + Weather;
-    Details_Sunrise.innerHTML = "Sunrise: " + dates;
-    Details_Sunset.innerHTML = "Sunset: " + dateess;
+    text_temperature.innerHTML = array[0] + "&#176";
+    Details_Temperature.innerHTML = "Temperature: " + array[0] + "&#176";
+    Details_Feels.innerHTML = "Feels like: " + array[1] + "&#176";
+    Details_Weather.innerHTML = "Weather: " + array[2];
+    Details_Sunrise.innerHTML = "Sunrise: " + array[3];
+    Details_Sunset.innerHTML = "Sunset: " + array[4];
 }
 async function addLocation(e) {
     e.preventDefault();
     console.log(city.value);
-    createElement2();
+    createElement2(city);
 
 
 }
@@ -160,7 +159,7 @@ function createElement(cityName) {
     ul.appendChild(li);
     right_bottom_content.appendChild(ul);
 
-    location.addEventListener("click", Likes);
+    location.addEventListener("click",  Likes);
 
     console.log(location.textContent)
     // serdce.addEventListener("click", addedLocation(location.textContent));
@@ -187,32 +186,16 @@ function render() {
 render();
 async function Likes(event) {
     const p = event.target;
-
-    const url = `${serverUrl}?q=${p.textContent}&appid=${apiKey}`;
-    const response = await fetch(url);
-    const data = await response.json();
-
-    const gender = Math.floor(data.main.temp - 273);
-    const feels_Like = Math.floor(data.main.feels_like - 273);
-    const Weather = data.weather[0].main;
-
-    const sunrise = data.sys.sunrise;
-    const datesr = new Date(sunrise * 1000);
-    const dates = datesr.getHours() + ":" + datesr.getMinutes();
-
-    const sunset = data.sys.sunset;
-    const datess = new Date(sunset * 1000);
-    const dateess = datess.getHours() + ":" + datess.getMinutes();
-
+    let array = await calculations();
+    console.log(array);
     Aktobe.textContent = p.textContent;
     left_content_temperature2_Aktobe.innerHTML = p.textContent;
-    text_temperature.innerHTML = gender + "&#176";
-    text_temperature.innerHTML = gender + "&#176";
-    Details_Temperature.innerHTML = "Temperature: " + gender + "&#176";
-    Details_Feels.innerHTML = "Feels like: " + feels_Like + "&#176";
-    Details_Weather.innerHTML = "Weather: " + Weather;
-    Details_Sunrise.innerHTML = "Sunrise: " + dates;
-    Details_Sunset.innerHTML = "Sunset: " + dateess;
+    text_temperature.innerHTML = array[0] + "&#176";
+    Details_Temperature.innerHTML = "Temperature: " + array[0] + "&#176";
+    Details_Feels.innerHTML = "Feels like: " + array[1] + "&#176";
+    Details_Weather.innerHTML = "Weather: " + array[2];
+    Details_Sunrise.innerHTML = "Sunrise: " + array[3];
+    Details_Sunset.innerHTML = "Sunset: " + array[4];
 }
 
 serdce.addEventListener("click", () => {
