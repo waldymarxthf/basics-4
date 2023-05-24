7c4fd73848bc39dc8c185bd26560eadcfb900bb9
DariaYukseeva <ducha2005@mail.ru>
2023-05-24 19:48:53 +0500

    changed favCities to new Set()

---
+++ DariaYukseeva/js/utils.js
@@ -0,0 +1,25 @@
+
+export const getDOMElement = (selector) => document.querySelector(selector);
+
+export function timeConverter(UNIX_timestamp, timeZone) {
+    const date = new Date((UNIX_timestamp + timeZone) * 1000);
+    const localDate = date.toLocaleString([], { hour: '2-digit', minute: '2-digit', timeZone: 'UTC' });
+    const time = localDate;
+    return time;
+}
+
+export function dateConverter(UNIX_timestamp, timeZone) {
+    const date = new Date((UNIX_timestamp + timeZone) * 1000);
+    const localDate = date.toLocaleString("en-GB", {month: 'long', day: 'numeric', timeZone: 'UTC' });
+    
+    return localDate;
+}
+
+export function saveToLocalStorage(key, object) {
+    localStorage.setItem(key, JSON.stringify(object));
+}
+
+export function loadFromLocalStorage(key) {
+    return JSON.parse(localStorage.getItem(key));
+   
+}
