94610334e475d777284603aa49c43410f9510293
Borise <starcenkoboris2@gmail.com>
2023-05-25 15:52:15 +0300

    Finnaly add constructor metod

--- brbsiemens-practise41/main.js
+++ brbsiemens-practise41/main.js
@@ -1,47 +1,7 @@
+import {PRIOPITIES,STATUS, UI_ELEMNTS } from './ui_elements.js';
+import{clearInputHigh,clearInputLow} from './utils.js'
 
-const UI_ELEMNTS = {
-  FROM_HIGH: document.querySelector('.addHighForm'),
-  INPUT_HIGH: document.querySelector('.addHighInput'),
-  FORM_BTN: document.querySelector('.addHighButton'),
-  LIST_TASKS_HIGH: document.querySelector('.list-taskHigh'),
-
-  FROM_LOW: document.querySelector('.addLowForm'),
-  INPUT_LOW: document.querySelector('.addLowInput'),
-  FORM_BTN: document.querySelector('.addLowButton'),
-  LIST_TASKS_LOW: document.querySelector('.list-taskLow'),
-
-  DEL_BTN: document.querySelectorAll('closeButton')
- 
-}
-
-const PRIOPITIES = {
-	HIGH: 'High',
-	LOW: 'Low'
-}
-const STATUS = {
-	TODO: 'To do',
-	DONE: 'Done'
-}
-
-
-const toDoList = [ 
-  {} ,
-
-];
-/*function User(name) {
-  this.name = name;
-  this.isAdmin = false;
-}
-
-let user = new User("Jack");
-
-*/
-
-
-/*task = { name: 'Task Name', status: ' TODO', priority: 'LOW'}
-сделать так
-task = new Task('Task Name', ' TODO', 'LOW')
-*/
+const toDoList = [];
  
 function createObject(name,priority,status){
   this.name=name;
@@ -67,14 +27,7 @@ function delTask(nameDel){
     console.clear()
   }
 
-function clearInputHigh(){
-  UI_ELEMNTS.INPUT_HIGH.value=("")
-}
 
-function clearInputLow(){
-  UI_ELEMNTS.INPUT_LOW.value=("")
-}
-//////////////////////////////////////////////////////////////////////////////////////
 
 UI_ELEMNTS.FROM_HIGH.addEventListener('submit', (event) => {
   event.preventDefault();
@@ -86,8 +39,9 @@ UI_ELEMNTS.FROM_HIGH.addEventListener('submit', (event) => {
 
 UI_ELEMNTS.FROM_LOW.addEventListener('submit', (event) => {
   event.preventDefault();
-  addTask(UI_ELEMNTS.INPUT_LOW.value,PRIOPITIES.LOW,STATUS.TODO);
-  functionsUItaskLow(UI_ELEMNTS.LIST_TASKS_LOW);
+  let task = new createObject(UI_ELEMNTS.INPUT_LOW.value ,PRIOPITIES.LOW,STATUS.TODO)
+  addTask(task);
+  renderLow(UI_ELEMNTS.LIST_TASKS_LOW);
  
 })
 
@@ -97,7 +51,7 @@ function renderHigh(listTasks) {
   
 const taskHigh = toDoList.filter(task => task.priority===PRIOPITIES.HIGH);
 taskHigh.forEach(user=>{
-  const task = document.createObject("div")
+  const task = document.createElement("div")
   task.classList.add('task');
   task.innerHTML=`
     <input type="checkbox" class="checkBox">
@@ -129,12 +83,12 @@ checkBox.addEventListener('change',()=>{
 clearInputHigh()
 }
 
-function functionsUItaskLow(listTasks){
+function renderLow(listTasks){
 UI_ELEMNTS.LIST_TASKS_LOW.innerHTML=""
 const taskLow = toDoList.filter(task => task.priority===PRIOPITIES.LOW);
 
 taskLow.forEach(user=>{
-  const task = document.createObject("div")
+  const task = document.createElement("div")
   task.classList.add('task');
   task.innerHTML=`
     <input type="checkbox" class="checkBox">
@@ -162,98 +116,6 @@ checkBox.addEventListener('change',()=>{
 })
   listTasks.append(task);
 })
-
-
-
 clearInputLow()
 }
 
-
-
-/*
-const STATUS={
-  DONE:"Done",
-  TODO:"To DO",
-}
-
-const PRIOPITIES = {
-	HIGH: 'High',
-	LOW: 'Low'
-}
-
-const toDoList = [ 
-  {name: 'test', priority: PRIOPITIES.HIGH,status:STATUS.DONE} ,
-
-];
-
-const showList = () => {
-  for (const namePriorities in STATUS) {
-    let noneTask = true;
-
-    console.log(`\n${STATUS[namePriorities]}`);
-
-    const filteredStatus = toDoList.filter(
-      task => task.status === STATUS[namePriorities]
-    );
-
-    filteredStatus.forEach((task) => {
-      if (task) {
-        console.log(`\t${task.name}: ${task.priority} priority;`);
-        noneTask = false;
-      }
-    });
-
-    if (noneTask) {
-      console.log(`\t -`);
-    }
-  }
-};
-
-const UI_ELEMNTS = {
-  FROM_HIGH: document.querySelector('.addHighForm'),
-  INPUT_HIGH: document.querySelector('.addHighInput'),
-  FORM_BTN: document.querySelector('.addHighButton'),
-  LIST_TASKS_HIGH: document.querySelector('.list-taskHigh'),
-
-  DEL_BTN: document.querySelectorAll('closeButton')
- 
-}
-
-UI_ELEMNTS.FROM_HIGH.addEventListener('submit', (event) => {
-  event.preventDefault();
-  addTask(UI_ELEMNTS.INPUT_HIGH, UI_ELEMNTS.LIST_TASKS_HIGH);
-})
-
-function addTask(name,status,priority){
-  name=UI_ELEMNTS.INPUT_HIGH
-  toDoList.push({name,status,priority})
-  if(status="undefined" || priority == "undefined"){
-console.log("! Please enter all data for "+name+" !")
-  }
-}
-
-function changeStatus(nameNew,statusNew){
-  const changeTask = toDoList.findIndex(
-    task => task.name === nameNew)
- if(changeTask===-1){
-  console.log("!---Undefined change name---!")
-}
-else{
-  toDoList[changeTask].status=statusNew
-}
-}
-
-function deleteTask(nameDel){
-  const delTask = toDoList.findIndex(
-    task => task.name === nameDel)
-    if(delTask===-1){
-      console.log("!---Undefined delete name---!")
-    }
-    else{
-    toDoList.splice(delTask, 1);
-    }
-  }
-
-
-showList();
-*/
