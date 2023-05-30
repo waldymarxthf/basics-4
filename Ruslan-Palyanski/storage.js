de20ad1ad812b0c12e31b3289cccba6d8ede7c6e
Ruslan Polyanski <ruslapolyanski@yandex.by>
2023-05-30 23:13:44 +0300

    feat: add asycn function

---
+++ Ruslan-Palyanski/storage.js
@@ -0,0 +1,28 @@
+
+const storage = {
+    saveFavoriteCities(favoriteCitiesArr){
+        const favoriteCities = new Set(favoriteCitiesArr);
+        localStorage.arrayCities = JSON.stringify([...favoriteCities]);
+    },
+    setStartCity(startCity){
+        localStorage.startCity = startCity;
+    },
+    getFavoriteCities(){
+        try{
+            if(!localStorage.arrayCities){
+                return [];
+            } 
+            return JSON.parse(localStorage.arrayCities)
+        } catch(err){
+            return console.log(err);
+        }
+    },
+    getCurrentCity(){
+        if(!localStorage.startCity){
+            return 'Minsk';
+        }
+        return localStorage.startCity;
+    }
+}
+
+export {storage};
