c5b20f3a2ec342f88d2c568745a49b5ad12e53f1
Canai <nikitacanai@yandex.ru>
2023-06-02 19:43:54 +0700

    recursion

---
+++ recursion/script.js
@@ -0,0 +1,23 @@
+// let i = 0;
+// while (i < 10) {
+//   i++;
+//   console.log(i);
+// }
+
+// for (let n = 1; n < 11; n++) {
+//   console.log(n);
+// }
+
+// function nn(o) {
+//   if (o === 1) {
+//     console.log(o);
+//     console.log("fin");
+//   } else {
+//     console.log(o);
+//     nn(o - 1);
+//   }
+// }
+
+// nn(20);
+
+// не было циклов в программе но как работает понял хорошо)
