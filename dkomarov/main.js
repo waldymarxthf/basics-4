b0043873e87f3926bc08e3dcde5802a5a3765905
Dmitry Komarov <dmitrykomarov@MacBook-Pro-Dmitry.local>
2023-06-03 16:31:54 +0300

    recursion

---
+++ dkomarov/main.js
@@ -0,0 +1,154 @@
+import { addItemsFormHigh, addItemsFormLow, taskBlockHigh, taskBlockLow } from "./modules/DOMelements.js";
+
+const STATUS = {
+    TODO: 'to do',
+    DONE: 'done'
+}
+
+const PRIORITY = {
+    HIGH: 'high',
+    LOW: 'low'
+}
+
+const todoList = JSON.parse(localStorage.getItem('todoList')) || [];
+
+
+function addTask(text) {
+    todoList.push(text);
+    localStorage.setItem('todoList', JSON.stringify(todoList));
+
+};
+
+
+//ФУНКЦИЯ КОНСТРУКТОР 
+function Task(text, priority) {
+    this.id = countId;
+    this.name = text;
+    this.checked = false;
+    this.status = STATUS.TODO;
+    this.priority = priority;
+}
+
+
+//добавление задачи с высоким приоритетом 
+function addItemHigh(event) {
+    event.preventDefault();
+    const textHigh = event.target.itemHigh.value;
+    const taskHigh = new Task(textHigh, PRIORITY.HIGH)
+    addTask(taskHigh);
+    render();
+    this.reset();
+};
+
+
+//добавление задачи с низким приоритетом
+function addItemLow(event) {
+    event.preventDefault();
+    const textLow = event.target.itemLow.value;
+    const taskLow = new Task(textLow, PRIORITY.LOW);
+    addTask(taskLow);
+    render();
+    this.reset();
+};
+
+let countId = 0;
+
+//функция создания элемента с высоким приоритетом
+function createItemHigh(taskBlockHigh, task, priority) {
+    const newElement = document.createElement('div');
+    const checkBox = document.createElement('input');
+    const taskText = document.createElement('p');
+    const delButton = document.createElement('button');
+    const imgForDelButton = document.createElement('img');
+
+    newElement.classList.add('add-items-form_high');
+    checkBox.type = 'checkbox';
+    // checkBox.id = `${countId++}`;
+    taskText.textContent = task;
+    delButton.classList.add('button_del');
+    imgForDelButton.src = './image/free-icon-close-151882-444.svg';
+    imgForDelButton.id = `${countId}`;
+    delButton.id = `${countId++}`;
+
+    newElement.appendChild(checkBox);
+    newElement.appendChild(taskText);
+    delButton.appendChild(imgForDelButton);
+    newElement.appendChild(delButton);
+    taskBlockHigh.appendChild(newElement);
+
+    delButton.addEventListener('click', deleteTask);
+    // checkBox.addEventListener('change', changeStatus);
+
+    return taskBlockHigh;
+};
+
+//функция создания элемента с низким приоритетом 
+function createItemLow(taskBlockLow, task, priority) {
+    const newElement = document.createElement('div');
+    const checkBox = document.createElement('input');
+    const taskText = document.createElement('p');
+    const delButton = document.createElement('button');
+    const imgForDelButton = document.createElement('img');
+
+    newElement.classList.add('add-items-form_high');
+    checkBox.type = 'checkbox';
+    // checkBox.id = `${countId++}`;
+    taskText.textContent = task;
+    delButton.classList.add('button_del');
+    imgForDelButton.src = './image/free-icon-close-151882-444.svg';
+    imgForDelButton.id = `${countId}`;
+    delButton.id = `${countId++}`;
+
+    newElement.appendChild(checkBox);
+    newElement.appendChild(taskText);
+    delButton.appendChild(imgForDelButton);
+    newElement.appendChild(delButton);
+    taskBlockLow.appendChild(newElement);
+
+    delButton.addEventListener('click', deleteTask);
+    // checkBox.addEventListener('change', changeStatus);
+
+    return taskBlockLow;
+};
+
+
+//удаление элемента из массива 
+function deleteTask(event) {
+    const indexButton = event.target.getAttribute("id");
+    console.log(indexButton);
+    todoList.splice(indexButton, 1);
+    localStorage.setItem('todoList', JSON.stringify(todoList));
+    render();
+};
+
+//функция "отрисовки" html
+function render() {
+    countId = 0;
+    taskBlockHigh.textContent = '';
+    taskBlockLow.textContent = '';
+    function arrayIteration(n) {
+        if (n >= todoList.length) return;
+        let task = todoList[n];
+        task.priority == PRIORITY.HIGH ?
+            createItemHigh(taskBlockHigh, task.name, task.status) :
+            createItemLow(taskBlockLow, task.name, task.status);
+        arrayIteration(n + 1);
+    }
+    arrayIteration(0);
+    console.log(todoList);
+};
+
+//изменение статуса задачи по чекбоксу 
+// function changeStatus(event) {
+//    const elem = event.target.getAttribute('id');
+//    if (elem === )  //допилить поиск по айди
+//    console.log(elem)
+
+// };
+
+addItemsFormHigh.addEventListener('submit', addItemHigh);
+addItemsFormLow.addEventListener('submit', addItemLow);
+
+render();
+
+
---
+++ dkomarov/modules/domElements.js
@@ -0,0 +1,7 @@
+const addItemsFormHigh = document.querySelector('.add-items-form_high');
+const addItemsFormLow = document.querySelector('.add-items-form_low')
+const taskBlockHigh = document.querySelector('.taskBlockHigh');
+const taskBlockLow = document.querySelector('.taskBlockLow');
+
+
+export {addItemsFormHigh, addItemsFormLow, taskBlockHigh, taskBlockLow};
