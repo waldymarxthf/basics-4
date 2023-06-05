97fe0f623664b65aa3e450b8f2222ee76107b7f1
Anastasia <tgmonresu@gmail.com>
2023-06-05 06:27:30 +0300

    weather in npm proj

---
+++ monresu/logicTask.js
@@ -0,0 +1,103 @@
+const statuses = {
+  TODO: 'ToDo',
+  DONE: 'Done',
+}
+
+const priorities = {
+  HIGH: 'high',
+  LOW: 'low',
+}
+
+const errors = {
+  taskIsNotExists: 'Задачи не существует',
+  taskIsExists: 'Задача уже существует',
+  priorityIsNotExists: 'Такого приоритета не существует',
+  statusIsNotExists: 'Такого статуса не существует',
+}
+
+const messages = {
+  changeStatus: 'Статус задачи изменён',
+  changePriority: 'Приоритет задачи изменён',
+  addTask: 'Задача добавлена',
+  deleteTask: 'Задача удалена',
+}
+
+let list = JSON.parse(localStorage.getItem('tasks')) || [];
+
+class Task {
+  constructor(name, priority) {
+    this.name = name;
+    this.status = statuses.TODO;
+    this.priority = priority;
+  }
+}
+
+function saveTasksInLocalStorage() {
+  localStorage.setItem('tasks', JSON.stringify(list));
+}
+
+const isEmpty = (task) => { return !task.trim(); };
+
+function isTaskExists(name) {
+  return list.map(t => t.name).includes(name);
+}
+
+function indexOfTask(task) {
+  return list.findIndex(t => t.name === task);
+}
+
+function isPriorityExists(priority) {
+  return Object.values(priorities).includes(priority);
+}
+
+function isStatusExists(status) {
+  return Object.values(statuses).includes(status);
+}
+
+/* Добавление задачи в массив */
+function addTask(taskName, priority = priorities.LOW) {
+  try {
+    if (isTaskExists(taskName)) {
+      throw new Error('Такая задача уже есть!');
+    }
+    if (isEmpty(taskName)) {
+      throw new Error('Вы пытаетесь добавить пустую задачу!');
+    }
+    const task = new Task(taskName, priority);
+    list.push(task);
+    saveTasksInLocalStorage();
+  } catch(err) {
+    if (err.name == 'Error') {
+      alert(err.message);
+    }
+  }
+}
+
+/* Удаление задачи из массива */
+function removeTask(task) {
+  if (!isTaskExists(task)) {
+    console.log(errors.taskIsNotExists); // ошибка для разработчика (у пользователя ее возникнуть не может)
+    return;
+  }
+  const indexTask = indexOfTask(task);
+  list.splice(indexTask, 1);
+  saveTasksInLocalStorage();
+  return;
+}
+
+/* Изменение статуса задачи */
+function changeStatus(task, status) {
+  if (!isTaskExists(task)) {
+    console.log(errors.taskIsNotExists); // ошибка для разработчика
+    return;
+  }
+  if (!isStatusExists(status)) {
+    console.log(errors.statusIsNotExists); // ошибка для разработчика 
+    return;
+  }
+  const indexTask = indexOfTask(task);
+  list[indexTask].status = status;
+  saveTasksInLocalStorage();
+  render();
+  return;
+}
