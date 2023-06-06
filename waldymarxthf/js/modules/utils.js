fc336093bf39b94edb42191240fcb6c16c401a6b
waldymarxthf <iliya.nov223@gmail.com>
2023-06-06 12:09:36 +0300

    [feat] feat: init project

---
+++ waldymarxthf/js/modules/utils.js
@@ -0,0 +1,23 @@
+export function timeConverter(time, timezone) {
+	const newDate = new Date((time + timezone) * 1000);
+	const localDate = newDate.toLocaleTimeString([], {
+		hour: "2-digit",
+		minute: "2-digit",
+		timeZone: "UTC",
+	});
+	return localDate;
+}
+
+//* функция конвертации unix времени в обычное
+
+export function dateConverter(date, timezone) {
+	const newDate = new Date((date + timezone) * 1000);
+	const humanDate = newDate.toLocaleString("en-GB", {
+		day: "numeric",
+		month: "long",
+		timeZone: "UTC"
+	});
+	return humanDate;
+}
+
+//* функция конвертации unix даты в нормальную
