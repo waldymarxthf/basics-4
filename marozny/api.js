765d2b83f04f0b7344f815de1ba0e3b67c182041
Ruslan Maroz <ich.nemoroz@gmail.com>
2023-06-09 19:29:29 +0200

    init

---
+++ marozny/api.js
@@ -0,0 +1,18 @@
+const serverUrl = 'http://api.openweathermap.org/data/2.5/weather';
+const apiKey = 'afc9f2df39f9e9e49eeb1afac7034d35';
+
+export async function getData(cityName) {
+  try {
+    const url = `${serverUrl}?q=${cityName}&appid=${apiKey}&units=metric`;
+    const response = await fetch(url);
+
+    if (response.ok) {
+      const data = await response.json();
+      return data;
+    } else {
+      throw new Error((await response.json()).message);
+    }
+  } catch (error) {
+    alert(error);
+  }
+}
---
+++ marozny/localstorage.js
@@ -0,0 +1,10 @@
+function saveLocationToLocalStorage(key, value) {
+  localStorage.setItem(key, JSON.stringify(value));
+}
+
+function loadLocations(key) {
+  const location = JSON.parse(localStorage.getItem(key));
+  return location;
+}
+
+export { loadLocations, saveLocationToLocalStorage };
