33a37f8a0c99282a0331fc40feb3766372eb2fb7
vladislaWoronin <vladvoronin428@gmail.com>
2023-06-10 00:19:04 +0300

    fix getTimes

--- weitend/src/js/main.js
+++ weitend/src/js/main.js
@@ -1,16 +1,24 @@
-import  { isDate, differenceInYears  }  from 'date-fns';
-import { countdown } from 'countdown';
+import { isDate, formatDuration, intervalToDuration } from 'date-fns';
 import { ELEMENTS } from './ui.js';
-
 function getTimes(date) {
-    date = new Date(date);
+	date = new Date(date);
 	if (isDate(date)) {
+		const now = new Date()
+		let duration = intervalToDuration({
+			start: date,
+			end: now,
+		});
+		const res = formatDuration(duration, { format: ["years", "days", "hours"]});
 
+		console.log(res);
 	};
 };
 
-function timeRender() {
-
+function timeRender(dates) {
+	console.log(dates.years);
+	ELEMENTS.TIMER.YEARS.textContent = dates.years;
+	ELEMENTS.TIMER.DAYS.textContent = dates.days;
+	ELEMENTS.TIMER.HOURS.textContent = dates.hours;
 }
 
 ELEMENTS.FORM.BTN.addEventListener('click', (e) => {
@@ -18,6 +26,6 @@ ELEMENTS.FORM.BTN.addEventListener('click', (e) => {
 
 	const date = ELEMENTS.FORM.INPUT.value;
 
-	getTimes(date);
-	timeRender();
+	const dates = getTimes(date);
+	timeRender(dates);
 });
