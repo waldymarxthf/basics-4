2e09246e71598a331f14a1eafd169e0c184b1ea4
Vadim Sysoev <sysoev.dev@gmail.com>
2023-06-10 01:22:20 +0300

    Задача сделана

---
+++ sysoev-dev/js/ui.js
@@ -0,0 +1,14 @@
+export const UI_ELEMENTS = {
+  FORM: document.querySelector('.app__form'),
+  INPUT: document.querySelector('.form__input'),
+  OUTPUT_YEAR: document.querySelector('.js-year'),
+  OUTPUT_DAY: document.querySelector('.js-day'),
+  OUTPUT_HOURS: document.querySelector('.js-hours')
+}
+
+export function showInterval(date) {
+  console.log(date);
+  UI_ELEMENTS.OUTPUT_YEAR.textContent = date.years;
+  UI_ELEMENTS.OUTPUT_DAY.textContent = date.days;
+  UI_ELEMENTS.OUTPUT_HOURS.textContent = date.hours;
+}
---
+++ sysoev-dev/main.js
@@ -0,0 +1,16 @@
+import { UI_ELEMENTS, showInterval } from "./js/ui";
+import intervalToDuration from 'date-fns/intervalToDuration'
+
+function submitFormHandler(event) {
+  event.preventDefault();
+  const nowDate = new Date();
+  const endDate = new Date(`${UI_ELEMENTS.INPUT.value}T00:00:00`)
+  const result = intervalToDuration({
+    start: nowDate,
+    end: endDate
+  });
+  showInterval(result);
+  event.target.reset();
+}
+
+UI_ELEMENTS.FORM.addEventListener('submit', submitFormHandler)
