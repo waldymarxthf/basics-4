b77be1af59bdc7e4e685dd68656b96d971ffcf41
johnbeelow <beelow-dj@mail.ru>
2023-06-10 04:11:11 +0300

    add 19

--- johnbeelow/src/js/main.js
+++ johnbeelow/src/js/main.js
@@ -1,38 +1,7 @@
-import { UI_ELEMENTS, clearInput, animateIcon } from './module/ui_elements'
-
-import {
-  handleContentLoaded,
-  handleSendingData,
-  getToggleLikeAction,
-  changeActiveButton,
-} from './module/business-logic'
-
-document.addEventListener('DOMContentLoaded', () => {
-  UI_ELEMENTS.LOADER_DISCONECTED.style.display = 'none'
-})
-
-document.addEventListener('DOMContentLoaded', handleContentLoaded)
-
-for (const button of UI_ELEMENTS.BUTTONS_ALL) {
-  button.addEventListener('click', changeActiveButton)
-}
+import { UI_ELEMENTS } from "./module/ui_elements";
+import { renderDate } from "./module/business-logic";
 
 UI_ELEMENTS.INPUT_FORM.addEventListener('submit', (event) => {
   event.preventDefault()
-  handleSendingData(UI_ELEMENTS.INPUT_TEXT.value)
-  animateIcon()
-  clearInput()
-})
-
-UI_ELEMENTS.LIKE.addEventListener('click', (event) => {
-  event.preventDefault()
-  getToggleLikeAction()
-})
-
-window.addEventListener('online', () => {
-  UI_ELEMENTS.LOADER_DISCONECTED.style.display = 'none'
-})
-
-window.addEventListener('offline', () => {
-  UI_ELEMENTS.LOADER_DISCONECTED.style.display = 'flex'
+  renderDate(UI_ELEMENTS.INPUT_DATE.value)
 })
