578a7d774b038c5815027762d1cbb835de6a23d1
vladislaWoronin <vladvoronin428@gmail.com>
2023-06-10 17:11:24 +0300

    add: Cookies,

--- weitend/src/js/main.js
+++ weitend/src/js/main.js
@@ -1,31 +1,71 @@
+import { ELEMENTS } from './uiVariables.js';
 import { isDate, formatDuration, intervalToDuration } from 'date-fns';
-import { ELEMENTS } from './ui.js';
-function getTimes(date) {
+import { ru } from 'date-fns/locale';
+import Cookies from 'js-cookie';
+
+// берем дату из печенек и рендерим, если эта дата там есть
+function getCookiesDate() {
+	const cookiesDates = Cookies.get('date');
+	if (cookiesDates) {
+		const dates = getDate(cookiesDates);
+		timeRender(dates);
+	};
+	return;
+};
+
+// Приводим вводимую дату в нужный вид
+function getDate(date) {
 	date = new Date(date);
 	if (isDate(date)) {
-		const now = new Date()
-		let duration = intervalToDuration({
+		const duration = intervalToDuration({
 			start: date,
-			end: now,
+			end: new Date(),
 		});
-		const res = formatDuration(duration, { format: ["years", "days", "hours"]});
 
-		console.log(res);
+		// Если получили несколько месяцев до нужной даты
+		// то вычисляем количество дней из этих месяцев
+		if (duration.months > 0) {
+			const avgDaysPerMontsh = 30.4;
+
+			duration.days = Math.ceil((duration.months * avgDaysPerMontsh) + duration.days);
+			duration.months = 0;
+		};
+ 
+		duration.days > 365 ? duration.days = 365 : duration.days - 365;
+
+		Cookies.set('date', date);
+
+		const result = formatDuration(duration, {
+			format: ["years", "days", "hours"],
+			locale: ru
+		});
+
+		return { dateInput: date, dateOutput: result };
 	};
 };
 
-function timeRender(dates) {
-	console.log(dates.years);
-	ELEMENTS.TIMER.YEARS.textContent = dates.years;
-	ELEMENTS.TIMER.DAYS.textContent = dates.days;
-	ELEMENTS.TIMER.HOURS.textContent = dates.hours;
-}
+// Рендерим, проверяем, введенная дата меньше нынешней или нет
+// Если меньше, то пишем "Прошло времени:"
+// Иначе пишем "Осталось времени"
+function timeRender(datesObj) {
+	const switchText = datesObj.dateInput < new Date() ? 'Прошло времени:' : 'Осталось времени:';
+
+	ELEMENTS.TIMER.TEXT.textContent = switchText;
+	ELEMENTS.TIMER.COUNTER.textContent = datesObj.dateOutput;
+};
 
 ELEMENTS.FORM.BTN.addEventListener('click', (e) => {
 	e.preventDefault();
-
-	const date = ELEMENTS.FORM.INPUT.value;
-
-	const dates = getTimes(date);
-	timeRender(dates);
+	const dateInput = ELEMENTS.FORM.INPUT.value;
+	const dateOutput = getDate(dateInput);
+	timeRender(dateOutput);
 });
+
+ELEMENTS.FORM.FORM_WRAPPER.addEventListener('submit', (e) => {
+	e.preventDefault();
+	const dateInput = ELEMENTS.FORM.INPUT.value;
+	const dateOutput = getDate(dateInput);
+	timeRender(dateOutput);
+});
+
+getCookiesDate();
--- weitend/src/js/uiVariables.js
+++ weitend/src/js/uiVariables.js
@@ -1,13 +1,13 @@
 const ELEMENTS = {
 	FORM: {
+		FORM_WRAPPER: document.querySelector('.form'),
 		INPUT: document.querySelector('.form__input'),
 		BTN: document.querySelector('.form__btn'),
 	},
 	TIMER: {
-		YEARS: document.getElementById('timerYears'),
-		DAYS: document.getElementById('timerDays'),
-		HOURS: document.getElementById('timerHours'),
-	},
+		COUNTER: document.querySelector('.timer__counter'),
+		TEXT: document.querySelector('.timer__text')
+	}
 };
 
 export { ELEMENTS };
