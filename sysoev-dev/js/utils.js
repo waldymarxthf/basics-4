4c839f6bbb002bf4da974cbd8fcfa50f1747093c
Vadim Sysoev <sysoev.dev@gmail.com>
2023-06-10 17:08:52 +0300

    Добавил date-fns в sunrise и sunset

--- sysoev-dev/js/utils.js
+++ sysoev-dev/js/utils.js
@@ -1,6 +1,7 @@
-export const roundValue = value => Math.round(value);
+import { fromUnixTime, getHours, getMinutes } from 'date-fns';
+
+export const roundValue = (value) => Math.round(value);
 
 export function convertDate(utc) {
-  const date = new Date(utc * 1000);
-  return `${date.getHours()}:${date.getMinutes()}`;
+  return `${getHours(fromUnixTime(utc))}:${getMinutes(fromUnixTime(utc))}`;
 }
