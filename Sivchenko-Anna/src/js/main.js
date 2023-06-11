72f934e4c03cec210eebe9ea1fb21194fcbaf07c
Sivchenko-Anna <anya.malakhova.99@mail.ru>
2023-06-11 15:44:50 +1000

    add js logic for countdown

--- Sivchenko-Anna/src/js/main.js
+++ Sivchenko-Anna/src/js/main.js
@@ -1,1 +1,38 @@
-console.log("Work");
+import { formatDuration, intervalToDuration } from "date-fns";
+import ru from "date-fns/locale/ru";
+import { VARIABLES } from "./variables";
+
+function getDateInterval(desiredDate) {
+	const duration = intervalToDuration({
+		start: new Date(),
+		end: new Date(desiredDate),
+	});
+	const result = formatDuration(duration, {
+		format: ["years", "months", "days", "hours", "minutes"],
+		locale: ru,
+	});
+	return result;
+}
+
+let interval;
+
+function render(desiredDate) {
+	const text = new Date(desiredDate) < new Date() ? "Прошло времени:" : "Осталось времени:";
+	VARIABLES.TEXT_RESULT.textContent = text;
+
+	if (interval) {
+		clearInterval(interval);
+	}
+
+	interval = setInterval(() => {
+		VARIABLES.RESULT.textContent = getDateInterval(desiredDate);
+	}, 100);
+}
+
+function submitForm(event) {
+	event.preventDefault();
+	const desiredDate = VARIABLES.DATE.value;
+	render(desiredDate);
+}
+
+VARIABLES.FORM.addEventListener("submit", submitForm);
---
+++ Sivchenko-Anna/src/js/variables.js
@@ -0,0 +1,6 @@
+export const VARIABLES = {
+	FORM: document.querySelector(".countdown-date"),
+	DATE: document.querySelector(".countdown-date__input"),
+	TEXT_RESULT: document.querySelector(".countdown-subtitle"),
+	RESULT: document.querySelector(".countdown-output"),
+};
