84e186e644818e00d7a8b9ea5b1e3394242616ba
Borise <starcenkoboris2@gmail.com>
2023-06-12 13:49:11 +0300

    Refactor code after live with Yaroslav

--- src/script.js
+++ src/script.js
@@ -1,35 +1,29 @@
 import intervalToDuration from "date-fns/intervalToDuration";
 import { UI_ELEMENTS } from "./ui_elements";
 
-function clearInput() {
-	UI_ELEMENTS.INPUT_DATE.value = "";
-}
-
 function renderDate(resultObj) {
 	if (resultObj.months > 0) {
-		const avgDay = 30.5;
+		const avgDay = 30.4;
 		// eslint-disable-next-line no-param-reassign
 		resultObj.days = Math.ceil(resultObj.months * avgDay + resultObj.days);
 	}
 	UI_ELEMENTS.RESULT_SCREEN.textContent = `${resultObj.years} years ${resultObj.days} days ${resultObj.hours} hours`;
 }
 
-function countdown() {
-	const nowDate = new Date();
-	const endDate = new Date(UI_ELEMENTS.INPUT_DATE.value);
-	const result = intervalToDuration({
-		start: nowDate,
-		end: endDate,
-	});
-	renderDate(result);
-}
-
-UI_ELEMENTS.FORM.addEventListener("submit", (e) => {
-	e.preventDefault();
+function countdown(e) {
 	try {
-		countdown();
+		e.preventDefault();
+		const nowDate = new Date();
+		const endDate = new Date(UI_ELEMENTS.INPUT_DATE.value);
+		const result = intervalToDuration({
+			start: nowDate,
+			end: endDate,
+		});
+		renderDate(result);
 	} catch (err) {
 		UI_ELEMENTS.RESULT_SCREEN.textContent = "Enter date like in example";
 	}
-	clearInput();
-});
+	UI_ELEMENTS.INPUT_DATE.value = "";
+}
+
+UI_ELEMENTS.FORM.addEventListener("submit", (e) => countdown(e));
