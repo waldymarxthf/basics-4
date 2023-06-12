138dafbeed1cecd440b033b58dc7dda1d4207242
wwwWebcom <dim.golowcko2014@yandex.ru>
2023-06-12 23:41:22 +0300

    Countdown eslint fixes

--- wwwWebcom/src/main.js
+++ wwwWebcom/src/main.js
@@ -1,32 +1,33 @@
-import { formatDuration, intervalToDuration } from "../node_modules/date-fns";
-import {utcToZonedTime} from "../node_modules/date-fns-tz"
+import { formatDuration, intervalToDuration } from 'date-fns/esm';
+import { utcToZonedTime } from 'date-fns-tz/esm';
 
 const DOM_ELEMENTS = {
-  DATE: document.querySelector(".container-date"),
-  FORM: document.querySelector(".container-form"),
-  RESULT: document.querySelector(".container-timer"),
+  DATE: document.querySelector('.container-date'),
+  FORM: document.querySelector('.container-form'),
+  RESULT: document.querySelector('.container-timer'),
 };
 
 function calculate(value) {
-    const duration = intervalToDuration({
-      start: utcToZonedTime(new Date(value),"UTC"),
-      end: new Date()
-    });
+  const duration = intervalToDuration({
+    start: utcToZonedTime(new Date(value), 'UTC'),
+    end: new Date(),
+  });
 
-    const formatting = formatDuration(duration, {
-      format: ["years", "days", "hours", "minutes"],
-    });
-    return formatting;
+  const formatting = formatDuration(duration, {
+    format: ['years', 'days', 'hours', 'minutes'],
+  });
+  return formatting;
 }
 
 function renderCalculate() {
-  if(!DOM_ELEMENTS.DATE.value) {
-    return DOM_ELEMENTS.RESULT.innerHTML = "Enter the date!!!"
+  if (!DOM_ELEMENTS.DATE.value) {
+    (DOM_ELEMENTS.RESULT.innerHTML = 'Enter the date!!!');
+    return;
   }
   DOM_ELEMENTS.RESULT.innerHTML = calculate(DOM_ELEMENTS.DATE.value);
 }
 
-DOM_ELEMENTS.FORM.addEventListener("submit", (event) => {
+DOM_ELEMENTS.FORM.addEventListener('submit', (event) => {
   event.preventDefault();
-  renderCalculate()
+  renderCalculate();
 });
