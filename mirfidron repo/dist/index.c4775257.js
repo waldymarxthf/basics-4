c31ead37cb3fe4f6378577777080ced447fc6588
mirfidron <mirfidron@mail.ru>
2023-06-14 00:44:10 +0300

    data-timer ~ v.1

---
+++ mirfidron repo/dist/index.c4775257.js
@@ -0,0 +1,13 @@
+// import { format } from './node_modules/date-fns';
+// let date = format(new Date(), 'dd/MM/yyyy');
+const btn = document.querySelector(".btn");
+const inpt = document.querySelector(".inpt");
+const form = document.querySelector(".form");
+form.addEventListener("submit", time);
+function addTimer() {}
+function time() {
+    let date = format(new Date(`${impt.value}`), "dd/MM/yyyy");
+    console.log(date);
+}
+
+//# sourceMappingURL=index.c4775257.js.map
