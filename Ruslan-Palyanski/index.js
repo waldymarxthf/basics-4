4240f52e54be7555be16f73c5ce75ae1574cfdc6
Ruslan Polyanski <ruslapolyanski@yandex.by>
2023-06-17 21:27:47 +0300

    feat: add new file

---
+++ Ruslan-Palyanski/index.js
@@ -0,0 +1,11 @@
+// const names = new Storage(’names’);
+// names.get() возвращает значение для ключа names в localStorage;
+// names.set() устанавливает значение для ключа names в localStorage;
+// names.clear()  очищает значение для ключа names в localStorage;
+// names.isEmpty()  вернет true если ключ names в localStorage имеет пустое значение (null || undefind);
+
+class Name {
+    constructor(name){
+        this.name = name;
+    }
+}
