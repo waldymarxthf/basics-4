22e0df706d7f5818e3a3f2b30ca5e0561182df88
Ruslan Polyanski <ruslapolyanski@yandex.by>
2023-06-18 00:05:39 +0300

    feat: add function

--- Ruslan-Palyanski/index.js
+++ Ruslan-Palyanski/index.js
@@ -4,8 +4,30 @@
 // names.clear()  очищает значение для ключа names в localStorage;
 // names.isEmpty()  вернет true если ключ names в localStorage имеет пустое значение (null || undefind);
 
-class Name {
-    constructor(name){
-        this.name = name;
+class Storage {
+    constructor(key, storage, defaultValue){
+        this.key = key;
+        this.storage = storage;
+        this.defaultValue = defaultValue;
+    }
+
+    set(value = this.defaultValue){
+        this.storage.setItem(this.key, JSON.stringify(value))
+    }
+
+    get(key = this.key){
+        return JSON.parse(this.storage.getItem(key));
+    }
+
+    clear(key = this.key){
+        this.storage.removeItem(key);
+    }
+
+    isEmpty(key = this.key){
+        if(!this.storage.getItem(key)){
+            return false;
+        }
+        return true;
     }
 }
+
