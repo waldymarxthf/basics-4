4d0cc37dcfccd564421a41b8bb2ff9bc18f9a7f4
waldymarxthf <iliya.nov223@gmail.com>
2023-06-28 19:47:07 +0300

    [feat] feat: add bettertv api

--- waldymarxthf/src/script.js
+++ waldymarxthf/src/script.js
@@ -1,28 +1,37 @@
-/* eslint-disable no-undef */
-class Storage {
-	constructor(key, value = null, storage = "localStorage") {
-		this.key = key;
-		this.value = value;
-		this.storage = storage === "session" ? sessionStorage : localStorage;
-		this.storage.setItem(key, value);
-	}
+const text = document.querySelector(".text");
 
-	get() {
-		return JSON.parse(this.storage.getItem(this.key));
-	}
-
-	set(value) {
-		return this.storage.setItem(this.key, JSON.stringify(value));
-	}
-
-	clear() {
-		return this.storage.clear();
-	}
-
-	isEmpty() {
-		return !this.get();
-	}
+function createEmoji(emote) {
+	const img = document.createElement("img");
+	img.src = `https://cdn.betterttv.net/emote/${emote.id}/2x.webp`;
+	return img;
 }
 
-const names = new Storage("strada", "iliya");
-console.log(names);
+async function getRequest() {
+	const response = await fetch("https://api.betterttv.net/3/cached/users/twitch/490702236");
+	const data = await response.json();
+
+	const words = text.textContent.split(" ");
+	text.textContent = ""; // Очищаем текстовый узел
+
+	words.forEach((word, index) => {
+		let img;
+		data.sharedEmotes.forEach((emote) => {
+			if (emote.code === word) {
+				img = createEmoji(emote);
+			}
+		});
+		if (img) {
+			text.append(img);
+		} else {
+			text.append(document.createTextNode(word));
+		}
+		// Добавляем пробелы между словами, если это не последнее слово
+		if (index !== words.length - 1) {
+			text.append(document.createTextNode(" "));
+		}
+	});
+
+	console.log(text);
+}
+
+getRequest();
