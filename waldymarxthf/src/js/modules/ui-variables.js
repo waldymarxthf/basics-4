b4f4df0cc388dce08926973dad26e185210abcb5
waldymarxthf <iliya.nov223@gmail.com>
2023-06-29 23:38:46 +0300

    [feat] feat: add template and renderMessage

---
+++ waldymarxthf/src/js/modules/ui-variables.js
@@ -0,0 +1,20 @@
+export const VARIABLES = {
+	CHAT: {
+		WINDOW_SELECTOR: ".wrapper",
+		TEMPLATE_SELECTOR: "#message-template",
+		MESSAGE_SELECTOR: ".chat-window__message",
+		TEXT_SELECTOR: ".chat-window__message-text",
+		TIME_SELECTOR: ".chat-window__message-time",
+		CONTENT_SELECTOR: ".chat-window__message-content",
+		AVATAR_SELECTOR: ".chat-window__message-avatar-img",
+		NICKNAME_SELECTOR: ".chat-window__message-nickname",
+		WINDOW: document.querySelector(".wrapper"),
+	},
+	FORM: document.querySelector(".chat-message__form"),
+};
+
+export const PROPERTIES = {
+	HIDDEN: "hidden",
+	LEFT_SIDE: "left",
+	RIGHT_SIDE: "right",
+};
