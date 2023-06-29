059bfebd08011259b745a4fc3bb73d3eb18692ee
Vadim Sysoev <sysoev.dev@gmail.com>
2023-06-29 14:59:03 +0300

    верстка

--- sysoev-dev/src/main.js
+++ sysoev-dev/src/main.js
@@ -1,7 +1,29 @@
-import { UI_ELEMENTS, createModal } from './js/ui';
-function modalShow() {}
+import { UI_ELEMENTS } from './js/ui';
+import { createModal } from './js/create';
 
-UI_ELEMENTS.btnSettings.addEventListener('click', () => {
-  createModal('settings');
-  console.log(typeof UI_ELEMENTS.btnSettings.name);
-});
+function checkTypeBtnForm(target) {
+  switch (target) {
+    case UI_ELEMENTS.btnSettings:
+      return 'settings';
+    default:
+      break;
+  }
+}
+
+function modalShow(modal) {
+  UI_ELEMENTS.chat.append(modal);
+}
+
+function modalShowHandler(event) {
+  const typeForm = checkTypeBtnForm(event.target);
+  const modal = createModal(typeForm);
+
+  const btnClose = modal.querySelector('.modal__btn-close');
+  btnClose.addEventListener('click', () => {
+    modal.remove();
+  });
+
+  modalShow(modal);
+}
+
+UI_ELEMENTS.btnSettings.addEventListener('click', modalShowHandler);
