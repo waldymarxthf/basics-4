1eb8321a4d0bac37e7afd8c5167239bbf79190ac
Dmitry Komarov <dmitrykomarov@MacBook-Pro-Dmitry.local>
2023-06-30 22:41:10 +0300

    test v1.1

--- dkomarov/index.js
+++ dkomarov/index.js
@@ -7,6 +7,8 @@ const template = document.querySelector("#template");
 
 const openSettingsModal = document.querySelector('.header__btn-settings');
 const openCodeModal = document.querySelector('.authorization-btn-code');
+const inputAuthorizationValue = document.querySelector('.authorization-input');
+const formForRequest = document.querySelector('.authorization-form__input');
 
 function createNewMessage(event) { 
   event.preventDefault();
@@ -19,6 +21,39 @@ function createNewMessage(event) {
     this.reset();
   }
 }
+
+async function postData() {
+  const url = 'https://edu.strada.one/api/user';
+  const data = { email: inputAuthorizationValue.value };
+
+  const response = await fetch(url, {
+    method: 'POST', 
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  })
+  const result = await response.json()
+  console.log(result);
+}
+
+function addInfoMessage() {
+  const textElem = document.createElement('p');
+  textElem.textContent = 'Код отправлен на вашу почту ✅';
+  const parentElem = document.querySelector('.authorization');
+  textElem.classList.add('info-message');
+  parentElem.append(textElem);
+}
+
+function test() {
+  window.authentication.showModal();
+}
+
+formForRequest.addEventListener('submit', () => {
+  postData();
+  addInfoMessage()
+  setTimeout(test, 1500)
+});
 
 formNode.addEventListener("submit", createNewMessage);
 
