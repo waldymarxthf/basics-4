adbd04389aeaec23b9d99a2b3a145c371556af50
Canai <nikitacanai@yandex.ru>
2023-06-30 20:19:31 +0700

    #40 Чат: авторизация

--- efimov/scripts/dom.js
+++ efimov/scripts/dom.js
@@ -3,9 +3,25 @@ const fastDOM = (s) => document.querySelector(s);
 const form = fastDOM("#form");
 const input = fastDOM("#massage");
 const place = fastDOM("#place");
+const chat = fastDOM("#chat-body");
+const register = fastDOM("#register-body");
+const confirm = fastDOM("#confirmation-body");
+const settings = fastDOM("#chat-body");
+const buttonLogOut = fastDOM("#button-logout");
+const inputEmail = fastDOM("#inputEmail");
+const buttonNext = fastDOM("#buttonNext");
+const crossback = fastDOM("#cross-back");
 
 export default {
   form,
   input,
   place,
+  chat,
+  register,
+  confirm,
+  settings,
+  buttonLogOut,
+  inputEmail,
+  buttonNext,
+  crossback,
 };
--- efimov/scripts/script.js
+++ efimov/scripts/script.js
@@ -1,5 +1,12 @@
 import { Hours, Minutes } from "./date.js";
 import dom from "./dom.js";
+import { validateEmail } from "./validate.js";
+
+dom.buttonLogOut.addEventListener("click", LogOut);
+function LogOut() {
+  dom.chat.style.display = "none";
+  dom.register.style.display = "block";
+}
 
 dom.form.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -7,7 +14,6 @@ dom.form.addEventListener("submit", (e) => {
     createMassege();
   }
 });
-
 function createMassege() {
   let myMassege = document.createElement("div");
   myMassege.classList.add("myMassege");
@@ -15,3 +21,31 @@ function createMassege() {
   dom.place.append(myMassege);
   dom.place.scrollTo({ top: 9999999999, behavior: "smooth" });
 }
+
+dom.buttonNext.addEventListener("click", sendCode);
+function sendCode() {
+  if (dom.inputEmail.value !== "" && validateEmail(dom.inputEmail.value)) {
+    const url = "https://edu.strada.one/api/user";
+    const data = { email: dom.inputEmail.value };
+
+    fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then(() => {
+        dom.register.style.display = "none";
+        dom.confirm.style.display = "block";
+      })
+      .catch((error) => {
+        console.error("Ошибка:", error);
+      });
+  }
+}
+
+dom.crossback.addEventListener("click", () => {
+  dom.register.style.display = "block";
+  dom.confirm.style.display = "none";
+});
