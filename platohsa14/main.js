d5e1faa15b80ccf65f9433af50e0e3c03a429f88
Platosha14 <PheonX1410@gmail.com>
2023-06-30 15:34:03 +0400

    19

---
+++ platohsa14/main.js
@@ -0,0 +1,13 @@
+const formDate = document.querySelector('.input__text');
+const btn = document.querySelector('.input__btn')
+const time = document.querySelector('.time')
+
+let sec 
+let min
+let hour
+
+btn.addEventListener('click', () =>{
+    console.log(formDate.value);
+      
+})
+console.log()
