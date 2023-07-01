02101c05c85ea6e5d2e922a55e41cd980004cab1
Canai <nikitacanai@yandex.ru>
2023-07-01 18:15:15 +0700

    #41 Чат: смена имени

--- efimov/scripts/dom.js
+++ efimov/scripts/dom.js
@@ -6,11 +6,16 @@ const place = fastDOM("#place");
 const chat = fastDOM("#chat-body");
 const register = fastDOM("#register-body");
 const confirm = fastDOM("#confirmation-body");
-const settings = fastDOM("#chat-body");
+const settings = fastDOM("#settings-body");
 const buttonLogOut = fastDOM("#button-logout");
 const inputEmail = fastDOM("#inputEmail");
 const buttonNext = fastDOM("#buttonNext");
 const crossback = fastDOM("#cross-back");
+const buttonGo = fastDOM("#buttonGo");
+const inputCode = fastDOM("#inputCode");
+const buttonsettings = fastDOM("#button-settings");
+const changeName = fastDOM("#changeName");
+const inputName = fastDOM("#inputName");
 
 export default {
   form,
@@ -24,4 +29,9 @@ export default {
   inputEmail,
   buttonNext,
   crossback,
+  buttonGo,
+  inputCode,
+  buttonsettings,
+  inputName,
+  changeName,
 };
--- efimov/scripts/script.js
+++ efimov/scripts/script.js
@@ -1,6 +1,8 @@
 import { Hours, Minutes } from "./date.js";
 import dom from "./dom.js";
 import { validateEmail } from "./validate.js";
+import axios from "axios";
+import Cookies from "js-cookie";
 
 dom.buttonLogOut.addEventListener("click", LogOut);
 function LogOut() {
@@ -48,4 +50,36 @@ function sendCode() {
 dom.crossback.addEventListener("click", () => {
   dom.register.style.display = "block";
   dom.confirm.style.display = "none";
+});
+
+dom.buttonGo.addEventListener("click", () => {
+  const code = `${dom.inputCode.value}`;
+  Cookies.set("code", code);
+  dom.register.style.display = "none";
+  dom.confirm.style.display = "none";
+  dom.chat.style.display = "block";
+});
+
+dom.buttonsettings.addEventListener("click", () => {
+  dom.settings.style.display = "block";
+  dom.chat.style.display = "none";
+});
+
+dom.changeName.addEventListener("click", () => {
+  console.log(dom.inputName.value);
+  if (dom.inputName.value != "") {
+    axios.defaults.headers.common["Authorization"] = `Bearer ${Cookies.get(
+      "code"
+    )}`;
+    axios
+      .patch("https://edu.strada.one/api/user", {
+        name: `${dom.inputName.value}`,
+      })
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
 });
