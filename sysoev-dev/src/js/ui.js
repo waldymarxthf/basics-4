e44a100d2afef31c23742565091c1c8c6c8f8f47
Vadim Sysoev <sysoev.dev@gmail.com>
2023-07-01 12:01:25 +0300

    Переделал 39 задание

--- sysoev-dev/src/js/ui.js
+++ sysoev-dev/src/js/ui.js
@@ -1,5 +1,12 @@
 export const UI_ELEMENTS = {
-  BTN_SETTINGS: document.querySelector(".chat__top-btn-settings"),
-  SETTINGS_MODAL: document.querySelector(".dialog-settings"),
-  BTN_CLOSE_DIALOG: document.querySelector(".dialog__top-btn"),
+  BTN_SETTINGS: document.querySelector('.chat__top-btn-settings'),
+  SETTINGS_MODAL: document.querySelector('.dialog-settings'),
+  BTN_CLOSE_DIALOG: document.querySelector('.dialog__top-btn'),
+};
+
+export const MESSAGE = {
+  FORM: document.querySelector('.chat__bottom-form'),
+  INPUT: document.querySelector('.chat__bottom-input'),
+  TEMPLATE: document.querySelector('.message-template'),
+  LIST: document.querySelector('.chat__messages'),
 };
--- sysoev-dev/src/main.js
+++ sysoev-dev/src/main.js
@@ -1,11 +1,53 @@
-import { UI_ELEMENTS } from "./js/ui";
+import { UI_ELEMENTS, MESSAGE } from './js/ui';
 
-console.log(UI_ELEMENTS.SETTINGS_MODAL);
+function showMessage(item) {
+  MESSAGE.LIST.append(item);
+}
 
-UI_ELEMENTS.BTN_SETTINGS.addEventListener("click", () => {
+function createMessage(author, text, time, isAuthor) {
+  const item = document.createElement('li');
+  item.classList.add('chat__message');
+  if (isAuthor) {
+    item.classList.add('chat__message-author');
+  }
+  item.append(MESSAGE.TEMPLATE.content.cloneNode(true));
+  const messageAuthor = item.querySelector('.chat__message-author');
+  const messageText = item.querySelector('.chat__message-text');
+  const messageTime = item.querySelector('.chat__message-time');
+  messageAuthor.textContent = author;
+  messageText.textContent = text;
+  messageTime.textContent = time;
+  return item;
+}
+
+function validateMessageText(value) {
+  if (value.trim().length === 0) {
+    console.log('Пустой текст');
+    return;
+  } else {
+    return value.trim();
+  }
+}
+
+function sendMessageHandler() {
+  event.preventDefault();
+  const messageText = validateMessageText(MESSAGE.INPUT.value);
+  if (!messageText) {
+    return;
+  }
+  const messageAuthor = 'Я';
+  const messageTime = '10:33';
+  const item = createMessage(messageAuthor, messageText, messageTime, true);
+  showMessage(item);
+  event.target.reset();
+}
+
+UI_ELEMENTS.BTN_SETTINGS.addEventListener('click', () => {
   UI_ELEMENTS.SETTINGS_MODAL.showModal();
 });
 
-UI_ELEMENTS.BTN_CLOSE_DIALOG.addEventListener("click", () => {
+UI_ELEMENTS.BTN_CLOSE_DIALOG.addEventListener('click', () => {
   UI_ELEMENTS.SETTINGS_MODAL.close();
 });
+
+MESSAGE.FORM.addEventListener('submit', sendMessageHandler);
