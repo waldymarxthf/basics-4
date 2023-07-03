6d0aa8f60a44743444724cdedecd9831b66f8009
Marat <maratmirzabalaev@gmail.com>
2023-07-03 19:27:09 +0300

    m

---
+++ Marat/module_js/variables.js
@@ -0,0 +1,21 @@
+
+export const main = {
+    modal_settings: document.querySelector(".modal_settings"),
+    modal_autorisation: document.querySelector('.modal_autorisation'),
+    modal_confirmation: document.querySelector(".modal_confirmation"),
+    modal_autorisation: document.querySelector('.modal_autorisation'),
+    close_autorisation: document.querySelector(".close_autorisation"),
+    seting_button: document.querySelector(".seting_button"),
+    close_settings: document.querySelector(".close_settings"),
+    template: document.querySelector(".template"),
+    message_my: document.querySelector('.message_my'),
+    message_you: document.querySelector('.message_you'),
+    message: document.querySelector(".message"),
+    form: document.querySelector(".bottom_content"),
+    input_message: document.querySelector(".input_message"),
+    get_code: document.querySelector(".get_code"),
+    enter_code: document.querySelector(".enter_code"),
+    close_confirmation: document.querySelector(".close_confirmation"),
+    form_confirmation: document.querySelector(".form_confirmation"),
+
+}
--- Marat/script.js
+++ Marat/script.js
@@ -1,19 +1,26 @@
-const modal = document.querySelector(".modal");
-const seting_button = document.querySelector(".seting_button");
-const close = document.querySelector(".close");
-const template = document.querySelector(".template");
-const message_my = document.querySelector('.message_my');
-const message_you = document.querySelector('.message_you');
-const message = document.querySelector(".message");
+import Cookies from "js-cookie";
+import { saveCodeToCookie, getCodeFromCookie, updateUserName, getUserData } from "./module_js/cookie";
+import { main } from "./module_js/variables";
 
-seting_button.addEventListener('click', function () {
-    modal.showModal();
+
+main.seting_button.addEventListener('click', function () {
+    main.modal_settings.showModal();
 });
-close.addEventListener('click', function () {
-    modal.close();
+main.close_settings.addEventListener('click', function () {
+    main.modal_settings.close();
 });
+main.close_autorisation.addEventListener('click', function () {
+    main.modal_autorisation.close();
+});
+
+main.enter_code.addEventListener("click", function (event) {
+    event.preventDefault();
+    main.modal_autorisation.close();
+    main.modal_confirmation.showModal();
+
+})
 function generatorTemplate(messagee, time) {
-    let clone = template.content.cloneNode(true);
+    let clone = main.template.content.cloneNode(true);
     let li = clone.querySelectorAll("li");
     const spanText = li[0].querySelector(".message_text");
     const spanTime = li[0].querySelector(".message_time");
@@ -21,27 +28,58 @@ function generatorTemplate(messagee, time) {
     spanText.textContent = messagee;
     spanTime.textContent = time;
 
-    message_my.appendChild(clone);
+    main.message_my.appendChild(clone);
 }
+main.form.addEventListener("submit", function (event) {
+    event.preventDefault();
+    generatorTemplate(main.input_message.value, "19:28");
+});
 
-generatorTemplate("hello", "12:45");
-generatorTemplate("hello", "12:47");
 
-// const open_module = document.querySelector(".open_module");
-// const module = document.querySelector('.module');
-// const closeModule = document.querySelector('.closeModule');
-// const module_inner = document.querySelector('module_inner');
+// Функция для отправления кода на почту
+async function codeToEmaol(event){
+    event.preventDefault();
 
-// open_module.addEventListener('click', function() {
-//     module.showModal();
-// })
+    const email = document.querySelector(".autorisation").value;
+    console.log(email);
 
-// closeModule.addEventListener('click', function(){
-//     module.close()
-// });
+    try {
+        const response = await fetch('https://edu.strada.one/api/user', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ email }),
+        });
 
-// module.addEventListener('click', function(event){
-//     console.log(event.target);
-//     if(event.target === module_inner) return;
-//     if(event.target === module) module.close()
-// })
+        if (response.ok) {
+            const data = await response.json();
+            console.log('Код отправлен на почту')
+        }
+        else {
+            console.log('Запрос откланен');
+        }
+    } catch (error) {
+        console.error('Error: ', error)
+    }
+}
+main.get_code.addEventListener("click", codeToEmaol);
+
+
+main.form_confirmation.addEventListener('submit', async function (event) {
+    event.preventDefault();
+    try {
+        const code = document.querySelector(".code");
+        console.log(code.value);
+        await saveCodeToCookie(code.value);
+        await updateUserName('name');
+        await getUserData()
+
+        main.modal_confirmation.close();
+        main.modal_autorisation.close();
+    } catch (error) {
+        console.error(error);
+
+    }
+
+})
